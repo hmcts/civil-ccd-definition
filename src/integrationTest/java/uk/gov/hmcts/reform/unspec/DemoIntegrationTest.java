470985040d4d25b4bca43cf9b05dacfa69e0882a
Piotr Dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2021-01-29 14:45:47 +0000

    feat: added dummy IT (CMC-1162) (#3)

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/unspec/DemoIntegrationTest.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.unspec;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoIntegrationTest {
+
+    @Test
+    public void exampleOfTest() {
+        assertTrue(System.currentTimeMillis() > 0, "Example of Integration Test");
+    }
+}
