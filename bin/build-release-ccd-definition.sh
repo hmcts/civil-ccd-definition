5f33987e2a1cba0e7ef0f24848dcdcb6c6ef9003
Piotr Dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2021-02-09 15:47:28 +0000

    feat: disabled managementWeb on preview and staging + pipeline fixes (#7)

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -18,11 +18,11 @@ fi
 root_dir=$(realpath $(dirname ${0})/..)
 config_dir=${root_dir}/ccd-definition
 build_dir=${root_dir}/build/ccd-release-config
-release_definition_output_file=${build_dir}/civil-damages-ccd-definition-${environment}.xlsx
+release_definition_output_file=${build_dir}/ccd-unspec-${environment}.xlsx
 
 mkdir -p ${build_dir}
 
 $(${root_dir}/bin/variables/load-${environment}-environment-variables.sh)
 
 # build the ccd definition file
-${root_dir}/bin/utils/process-definition.sh ${config_dir} ${release_definition_output_file} "${excludedFilenamePatterns}"
+${root_dir}/civil-unspecified-docker/bin/utils/process-definition.sh ${config_dir} ${release_definition_output_file} "${excludedFilenamePatterns}"
