82f6da077b1ade41a2b91d437b19eb0f96c55cfc
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-05-11 16:06:21 +0100

    refactor: changed valid pba number + removed nonexistent one

--- e2e/fixtures/events/resubmitClaim.js
+++ e2e/fixtures/events/resubmitClaim.js
@@ -1,13 +1,13 @@
 const { listElement } = require('../../api/dataHelper');
 
-const selectedPBA = listElement('PBA0077597');
+const selectedPBA = listElement('PBA0088192');
 module.exports = {
   valid: {
     ResubmitClaim: {
       applicantSolicitor1PbaAccounts: {
         list_items: [
-          selectedPBA,
-          listElement('PBA0078094')
+          selectedPBA
+          // listElement('PBA0078094')
         ],
         value: selectedPBA
       }
