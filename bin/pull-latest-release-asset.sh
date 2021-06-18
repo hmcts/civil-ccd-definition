cb13ad25bb11d0cfa7d43587ffccaf84b10fa284
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-06-18 12:01:11 +0100

    Attempt to log github api error

--- bin/pull-latest-release-asset.sh
+++ bin/pull-latest-release-asset.sh
@@ -6,8 +6,10 @@ assetName=$2
 for retries in {1..5}
 do
   echo "Try ${retries}"
+  latestAsset=$(curl https://api.github.com/repos/hmcts/${repoName}/releases/latest)
+  echo $latestAsset
 
-  latestAssetId=$(curl https://api.github.com/repos/hmcts/${repoName}/releases/latest \
+  latestAssetId=$(echo $latestAsset \
    | docker run --rm --interactive stedolan/jq ".assets[] | select(.name==\"${assetName}\") | .id")
 
   curl -L \
