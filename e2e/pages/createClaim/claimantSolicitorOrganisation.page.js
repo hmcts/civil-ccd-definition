e052b3298dc3ed9c345eb375fbb28d0f134119b9
Sam Walker <sam.walker@hmcts.net>
2021-07-14 13:56:32 +0100

    feat: prepopulate claimant organisation (CMC-1059) (#157)

--- e2e/pages/createClaim/claimantSolicitorOrganisation.page.js
+++ e2e/pages/createClaim/claimantSolicitorOrganisation.page.js
@@ -3,17 +3,13 @@ const {I} = inject();
 module.exports = {
 
   fields: {
-    orgPolicyReference: '#applicant1OrganisationPolicy_OrgPolicyReference',
-    searchText: '#search-org-text'
+    searchText: '#search-org-text',
+    selectedOrg: '#organisation-selected-table'
   },
 
   async enterOrganisationDetails() {
-    I.waitForElement(this.fields.orgPolicyReference);
+    I.waitForElement(this.fields.selectedOrg);
     await I.runAccessibilityTest();
-    I.fillField(this.fields.orgPolicyReference, 'Claimant policy reference');
-    I.waitForElement(this.fields.searchText);
-    I.fillField(this.fields.searchText, 'Civil');
-    I.click('a[title="Select the organisation Civil - Organisation 1"]');
     await I.clickContinue();
   }
 };
