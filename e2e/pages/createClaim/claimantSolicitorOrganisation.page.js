f2514da5712b5f462a87ac54955487971aca500b
Piotr Dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2021-07-14 16:08:00 +0100

    Revert "feat: prepopulate claimant organisation (CMC-1059) (#157)"
    This reverts commit e052b3298dc3ed9c345eb375fbb28d0f134119b9.

--- e2e/pages/createClaim/claimantSolicitorOrganisation.page.js
+++ e2e/pages/createClaim/claimantSolicitorOrganisation.page.js
@@ -3,13 +3,17 @@ const {I} = inject();
 module.exports = {
 
   fields: {
-    searchText: '#search-org-text',
-    selectedOrg: '#organisation-selected-table'
+    orgPolicyReference: '#applicant1OrganisationPolicy_OrgPolicyReference',
+    searchText: '#search-org-text'
   },
 
   async enterOrganisationDetails() {
-    I.waitForElement(this.fields.selectedOrg);
+    I.waitForElement(this.fields.orgPolicyReference);
     await I.runAccessibilityTest();
+    I.fillField(this.fields.orgPolicyReference, 'Claimant policy reference');
+    I.waitForElement(this.fields.searchText);
+    I.fillField(this.fields.searchText, 'Civil');
+    I.click('a[title="Select the organisation Civil - Organisation 1"]');
     await I.clickContinue();
   }
 };
