10e8365d62a917722353862b58a25f40057d2d72
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2021-07-14 10:09:46 +0100

    feat:cleanu piplein for high level data setup (CMC-a578)

--- bin/variables/load-prod-environment-variables.sh
+++ bin/variables/load-prod-environment-variables.sh
@@ -4,4 +4,8 @@ set -eu
 
 echo 'export ENVIRONMENT=prod'
 
+echo "export S2S_URL_BASE=http://rpe-service-auth-provider-prod.service.core-compute-prod.internal"
+echo "export IDAM_API_URL_BASE=https://idam-api.prod.platform.hmcts.net"
+echo "export CCD_IDAM_REDIRECT_URL=https://www-ccd.prod.platform.hmcts.net/oauth2redirect"
 echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-prod.service.core-compute-prod.internal"
+echo "export CAMUNDA_BASE_URL=http://civil-ccd-camunda-staging-aat.service.core-compute-aat.internal"
---
+++ src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.civil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
+import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+
+import java.util.List;
+import java.util.Locale;
+
+public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
+    private static final Logger logger = LoggerFactory.getLogger(HighLevelDataSetupApp.class);
+
+    private final CcdEnvironment environment;
+
+    public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
+        super(dataSetupEnvironment);
+        environment = dataSetupEnvironment;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        if (CcdEnvironment.valueOf(args[0].toUpperCase(Locale.UK)).equals(CcdEnvironment.PROD)) {
+            return;
+        }
+
+        main(HighLevelDataSetupApp.class, args);
+    }
+
+    @Override
+    protected boolean shouldTolerateDataSetupFailure() {
+        return true;
+    }
+
+
+    @Override
+    protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
+        return List.of("build/ccd-config/ccd-nfd-" + environment.name().toLowerCase(Locale.UK) + ".xlsx");
+    }
+}
