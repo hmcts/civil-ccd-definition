4ca38270a8aabb1b6f98d4b1d447a500e20b6433
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2021-07-16 12:24:08 +0100

    update the pipeline name

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # User used during the CCD import and ccd-role creation
-./bin/utils/ccd-add-role.sh "caseworker-civil"
-./bin/utils/ccd-add-role.sh "caseworker-caa"
-./bin/utils/ccd-add-role.sh "caseworker-approver"
-./bin/utils/ccd-add-role.sh "prd-admin"
+${root_dir}/bin/utils/ccd-add-role.sh "caseworker-civil"
+${root_dir}/bin/utils/ccd-add-role.sh "caseworker-caa"
+${root_dir}/bin/utils/ccd-add-role.sh "caseworker-approver"
+${root_dir}/bin/utils/ccd-add-role.sh "prd-admin"
 
 roles=("solicitor" "systemupdate" "admin" "staff")
 for role in "${roles[@]}"
 do
-  ./bin/utils/ccd-add-role.sh "caseworker-civil-${role}"
+  ${root_dir}/bin/utils/ccd-add-role.sh "caseworker-civil-${role}"
 done
