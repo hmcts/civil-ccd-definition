e9d72c9bffb622d2d92351e8f7c596c409030d10
Doug Rice <douglasrice@yahoo.com>
2021-07-16 09:16:27 +0100

    test

--- e2e/tests/smoke_test.js
+++ e2e/tests/smoke_test.js
@@ -4,5 +4,6 @@ Feature('Smoke tests @smoke-tests');
 
 Scenario('Sign in as solicitor user', async (I) => {
   await I.login(config.applicantSolicitorUser);
+  I.click("body > exui-root > xuilib-cookie-banner > div > div > div.govuk-button-group > button:nth-child(1)");
   await I.see('Case List');
 });
