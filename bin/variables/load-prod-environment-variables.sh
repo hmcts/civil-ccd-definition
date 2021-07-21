b4dc40786012d9c035dbafbc8b6b4961876b786a
dharmendra kumar <dharmendra.kumar@hmcts.net>
2021-07-21 12:37:29 +0100

    update env variables

--- bin/variables/load-prod-environment-variables.sh
+++ bin/variables/load-prod-environment-variables.sh
@@ -3,9 +3,4 @@
 set -eu
 
 echo 'export ENVIRONMENT=prod'
-
-echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-prod.service.core-compute-prod.internal"
-echo "export IDAM_API_BASE_URL=https://idam-api.prod.platform.hmcts.net"
-echo "export CCD_IDAM_REDIRECT_URL=https://www-ccd.prod.platform.hmcts.net/oauth2redirect"
-echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://ccd-definition-store-api-prod.service.core-compute-prod.internal"
 echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-prod.service.core-compute-prod.internal"
