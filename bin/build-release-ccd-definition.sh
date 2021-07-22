7e0a73da295709d4d6d0c92e18013fce910f8562
dharmendra kumar <dharmendra.kumar@hmcts.net>
2021-07-22 11:04:37 +0100

    refactor:move the files in different location

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -22,6 +22,7 @@ release_definition_output_file=${build_dir}/civil-ccd-${environment}.xlsx
 github_file=${github_dir}/civil-ccd-${environment}.xlsx
 
 mkdir -p ${build_dir}
+mkdir -p ${github_dir}
 
 # build the ccd definition file
 ${root_dir}/bin/utils/process-definition.sh ${config_dir} ${release_definition_output_file} "${excludedFilenamePatterns}"
