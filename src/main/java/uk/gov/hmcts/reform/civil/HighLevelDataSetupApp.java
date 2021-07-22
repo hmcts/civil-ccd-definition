18ede27bbedcc45853df609ff074ed4f38e016b7
dharmendra kumar <dharmendra.kumar@hmcts.net>
2021-07-22 13:53:35 +0100

    refactor:enable ccd deployment in prod

--- src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
@@ -33,10 +33,6 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     }
 
     public static void main(String[] args) throws Throwable {
-        if (CcdEnvironment.valueOf(args[0].toUpperCase(Locale.UK)).equals(CcdEnvironment.PROD)) {
-            return;
-        }
-
         main(HighLevelDataSetupApp.class, args);
     }
 
