9a5c32cb46b096014873433c961c93aa95322b8f
rb-hmcts <richard.ball@hmcts.net>
2021-08-03 09:29:12 +0100

    Fixed lint errors

--- e2e/fragments/specClaimantDetails.js
+++ e2e/fragments/specClaimantDetails.js
@@ -1,6 +1,5 @@
 const { I } = inject();
-const party = require("../helpers/party");
-const postcodeLookup = require("./addressPostcodeLookup");
+const postcodeLookup = require('./addressPostcodeLookup');
 
 module.exports = {
   fields: function (partyType) {
@@ -8,19 +7,19 @@ module.exports = {
       type: {
         id: `#${partyType}_type`,
         options: {
-          individual: "Individual",
-          company: "Company",
-          organisation: "Organisation",
-          soleTrader: "Sole trader",
+          individual: 'Individual',
+          company: 'Company',
+          organisation: 'Organisation',
+          soleTrader: 'Sole trader',
         },
       },
       individual: {
         title: `#${partyType}_individualTitle`,
         firstName: `#${partyType}_individualFirstName`,
         lastName: `#${partyType}_individualLastName`,
-        day: "#individualDateOfBirth-day",
-        month: "#individualDateOfBirth-month",
-        year: "#individualDateOfBirth-year",
+        day: '#individualDateOfBirth-day',
+        month: '#individualDateOfBirth-month',
+        year: '#individualDateOfBirth-year',
       },
       company: {
         name: `#${partyType}_companyName`,
@@ -30,9 +29,9 @@ module.exports = {
         firstName: `#${partyType}_soleTraderFirstName`,
         lastName: `#${partyType}_soleTraderLastName`,
         tradingAs: `#${partyType}_soleTraderTradingAs`,
-        day: "#soleTraderDateOfBirth-day",
-        month: "#soleTraderDateOfBirth-month",
-        year: "#soleTraderDateOfBirth-year",
+        day: '#soleTraderDateOfBirth-day',
+        month: '#soleTraderDateOfBirth-month',
+        year: '#soleTraderDateOfBirth-year',
       },
       organisation: {
         name: `#${partyType}_organisationName`,
@@ -49,31 +48,31 @@ module.exports = {
     });
 
     switch (party) {
-      case "individual":
-        I.fillField(this.fields(partyType).individual.title, "Mr");
-        I.fillField(this.fields(partyType).individual.firstName, "John");
-        I.fillField(this.fields(partyType).individual.lastName, "Jones");
-        if (partyType === "applicant1") {
+      case 'individual':
+        I.fillField(this.fields(partyType).individual.title, 'Mr');
+        I.fillField(this.fields(partyType).individual.firstName, 'John');
+        I.fillField(this.fields(partyType).individual.lastName, 'Jones');
+        if (partyType === 'applicant1') {
           I.fillField(this.fields(partyType).individual.day, 1);
           I.fillField(this.fields(partyType).individual.month, 1);
           I.fillField(this.fields(partyType).individual.year, 1981);
         }
         break;
-      case "company":
+      case 'company':
         I.fillField(this.fields(partyType).company.name, `Example ${partyType} company`);
         break;
-      case "soleTrader":
-        I.fillField(this.fields(partyType).soleTrader.title, "Mr");
-        I.fillField(this.fields(partyType).soleTrader.firstName, "John");
-        I.fillField(this.fields(partyType).soleTrader.lastName, "Jones");
-        if (partyType === "applicant1") {
-          I.fillField(this.fields(partyType).soleTrader.tradingAs, "John Jones Windows");
+      case 'soleTrader':
+        I.fillField(this.fields(partyType).soleTrader.title, 'Mr');
+        I.fillField(this.fields(partyType).soleTrader.firstName, 'John');
+        I.fillField(this.fields(partyType).soleTrader.lastName, 'Jones');
+        if (partyType === 'applicant1') {
+          I.fillField(this.fields(partyType).soleTrader.tradingAs, 'John Jones Windows');
           I.fillField(this.fields(partyType).soleTrader.day, 1);
           I.fillField(this.fields(partyType).soleTrader.month, 1);
           I.fillField(this.fields(partyType).soleTrader.year, 1981);
         }
         break;
-      case "organisation":
+      case 'organisation':
         I.fillField(this.fields(partyType).organisation.name, `Example ${partyType} organisation`);
         break;
     }
