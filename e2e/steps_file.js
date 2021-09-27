09c633ad3b696ba89c24110e4f8980fab0009cbe
Doug Rice <douglasrice@yahoo.com>
2021-09-27 08:48:08 +0100

    add retries to see if helps

--- e2e/steps_file.js
+++ e2e/steps_file.js
@@ -85,16 +85,16 @@ module.exports = function () {
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
     async login(user) {
-      if (await this.hasSelector(SIGNED_IN_SELECTOR)) {
-        await this.signOut();
+      if (await I.retry(5).this.hasSelector(SIGNED_IN_SELECTOR)) {
+        await I.retry(5).this.signOut();
       }
 
       await this.retryUntilExists(async () => {
-        this.amOnPage(config.url.manageCase);
+        I.retry(5).this.amOnPage(config.url.manageCase);
 
         if (!config.idamStub.enabled || config.idamStub.enabled === 'false') {
           output.log(`Signing in user: ${user.type}`);
-          await loginPage.signIn(user);
+          await I.retry(5).loginPage.signIn(user);
         }
       }, SIGNED_IN_SELECTOR);
     },
