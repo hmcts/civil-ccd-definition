0b5d8854e361cf5b1f82c0556cc274218c467124
douglasrice <douglasrice@yahoo.com>
2021-09-27 14:24:32 +0100

    add retries to scenarios (#200)

--- e2e/tests/solicitorCreatesClaim_test.js
+++ e2e/tests/solicitorCreatesClaim_test.js
@@ -14,7 +14,7 @@ Scenario('Applicant solicitor creates claim @create-claim', async ({I}) => {
 
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
-});
+}).retry(3);
 
 Scenario('Applicant solicitor notifies defendant solicitor of claim', async ({I}) => {
   await I.notifyClaim();
@@ -23,18 +23,20 @@ Scenario('Applicant solicitor notifies defendant solicitor of claim', async ({I}
   if (config.multipartyTestsEnabled) {
     await assignCaseToDefendant(caseId(), 'RESPONDENTSOLICITORTWO', config.secondDefendantSolicitorUser);
   }
-});
+}).retry(3);
 
 Scenario('Applicant solicitor notifies defendant solicitor of claim details', async ({I}) => {
   await I.notifyClaimDetails();
   await I.see(caseEventMessage('Notify claim details'));
-});
+  await I.click('Sign out');
+}).retry(3);
 
 Scenario('Defendant solicitor acknowledges claim', async ({I}) => {
   await I.login(config.defendantSolicitorUser);
   await I.acknowledgeClaim('fullDefence');
   await I.see(caseEventMessage('Acknowledge claim'));
-});
+  await I.click('Sign out');
+}).retry(3);
 
 Scenario('Defendant solicitor requests deadline extension', async ({I}) => {
   if (config.multipartyTestsEnabled) {
@@ -46,7 +48,7 @@ Scenario('Defendant solicitor requests deadline extension', async ({I}) => {
   await I.informAgreedExtensionDate();
   await I.see(caseEventMessage('Inform agreed extension date'));
 
-});
+}).retry(3);
 
 Scenario('Defendant solicitor adds defendant litigation friend', async ({I}) => {
   await I.addDefendantLitigationFriend();
@@ -56,11 +58,12 @@ Scenario('Defendant solicitor adds defendant litigation friend', async ({I}) => 
 Scenario('Defendant solicitor responds to claim', async ({I}) => {
   await I.respondToClaim('fullDefence');
   await I.see(caseEventMessage('Respond to claim'));
-});
+  await I.click('Sign out');
+}).retry(3);
 
 Scenario('Applicant solicitor responds to defence', async ({I}) => {
   await I.login(config.applicantSolicitorUser);
   await I.respondToDefence();
   await I.see(caseEventMessage('View and respond to defence'));
   await waitForFinishedBusinessProcess(caseId());
-});
+}).retry(3);
