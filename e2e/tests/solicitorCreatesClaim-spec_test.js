fea1ab4901d9122ef4af1010df65f52de8850ab1
Jaber Elfasi <jaberelfasi@gmail.com>
2021-10-19 09:50:07 +0100

    ROC-9593 upgrading spec tests

--- e2e/tests/solicitorCreatesClaim-spec_test.js
+++ e2e/tests/solicitorCreatesClaim-spec_test.js
@@ -51,77 +51,77 @@ const journeys = {
 
 Feature('Claim creation @e2e-tests-spec');
 
-Scenario('Applicant solicitor creates specified claim individual-to-individual @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim individual-to-individual @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey1.applicant, journeys.journey1.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim individual-to-company @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim individual-to-company @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey2.applicant, journeys.journey2.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim individual-to-soletrader @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim individual-to-soletrader @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey3.applicant, journeys.journey3.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim individual-to-organisation @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim individual-to-organisation @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey4.applicant, journeys.journey4.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim company-to-company @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim company-to-company @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey5.applicant, journeys.journey5.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim company-to-soletrader @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim company-to-soletrader @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey6.applicant, journeys.journey6.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim company-to-organisation @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim company-to-organisation @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey7.applicant, journeys.journey7.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim soletrader-to-soletrader @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim soletrader-to-soletrader @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey8.applicant, journeys.journey8.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim soletrader-to-organisation @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim soletrader-to-organisation @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey9.applicant, journeys.journey9.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Applicant solicitor creates specified claim organisation-to-organisation @create-claim', async (I) => {
+Scenario('Applicant solicitor creates specified claim organisation-to-organisation @create-claim', async ({ I }) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCaseSpec(journeys.journey10.applicant, journeys.journey10.defendant);
   caseNumber = await I.grabCaseNumber();
   await I.see(`Case ${caseNumber} has been created.`);
 });
 
-Scenario('Defendant solicitor acknowledges claim', async (I) => {
+Scenario('Defendant solicitor acknowledges claim', async ({ I }) => {
   await assignCaseToDefendant(caseId());
   await I.login(config.defendantSolicitorUser);
   await I.acknowledgeClaimSpec();
