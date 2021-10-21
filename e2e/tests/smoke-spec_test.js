acdf5d9f88d041cbcb044931db3601621c7af217
Jaber Elfasi <jaberelfasi@gmail.com>
2021-10-21 13:31:37 +0100

    ROC-9593 removed e2e/tests/smoke-spec_test.js

--- e2e/tests/smoke-spec_test.js
+++
@@ -1,8 +0,0 @@
-const config = require('../config.js');
-
-Feature('Smoke tests @smoke-tests-spec');
-
-Scenario('Sign in as solicitor user', async ({ I }) => {
-  await I.login(config.applicantSolicitorUser);
-  await I.see('Case list');
-});
