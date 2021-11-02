73a19a8d6fa50cbd8c521d4acfcfa12941bad6f0
douglasrice <douglasrice@yahoo.com>
2021-11-02 17:39:55 +0000

    Revert "Feature/roc 9592 (#215)" (#220)

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -7,11 +7,11 @@ excludeNonProdFiles=${2:-true}
 
 
 if [ ${excludeNonProdFiles} == true ]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*LRspec.json"
+  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json"
 elif [ ${environment} == preview ]; then
-  excludedFilenamePatterns="-e *-nonprod.json,*LRspec.json"
+  excludedFilenamePatterns="-e *-nonprod.json"
 else
-  excludedFilenamePatterns="-e UserProfile.json,*LRspec.json"
+  excludedFilenamePatterns="-e UserProfile.json"
 fi
 
 root_dir=$(realpath $(dirname ${0})/..)
