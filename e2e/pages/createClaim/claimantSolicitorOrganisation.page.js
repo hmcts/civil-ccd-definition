45b95d9cdcb8c33f666b3e9dee2e1e07839aab83
Doug Rice <douglasrice@yahoo.com>
2021-11-08 18:38:44 +0000

    Fix test

--- e2e/pages/createClaim/claimantSolicitorOrganisation.page.js
+++ e2e/pages/createClaim/claimantSolicitorOrganisation.page.js
@@ -12,7 +12,6 @@ module.exports = {
     await I.runAccessibilityTest();
     I.fillField(this.fields.orgPolicyReference, 'Claimant policy reference');
     I.waitForElement(this.fields.searchText);
-    I.fillField(this.fields.searchText, 'Civil');
     await I.clickContinue();
   }
 };
