00e97ac752c03e8d1b3b6187da25f770147ef9c0
Alsahid Simms <simms1993@hotmail.co.uk>
2021-12-23 23:19:53 +0000

    Updated tests

--- e2e/fragments/litigationFriend.js
+++ e2e/fragments/litigationFriend.js
@@ -7,7 +7,7 @@ module.exports = {
     return {
       litigationFriendName: '#genericLitigationFriend_fullName',
       litigantInFriendDifferentAddress: {
-        id: 'LitigationFriend_hasSameAddressAsLitigant',
+        id: 'SelectALitigationFriend_hasSameAddressAsLitigant',
         options: {
           yes: 'Yes',
           no: 'No'
@@ -18,20 +18,20 @@ module.exports = {
     };
   },
 
-  async enterLitigantFriendWithDifferentAddressToLitigant(partyType, address, file) {
-    I.fillField(this.fields(partyType).litigationFriendName, 'John Smith');
+  async enterLitigantFriendWithDifferentAddressToLitigant(address, file) {
+    I.fillField(litigationFriendName, 'John Smith');
 
-    await within(this.fields(partyType).litigantInFriendDifferentAddress.id, () => {
-      I.click(this.fields(partyType).litigantInFriendDifferentAddress.options.no);
+    await within(litigantInFriendDifferentAddress.id, () => {
+      I.click(litigantInFriendDifferentAddress.options.no);
     });
 
-    await within(this.fields(partyType).litigantInFriendAddress, () => {
+    await within(litigantInFriendAddress, () => {
       postcodeLookup.enterAddressManually(address);
     });
 
     await I.addAnotherElementToCollection();
-    I.waitForElement(this.fields(partyType).certificateOfSuitability);
-    I.attachFile(this.fields(partyType).certificateOfSuitability, file);
+    I.waitForElement(certificateOfSuitability);
+    I.attachFile(certificateOfSuitability, file);
     await I.waitForInvisible(locate('.error-message').withText('Uploading...'));
   }
 };
