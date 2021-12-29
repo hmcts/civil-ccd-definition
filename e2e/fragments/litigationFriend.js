2250525d6f58c0ddf56d8ed39492e9eaa07739a2
Alsahid Simms <simms1993@hotmail.co.uk>
2021-12-29 12:34:46 +0000

    test- updatefield

--- e2e/fragments/litigationFriend.js
+++ e2e/fragments/litigationFriend.js
@@ -3,35 +3,35 @@ const postcodeLookup = require('./addressPostcodeLookup');
 
 module.exports = {
 
-  fields: function (partyType) {
+  fields: function () {
     return {
-      litigationFriendName: `#${partyType}genericLitigationFriend_fullName`,
+      litigationFriendName: '#genericLitigationFriend_fullName',
       litigantInFriendDifferentAddress: {
-        id: `#${partyType}LitigationFriend_hasSameAddressAsLitigant`,
+        id: '#genericLitigationFriend_hasSameAddressAsLitigant',
         options: {
           yes: 'Yes',
           no: 'No'
         }
       },
-      litigantInFriendAddress: `#${partyType}LitigationFriend_primaryAddress_primaryAddress`,
-      certificateOfSuitability: `#${partyType}LitigationFriend_certificateOfSuitability_0_document`
+      litigantInFriendAddress: '#genericLitigationFriend_primaryAddress_primaryAddress',
+      certificateOfSuitability: '#genericLitigationFriend_certificateOfSuitability_0_document'
     };
   },
 
   async enterLitigantFriendWithDifferentAddressToLitigant(partyType, address, file) {
-    I.fillField(this.fields(partyType).litigationFriendName, 'John Smith');
+    I.fillField(this.fields.litigationFriendName, 'John Smith');
 
-    await within(this.fields(partyType).litigantInFriendDifferentAddress.id, () => {
-      I.click(this.fields(partyType).litigantInFriendDifferentAddress.options.no);
+    await within(this.fields.litigantInFriendDifferentAddress.id, () => {
+      I.click(this.fields.litigantInFriendDifferentAddress.options.no);
     });
 
-    await within(this.fields(partyType).litigantInFriendAddress, () => {
+    await within(this.fields.litigantInFriendAddress, () => {
       postcodeLookup.enterAddressManually(address);
     });
 
     await I.addAnotherElementToCollection();
-    I.waitForElement(this.fields(partyType).certificateOfSuitability);
-    I.attachFile(this.fields(partyType).certificateOfSuitability, file);
+    I.waitForElement(this.fields.certificateOfSuitability);
+    I.attachFile(this.fields.certificateOfSuitability, file);
     await I.waitForInvisible(locate('.error-message').withText('Uploading...'));
   }
 };
