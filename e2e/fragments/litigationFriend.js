e9c8c49fa234fdae6f076dbf8d4f21be291ea643
Alsahid Simms <simms1993@hotmail.co.uk>
2021-12-29 11:37:04 +0000

    removed brckets

--- e2e/fragments/litigationFriend.js
+++ e2e/fragments/litigationFriend.js
@@ -19,19 +19,19 @@ module.exports = {
   },
 
   async enterLitigantFriendWithDifferentAddressToLitigant(address, file) {
-    I.fillField(this.fields().litigationFriendName, 'John Smith');
+    I.fillField(this.fields.litigationFriendName, 'John Smith');
 
-    await within(this.fields().litigantInFriendDifferentAddress.id, () => {
-      I.click(this.fields().litigantInFriendDifferentAddress.options.no);
+    await within(this.fields.litigantInFriendDifferentAddress.id, () => {
+      I.click(this.fields.litigantInFriendDifferentAddress.options.no);
     });
 
-    await within(this.fields().litigantInFriendAddress, () => {
+    await within(this.fields.litigantInFriendAddress, () => {
       postcodeLookup.enterAddressManually(address);
     });
 
     await I.addAnotherElementToCollection();
-    I.waitForElement(this.fields().certificateOfSuitability);
-    I.attachFile(this.fields().certificateOfSuitability, file);
+    I.waitForElement(this.fields.certificateOfSuitability);
+    I.attachFile(this.fields.certificateOfSuitability, file);
     await I.waitForInvisible(locate('.error-message').withText('Uploading...'));
   }
 };
