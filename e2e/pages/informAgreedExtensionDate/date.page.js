675a37ae548f6bb814f0aad6fc10234f2c03ff5e
Doug Rice <douglasrice@yahoo.com>
2022-01-10 12:52:41 +0000

    fix e2e test

--- e2e/pages/informAgreedExtensionDate/date.page.js
+++ e2e/pages/informAgreedExtensionDate/date.page.js
@@ -14,7 +14,7 @@ module.exports = {
 
   async enterExtensionDate(respondentSolicitorNumber) {
     await I.runAccessibilityTest();
-    await date.enterDate(this.fields(respondentSolicitorNumber).extensionDate.id, 50);
+    await date.enterDate(this.fields(respondentSolicitorNumber).extensionDate.id);
     await I.clickContinue();
   }
 };
