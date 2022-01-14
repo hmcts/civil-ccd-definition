e270cbecdc53a95a8917bad8ba6dd6ca6e94f692
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2022-01-14 14:51:56 +0000

    Make an application Journey - Initial commit

---
+++ e2e/pages/generalApplication/applicationDetails.page.js
@@ -0,0 +1,32 @@
+const {I} = inject();
+
+const servedDocuments = require('../../fragments/servedDocument');
+
+module.exports = {
+
+  fields: {
+    generalAppDetailsOfOrder: '#generalAppDetailsOfOrder',
+    generalAppReasonsOfOrder: '#generalAppReasonsOfOrder',
+    consentAgreementCheckBox: '#generalAppStatementOfTruthConsent-ConsentAgreementCheckBox',
+    reasonsForUrgency: '#generalAppUrgencyRequirement_reasonsForUrgency',
+    statementOfTruth_name: '#generalAppUIStatementOfTruth_name',
+    statementOfTruth_role: '#generalAppUIStatementOfTruth_role',
+    supportingEvidenceDocumentFiles: {
+      options: [
+        '#evidenceDocument'
+      ]
+    }
+  },
+
+  async enterApplicationDetails(file) {
+    I.waitForElement(this.fields.generalAppDetailsOfOrder);
+    await I.fillField(this.fields.generalAppDetailsOfOrder, 'test test test');
+    await I.fillField(this.fields.generalAppReasonsOfOrder, 'test test test');
+    await I.click(this.fields.consentAgreementCheckBox);
+    await I.fillField(this.fields.statementOfTruth_name, 'John Smith');
+    await I.fillField(this.fields.statementOfTruth_role, 'Solicitor');
+    await servedDocuments.upload(file, this.fields.supportingEvidenceDocumentFiles.options);
+    await I.clickContinue();
+  }
+};
+
