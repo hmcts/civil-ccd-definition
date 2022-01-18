e1e5ed39b329e5fb08ff3015c4f1bba093faca52
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2022-01-18 11:47:57 +0000

    Civ 736 (#381)
    * Make an application Journey - Initial commit
    
    * Make an application Journey - Initial commit
    
    * Added HearingDetails and PBA Number page objects
    
    * localhost config.js
    
    * localhost config.js changes and removed duplicates
    
    * code tidy up
    
    * refactor and tidy up
    
    Co-authored-by: Madhan Mahadevan <madhan.mahadevan@hmcts.net>

---
+++ e2e/pages/generalApplication/applicationType.page.js
@@ -0,0 +1,29 @@
+const {I} = inject();
+
+module.exports = {
+
+  fields: {
+    applicationType: {
+      id: '#generalAppType_types',
+      options: {
+        strikeOut: 'Strike Out',
+        summaryJudgement: 'Summary Judgement',
+        stayTheClaim: 'Stay the claim',
+        extendTime: 'Extend Time'
+      }
+    },
+  },
+
+  async selectApplicationType(applicationType) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (!this.fields.applicationType.options.hasOwnProperty(applicationType)) {
+      throw new Error(`Application type: ${applicationType} does not exist`);
+    }
+
+    I.waitForElement(this.fields.applicationType.id);
+    await within(this.fields.applicationType.id, () => {
+      I.click(this.fields.applicationType.options[applicationType]);
+    });
+    await I.clickContinue();
+  }
+};
---
+++ e2e/tests/gaSpec/generalApplication_test.js
@@ -0,0 +1,20 @@
+const config = require('../../config.js');
+const {waitForFinishedBusinessProcess} = require('../../api/testingSupport');
+const caseEventMessage = eventName => `Case ${caseNumber} has been updated with event: ${eventName}`;
+const caseId = () => `${caseNumber.split('-').join('').replace(/#/, '')}`;
+
+let caseNumber;
+
+Feature('General Application creation @ga-e2e-tests');
+
+Scenario('Applicant solicitor creates Strike out general application @ga', async ({I}) => {
+  await I.login(config.applicantSolicitorUser);
+  await I.createCase();
+  caseNumber = await I.grabCaseNumber();
+  await I.see(`Case ${caseNumber} has been created.`);
+  await I.makeAnApplication('strikeOut');
+  await I.see(caseNumber);
+  await waitForFinishedBusinessProcess(caseId());
+  await I.click('Close and Return to case details');
+  await I.see(caseEventMessage('Make an application'));
+}).retry(2);
