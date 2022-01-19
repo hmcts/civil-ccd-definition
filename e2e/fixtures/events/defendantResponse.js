262717d346806a77dc2f667d493b61b1b52cdbaa
jaberelfasi <jaberelfasi@gmail.com>
2022-01-19 13:12:35 +0000

    Fixes end to end duplicate key issue.

--- e2e/fixtures/events/defendantResponse.js
+++ e2e/fixtures/events/defendantResponse.js
@@ -25,20 +25,10 @@ module.exports = {
         primaryAddress: buildAddress('respondent'),
         partyName: 'Sir John Doe',
         partyTypeDisplayValue: 'Individual',
-      }
-    },
-    RespondentResponseType: {
-      respondent1ClaimResponseType: 'FULL_DEFENCE'
-    },
-    SolicitorReferences: {
+      },
       solicitorReferences: {
         applicantSolicitor1Reference: 'Applicant reference',
         respondentSolicitor1Reference: 'Respondent reference'
-      }
-    },
-    Upload: {
-      respondent1ClaimResponseDocument: {
-        file: document('claimResponse.pdf')
       }
     },
     FileDirectionsQuestionnaire: {
