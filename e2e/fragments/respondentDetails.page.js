4dc12eaae090fae5699171405030d3b44811828c
Doug Rice <douglasrice@yahoo.com>
2022-01-25 18:42:11 +0000

    modify respondentDetails test data to cater for 1v2

--- e2e/fragments/respondentDetails.page.js
+++ e2e/fragments/respondentDetails.page.js
@@ -2,16 +2,22 @@ const {I} = inject();
 
 module.exports = {
 
-  fields: (respondentNumber = '') => {
-    return {
-      respondentDetails: `#respondent${respondentNumber}Details`
-    }
+  fields:  {
+    respondent1Details: '#respondent1Details',
+    respondent2Details: '#respondent2Details',
   },
 
-  async verifyDetails(respondentNumber) {
-    I.waitForElement(this.fields(respondentNumber).respondentDetails);
+  async verifyDetails(twoDefendants) {
+    I.waitForElement(this.fields.respondent1Details);
+    if (twoDefendants) {
+      I.waitForElement(this.fields.respondent2Details);
+    }
     await I.runAccessibilityTest();
     await I.see('Example respondent1 company');
+
+    if (twoDefendants) {
+      await I.see('Example respondent2 company');
+    }
 
     await I.clickContinue();
   }
