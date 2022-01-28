28c3f808106bc71dcd84b9dec117533a7ec1e4d4
sherlynkhaw <sherlynkhaw@gmail.com>
2022-01-28 01:20:21 +0000

    feat/CMC-1849 CMC-1880 Refactored E2Es to include MP tests, API tests include 1v1 scenario (#435)
    * Refactored existing e2es and added multiparty tests
    
    * Fixed linting issues
    
    * Adjusted e2e scenario wording
    
    * Restored multipartyTestsEnabled flags in e2e steps
    
    * Added another multipartyTestsEnabled flag
    
    * Removed multipartyTestsEnabled flag and moved responseIntentionPage.selectResponseIntention to before confirmDetailsPage.confirmReference
    
    * Fixed linting issues
    
    * Reverted number of days after extension date
    
    * Added more multipartyTestsEnabled flags
    
    * Fixed defenceSteps input
    
    * Trigger missed build
    
    * Updated acknowledge claim page objects
    
    * Fixed linting issues
    
    * 1v1 works for mp, 1v2 journeys are WIP
    
    * fix linting
    
    * Fixed confirm details page interaction
    
    * modify respondentDetails test data to cater for 1v2
    
    * Removed multipartyTestsEnabled flag
    
    * removed multipartyTestsEnabled flag and comments
    
    * Added 1v2 different solicitor scenario
    
    * removed unnecessary line
    
    * split tests into path-to-live and nightly
    
    * Updated 1v2 diff scenario to use organisation 3. Updated correct assigning of permissions in 1v2 same solicitor scenario. Updated solicitor 2 references field selector.
    
    * Updated e2es to support new solicitor 2 reference input
    
    * Added api test workaround
    
    Co-authored-by: GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
    Co-authored-by: Doug Rice <douglasrice@yahoo.com>
    Co-authored-by: akriti-hmcts <90684819+akriti-hmcts@users.noreply.github.com>

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,5 +1,9 @@
 exports.config = {
-  tests: './bin/*_test.js',
+  tests: [
+    './e2e/tests/*_test.js',
+    './e2e/tests/api_multiparty/*_test.js',
+    './e2e/tests/multiparty/*_test.js'
+  ],
   output: './output',
   helpers: {
     Puppeteer: {
