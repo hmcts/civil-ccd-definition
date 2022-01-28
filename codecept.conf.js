c234c289689587be9236cbbf16b1728d27de4a8a
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2022-01-28 09:55:16 +0000

    Checking if api tests pass ack claim

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,8 +1,6 @@
 exports.config = {
   tests: [
-    './e2e/tests/*_test.js',
     './e2e/tests/api_multiparty/*_test.js',
-    './e2e/tests/multiparty/*_test.js'
   ],
   output: './output',
   helpers: {
