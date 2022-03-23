9add1736d4186886c957fd2440e054989a6a4074
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2022-03-23 15:32:09 +0000

    Update

--- e2e/tests/rpaHandoff_test.js
+++ e2e/tests/rpaHandoff_test.js
@@ -1,102 +1,82 @@
 const config = require('../config.js');
 const {waitForFinishedBusinessProcess, assignCaseToDefendant} = require('../api/testingSupport');
 
-const caseId = () => `${caseNumber.split('-').join('').replace(/#/, '')}`;
-let caseNumber;
+const mpScenario = 'ONE_V_ONE';
 
 
 Feature('RPA handoff points tests @rpa-handoff-tests');
 
-Scenario('Take claim offline', async ({I}) => {
-  await createCaseUpUntilNotifyClaimDetails(I);
+Scenario('Take claim offline', async ({I, api}) => {
+  await createCaseUpUntilNotifyClaimDetails(I, api);
 
-  await I.login(config.defendantSolicitorUser);
-  await I.navigateToCaseDetails(caseNumber);
-  await I.acknowledgeClaim('fullDefence');
-  await I.informAgreedExtensionDate();
+  await api.acknowledgeClaim(config.defendantSolicitorUser, mpScenario);
+  await api.informAgreedExtension(config.defendantSolicitorUser, mpScenario);
 
   await I.login(config.adminUser);
-  await I.navigateToCaseDetails(caseNumber);
+  await I.navigateToCaseDetails(api.caseId);
   await I.caseProceedsInCaseman();
   await I.assertNoEventsAvailable();
   await I.signOut();
 }).retry(3);
 
-Scenario('Defendant representativeRegistered false - Claimant Litigant In Person', async ({I}) => {
-  const claimant1 = {
-    litigantInPerson: true
-  };
-  const respondent1 = {
-    represented: true,
-    representativeRegistered: false,
-    representativeOrgNumber: 2
-  };
-
-  const shouldStayOnline = false;
+Scenario('Defendant representativeRegistered false - Claimant Litigant In Person', async ({I, api}) => {
+  await api.createClaimWithRespondentLitigantInPerson(config.applicantSolicitorUser);
 
   await I.login(config.applicantSolicitorUser);
-  await I.createCase(claimant1, null , respondent1, null, shouldStayOnline);
-  caseNumber = await I.grabCaseNumber();
-
-  await waitForFinishedBusinessProcess(caseId());
-  await I.navigateToCaseDetails(caseNumber);
+  await I.navigateToCaseDetails(api.caseId);
   await I.assertNoEventsAvailable();
   await I.signOut();
 }).retry(3);
 
-Scenario('Defendant - Defend part of Claim', async ({I}) => {
-  await createCaseUpUntilNotifyClaimDetails(I);
-  await defendantAcknowledgeAndRespondToClaim(I, 'partDefence', 'partAdmission');
+Scenario('Defendant - Defend part of Claim', async ({I, api}) => {
+  await createCaseUpUntilNotifyClaimDetails(I, api);
+  await defendantAcknowledgeAndRespondToClaim(I, api, 'partDefence', 'partAdmission');
 
-  await waitForFinishedBusinessProcess(caseId());
-  await I.navigateToCaseDetails(caseNumber);
+  await I.navigateToCaseDetails(api.caseId);
   await I.assertNoEventsAvailable();
   await I.signOut();
 }).retry(3);
 
-Scenario('Defendant - Defends, Claimant decides not to proceed', async ({I}) => {
-  await createCaseUpUntilNotifyClaimDetails(I);
-  await defendantAcknowledgeAndRespondToClaim(I, 'fullDefence', 'fullDefence');
+Scenario('Defendant - Defends, Claimant decides not to proceed', async ({I, api}) => {
+  await createCaseUpUntilNotifyClaimDetails(I, api);
+  await defendantAcknowledgeAndRespondToClaimFullDefence(I, api);
 
   await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(caseNumber);
+  await I.navigateToCaseDetails(api.caseId);
   await I.respondToDefenceDropClaim();
   await I.assertNoEventsAvailable();
   await I.signOut();
 }).retry(3);
 
-Scenario('Defendant - Defends, Claimant decides to proceed', async ({I}) => {
-  await createCaseUpUntilNotifyClaimDetails(I);
-  await defendantAcknowledgeAndRespondToClaim(I, 'fullDefence', 'fullDefence');
+Scenario('Defendant - Defends, Claimant decides to proceed', async ({I, api}) => {
+  await createCaseUpUntilNotifyClaimDetails(I, api);
+  await defendantAcknowledgeAndRespondToClaimFullDefence(I, api);
+  await api.claimantResponse(config.applicantSolicitorUser);
 
   await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(caseNumber);
-  await I.respondToDefence();
+  await I.navigateToCaseDetails(api.caseId);
   await I.assertNoEventsAvailable();
   await I.signOut();
 }).retry(3);
 
-const createCaseUpUntilNotifyClaimDetails = async (I, shouldStayOnline = true) => {
-  const claimant1 = {
-    litigantInPerson: false
-  };
-  const respondent1 = {
-    represented: true,
-    representativeRegistered: true,
-    representativeOrgNumber: 2
-  };
-  await I.login(config.applicantSolicitorUser);
-  await I.createCase(claimant1, null , respondent1, null, shouldStayOnline);
-  caseNumber = await I.grabCaseNumber();
-  await I.notifyClaim();
-  await assignCaseToDefendant(caseId());
-  await I.notifyClaimDetails();
+const createCaseUpUntilNotifyClaimDetails = async (I, api) => {
+  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
+  await api.notifyClaim(config.applicantSolicitorUser);
+  await api.notifyClaimDetails(config.applicantSolicitorUser);
+  await assignCaseToDefendant(api.caseId);
 };
 
-const defendantAcknowledgeAndRespondToClaim = async (I, acknowledgeClaimResponse, respondToClaimResponse) => {
+const defendantAcknowledgeAndRespondToClaim = async (I, api, acknowledgeClaimResponse, respondToClaimResponse) => {
   await I.login(config.defendantSolicitorUser);
-  await I.navigateToCaseDetails(caseNumber);
+  await I.navigateToCaseDetails(api.caseId);
   await I.acknowledgeClaim(acknowledgeClaimResponse);
-  await I.informAgreedExtensionDate();
-  await I.respondToClaim(I.respondToClaim({defendant1Response: respondToClaimResponse}));
+  await api.informAgreedExtension(config.defendantSolicitorUser, mpScenario);
+  await I.respondToClaim({defendant1Response: respondToClaimResponse});
+  await waitForFinishedBusinessProcess(api.caseId);
+};
+
+const defendantAcknowledgeAndRespondToClaimFullDefence = async (I, api) => {
+  await api.acknowledgeClaim(config.defendantSolicitorUser, mpScenario);
+  await api.informAgreedExtension(config.defendantSolicitorUser, mpScenario);
+  await api.defendantResponse(config.defendantSolicitorUser, mpScenario);
 };
