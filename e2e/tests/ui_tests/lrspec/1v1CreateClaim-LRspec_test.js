72d7f8930dd44994e6bdf16783b61192fc6e8843
Zaki Shaik <zaki.shaik@hmcts.net>
2022-04-13 15:14:54 +0100

    to fix pipeline failures

--- e2e/tests/ui_tests/lrspec/1v1CreateClaim-LRspec_test.js
+++ e2e/tests/ui_tests/lrspec/1v1CreateClaim-LRspec_test.js
@@ -10,7 +10,7 @@ Feature('Claim creation 1v1 @e2e-tests-spec');
 Scenario('1v1 Applicant solicitor creates specified claim for fast track @create-claim-spec', async ({LRspec}) => {
   console.log('1v1 Applicant solicitor creates specified claim for fast track @create-claim-spec');
   await LRspec.login(config.applicantSolicitorUser);
-  await LRspec.createCaseSpecified('organisation', null , 'company', null  ,false, 19000);
+  await LRspec.createCaseSpecified('organisation', null , 'company', null , 19000);
   caseNumber = await LRspec.grabCaseNumber();
   await LRspec.see(`Case ${caseNumber} has been created.`);
 }).retry(3);
