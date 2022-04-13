310d07e9a628f66109385017f22fd22f37244348
Zaki Shaik <zaki.shaik@hmcts.net>
2022-04-13 16:13:15 +0100

    to fix pipeline failures

--- e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_test.js
+++ e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_test.js
@@ -11,7 +11,7 @@ Feature('1v2 Multi Party Claim creation 1v2 @e2e-tests-spec');
 Scenario('Applicant solicitor creates 1v2 specified claim both defendants same LR for small claims @create-claim-spec', async ({LRspec}) => {
   console.log('Applicant solicitor creates 1v2 specified claim both defendants Same LR for small claims @create-claim-spec');
   await LRspec.login(config.applicantSolicitorUser);
-  await LRspec.createCaseSpecified('organisation', null, 'company', 'company',false, 1000);
+  await LRspec.createCaseSpecified('organisation', null, 'company', 'company', 1000);
   caseNumber = await LRspec.grabCaseNumber();
   await LRspec.see(`Case ${caseNumber} has been created.`);
 }).retry(3);
