f0827e06f14b2a514bd41104783d3f00d1f3b4df
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2022-04-13 11:42:32 +0100

    feat/CIV-1688 full scenario

--- e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
@@ -4,14 +4,12 @@ const config = require('../../../config.js');
 
 Feature('CCD 1v1 API test @api-spec @api-spec-1v1');
 
-Scenario('Create claim spec', async ({I, api_spec}) => {
+/**
+ * creates a claim with LR, responds as the defendant with full defence, and last the applicant responds
+ * to defence.
+ */
+Scenario('Create, respond and applicant respond 1v1 full defence', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
-});
-
-Scenario('Defendant response', async ({I, api_spec}) => {
   await api_spec.defendantResponse(config.defendantSolicitorUser);
-});
-
-Scenario('Claimant response', async ({I, api_spec}) => {
   await api_spec.claimantResponse(config.applicantSolicitorUser);
 });
