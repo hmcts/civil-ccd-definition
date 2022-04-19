12026b06193ee389600c64b815cd381b4c09c157
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2022-04-19 16:56:39 +0100

    update LRspec configs

--- codecept.conf.js
+++ codecept.conf.js
@@ -29,8 +29,9 @@ exports.config = {
   },
   include: {
     I: './e2e/steps_file.js',
+    LRspec: './e2e/steps_file_LRspec.js',
     api: './e2e/api/steps.js',
-    LRspec: './e2e/api/steps_LRspec.js'
+    api_spec: './e2e/api/steps_LRspec.js'
   },
   plugins: {
     autoDelay: {
