6c7f241df07e8376589e10e253fa28fce9e3f540
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2022-04-20 09:16:44 +0100

    Update demo

--- e2e/api/steps_LRspec.js
+++ e2e/api/steps_LRspec.js
@@ -210,7 +210,7 @@ const deleteCaseFields = (...caseFields) => {
 const assertCorrectEventsAreAvailableToUser = async (user, state) => {
   console.log(`Asserting user ${user.type} in env ${config.runningEnv} has correct permissions`);
   const caseForDisplay = await apiRequest.fetchCaseForDisplay(user, caseId);
-  if (config.runningEnv == 'preview') {
+  if (['preview', 'demo'].includes(config.runningEnv)) {
     expect(caseForDisplay.triggers).to.deep.include.members(expectedEvents[user.type][state]);
   } else {
     expect(caseForDisplay.triggers).to.deep.equalInAnyOrder(expectedEvents[user.type][state]);
