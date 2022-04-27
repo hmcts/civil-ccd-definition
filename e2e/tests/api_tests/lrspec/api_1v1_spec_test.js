21366c3d2d1f59b4e66188ee6b7fecca90b6937c
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2022-04-27 14:22:10 +0100

    fix for CIV-1773, changes in tests

--- e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
@@ -4,6 +4,10 @@ const config = require('../../../config.js');
 
 Feature('CCD 1v1 API test @api-spec @api-spec-1v1');
 
-Scenario('Create claim spec', async ({I, api_spec}) => {
+Scenario.skip('Create claim spec', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
+});
+
+Scenario('Inform agreed extension date', async ({I, api_spec}) => {
+  await api_spec.informAgreedExtensionDate(config.applicantSolicitorUser);
 });
