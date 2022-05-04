1ce2c98960c8a08f291c3c5a6913e836c69c47cc
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2022-05-04 14:39:11 +0100

    enable all tests

--- e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
@@ -4,7 +4,7 @@ const config = require('../../../config.js');
 
 Feature('CCD 1v1 API test @api-spec @api-spec-1v1');
 
-Scenario.skip('Create claim spec', async ({I, api_spec}) => {
+Scenario('Create claim spec', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
 });
 
