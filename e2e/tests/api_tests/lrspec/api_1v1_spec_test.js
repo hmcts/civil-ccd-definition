8de600353cdb8dc829f36be15a445771f69a73e7
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-05-16 16:47:37 +0100

    Separate 2v1 Small Claim Testing Scenarios from 1v1 test file

--- e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
@@ -33,31 +33,3 @@ Scenario.skip('1v1 full defence claimant and defendant response', async ({I, api
   await api_spec.defendantResponse(config.defendantSolicitorUser);
   await api_spec.claimantResponse(config.applicantSolicitorUser);
 });
-
-Scenario('2v1 full defence', async ({I, api_spec_small}) => {
-  await api_spec_small.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'TWO_V_ONE');
-  await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'TWO_V_ONE');
-});
-
-Scenario('2v1 full defence and defendant response', async ({I, api_spec_small}) => {
-  await api_spec_small.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'TWO_V_ONE');
-  await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'TWO_V_ONE');
-  await api_spec_small.claimantResponse(config.applicantSolicitorUser, 'TWO_V_ONE');
-});
-
-Scenario('2v1 counter claim', async ({I, api_spec_small}) => {
-  await api_spec_small.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'TWO_V_ONE');
-  await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'COUNTER_CLAIM', 'TWO_V_ONE');
-});
-
-Scenario('2v1 full admission', async ({I, api_spec_small}) => {
-  await api_spec_small.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'TWO_V_ONE');
-  await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'FULL_ADMISSION', 'TWO_V_ONE');
-});
-
-Scenario('2v1 part admission', async ({I, api_spec_small}) => {
-  await api_spec_small.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'TWO_V_ONE');
-  await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'PART_ADMISSION', 'TWO_V_ONE');
-});
-
-
