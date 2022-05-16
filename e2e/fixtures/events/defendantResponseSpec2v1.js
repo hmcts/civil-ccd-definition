3aed1a655947a433fa042ea337cb667c45f95ab1
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-05-16 14:07:49 +0100

    Remove Comment Lines

--- e2e/fixtures/events/defendantResponseSpec2v1.js
+++ e2e/fixtures/events/defendantResponseSpec2v1.js
@@ -13,57 +13,7 @@ module.exports = {
         },
         RespondentResponseTypeSpec: {
           respondent1ClaimResponseTypeForSpec: 'FULL_DEFENCE'
-        },
-        // defenceRoute: {
-        //   defenceRouteRequired: 'DISPUTES_THE_CLAIM'
-        // },
-      //   Upload: {
-      //     detailsOfWhyDoesYouDisputeTheClaim: 'details'
-      //   },
-      //   HowToAddTimeline: {
-      //     specClaimResponseTimelineList: 'MANUAL'
-      //   },
-      //   FileDirectionsQuestionnaire: {
-      //     respondent1DQFileDirectionsQuestionnaire: {
-      //       explainedToClient: ['CONFIRM'],
-      //       oneMonthStayRequested: 'Yes',
-      //       reactionProtocolCompliedWith: 'Yes'
-      //     }
-      //   },
-      //   DisclosureOfElectronicDocumentsLRspec: {
-      //     specRespondent1DQDisclosureOfElectronicDocuments: {
-      //       reachedAgreement: 'Yes'
-      //     }
-      //   },
-      //   Experts: {
-      //     respondent1DQExperts: {
-      //       expertRequired: 'No'
-      //     },
-      //   },
-      //   Witnesses: {
-      //     respondent1DQWitnesses: {
-      //       witnessesToAppear: 'No'
-      //     }
-      //   },
-      //   Language: {
-      //     respondent1DQLanguage: {
-      //       evidence: 'ENGLISH',
-      //       court: 'ENGLISH',
-      //       documents: 'ENGLISH'
-      //     }
-      //   },
-      //   HearingLRspec: {
-      //     respondent1DQHearing: {
-      //       hearingLength: 'ONE_DAY',
-      //       unavailableDatesRequired: 'No'
-      //     }
-      //   },
-      //   StatementOfTruth: {
-      //     uiStatementOfTruth: {
-      //       name: 'name',
-      //       role: 'role'
-      //     }
-      //   }
+        }
       },
     };
 
