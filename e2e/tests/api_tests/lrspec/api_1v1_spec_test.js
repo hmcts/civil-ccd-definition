bcccb3906f9214bcf26ed1d6c88acee93a37bb2b
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-05-16 14:25:23 +0100

    Update Test

--- e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
@@ -39,7 +39,7 @@ Scenario('2v1 full defence', async ({I, api_spec}) => {
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'TWO_V_ONE');
 });
 
-Scenario.only('2v1 full defence and defendant response', async ({I, api_spec}) => {
+Scenario('2v1 full defence and defendant response', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'TWO_V_ONE');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'TWO_V_ONE');
   await api_spec.claimantResponse(config.applicantSolicitorUser, 'TWO_V_ONE');
