5bdc256c34e084afcd9c3034f562cca7f7ceee3e
zakishaik <45819863+zakishaik@users.noreply.github.com>
2022-05-17 14:22:51 +0100

    Merge 1825_Automation (#810)

--- e2e/pages/respondToClaimLRspec/hearingSmallClaimsLRspec.page.js
+++ e2e/pages/respondToClaimLRspec/hearingSmallClaimsLRspec.page.js
@@ -1,38 +1,43 @@
 const {I} = inject();
-
 module.exports = {
-  fields: {
-    hearingType: {
-      id: '#respondent1DQHearingSmallClaim_unavailableDatesRequired_radio',
-      options: {
-        yes: 'Yes',
-        no: 'No',
+  fields: function(mpScenario) {
+    switch (mpScenario) {
+      case 'ClaimantResponse': {
+        return {
+          hearingType: {
+            id: '#applicant1DQSmallClaimHearing_unavailableDatesRequired_radio',
+            options: {
+              yes: 'Yes',
+              no: 'No'
+            }
+          },
+        };
       }
-    },
-    interpreterType: {
-          id: '#SmallClaimHearingInterpreterRequired_radio',
-          options: {
-            yes: 'Yes',
-            no: 'No',
+
+      case 'DefendantResponse':
+      default: {
+        return {
+          hearingType: {
+            id: '#respondent1DQHearingSmallClaim_unavailableDatesRequired_radio',
+            options: {
+              yes: 'Yes',
+              no: 'No'
+            }
+
           }
-     }
+        };
+      }
+    }
   },
 
-  async selectHearing(responseType) {
+ async selectHearing(mpScenario) {
 
-    I.waitForElement(this.fields.hearingType.id);
+    I.waitForElement(this.fields(mpScenario).hearingType.id);
     await I.runAccessibilityTest();
-    await within(this.fields.hearingType.id, () => {
-    I.click(this.fields.hearingType.options[responseType]);
-    I.click(this.fields.interpreterType.options[responseType]);
+    await within(this.fields(mpScenario).hearingType.id, () => {
+    I.click(this.fields(mpScenario).hearingType.options.no);
     });
 
-    I.waitForElement(this.fields.interpreterType.id);
-    await I.runAccessibilityTest();
-    await within(this.fields.interpreterType.id, () => {
-    I.click(this.fields.interpreterType.options[responseType]);
-    });
     await I.clickContinue();
   }
 };
-
