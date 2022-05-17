7ec0621f09fa724998e38ec5ec209cd32a72b705
Mark Drummond <mark.drummond@version1.com>
2022-05-17 16:34:09 +0100

    nonprod postfix, added "judgeprofile"

--- src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
@@ -22,7 +22,8 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
         new CcdRoleConfig("caseworker-civil-solicitor", "PUBLIC"),
         new CcdRoleConfig("caseworker-civil-staff", "PUBLIC"),
         new CcdRoleConfig("caseworker-civil-systemupdate", "PUBLIC"),
-        new CcdRoleConfig("caseworker-caa", "PUBLIC")
+        new CcdRoleConfig("caseworker-caa", "PUBLIC"),
+        new CcdRoleConfig("judge-profile", "PUBLIC")
 
     };
 
