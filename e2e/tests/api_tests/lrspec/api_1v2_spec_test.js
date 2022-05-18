2e4207524b03df5930fcaff9a302ccedf1ef6e00
Sampan Kumar <sampan.kumar@hmcts.net>
2022-05-18 09:52:40 +0100

    skipping spec tests

--- e2e/tests/api_tests/lrspec/api_1v2_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_1v2_spec_test.js
@@ -4,36 +4,36 @@ const config = require('../../../config.js');
 
 Feature('CCD 1v1 API test @api-spec @api-spec-1v2');
 
-Scenario('Create claim spec 1v2', async ({I, api_spec}) => {
+Scenario.skip('Create claim spec 1v2', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO');
 });
 
-Scenario('1v2 full admit', async ({I, api_spec}) => {
+Scenario.skip('1v2 full admit', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_ADMISSION', 'ONE_V_TWO');
 });
 
-Scenario('1v2 part admit', async ({I, api_spec}) => {
+Scenario.skip('1v2 part admit', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'PART_ADMISSION', 'ONE_V_TWO');
 });
 
-Scenario('1v2 counter claim', async ({I, api_spec}) => {
+Scenario.skip('1v2 counter claim', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'COUNTER_CLAIM', 'ONE_V_TWO');
 });
 
-Scenario('Inform agreed extension date', async ({I, api_spec}) => {
+Scenario.skip('Inform agreed extension date', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
   await api_spec.informAgreedExtensionDate(config.applicantSolicitorUser);
 });
 
-Scenario('1v2 full defence, defendant response', async ({I, api_spec}) => {
+Scenario.skip('1v2 full defence, defendant response', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
 });
 
-Scenario('1v2 full defence, claimant response', async ({I, api_spec}) => {
+Scenario.skip('1v2 full defence, claimant response', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
   await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
