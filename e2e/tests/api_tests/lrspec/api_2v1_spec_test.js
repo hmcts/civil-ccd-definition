45181646351848b59a3007e58b974ec50da34286
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-05-18 01:28:54 +0100

    Update for 2v1 Part

--- e2e/tests/api_tests/lrspec/api_2v1_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_2v1_spec_test.js
@@ -39,7 +39,7 @@ Scenario('2v1 small claim full defence and defendant response', async ({I, api_s
   await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'TWO_V_ONE');
 });
 
-Scenario.skip('2v1 small claim full defence and not proceed', async ({I, api_spec}) => {
+Scenario('2v1 small claim full defence and not proceed', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'TWO_V_ONE');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'TWO_V_ONE');
   await api_spec.claimantResponse(config.applicantSolicitorUser, 'NOT_PROCEED', 'TWO_V_ONE');
