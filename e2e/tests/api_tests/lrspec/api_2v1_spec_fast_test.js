0fe26bed57104667fb72010c93e37c3fc5cbb030
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-05-18 09:43:46 +0100

    Update Successful Cases

--- e2e/tests/api_tests/lrspec/api_2v1_spec_fast_test.js
+++ e2e/tests/api_tests/lrspec/api_2v1_spec_fast_test.js
@@ -39,7 +39,7 @@ Scenario('2v1 fast claim full defence and claimant response', async ({I, api_spe
   await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'TWO_V_ONE');
 });
 
-Scenario.skip('2v1 fast claim full defence and not proceed', async ({I, api_spec_fast}) => {
+Scenario('2v1 fast claim full defence and not proceed', async ({I, api_spec_fast}) => {
   await api_spec_fast.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'TWO_V_ONE');
   await api_spec_fast.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'TWO_V_ONE');
   await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'NOT_PROCEED', 'TWO_V_ONE');
