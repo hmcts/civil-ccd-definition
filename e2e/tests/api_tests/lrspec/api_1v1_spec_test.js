10e1dea0b3c28ddf61df847217d6cc414f3769b7
kamilewandowski <kamil.lewandowski@evoco.co.uk>
2022-05-19 13:25:06 +0100

    remove redundant skiped tests

--- e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_1v1_spec_test.js
@@ -28,8 +28,20 @@ Scenario('Inform agreed extension date', async ({I, api_spec}) => {
   await api_spec.informAgreedExtensionDate(config.applicantSolicitorUser);
 });
 
-Scenario.only('1v1 full defence claimant and defendant response', async ({I, api_spec}) => {
+Scenario('1v1 full defence claimant and defendant response', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
   await api_spec.defendantResponse(config.defendantSolicitorUser);
   await api_spec.claimantResponse(config.applicantSolicitorUser);
+});
+
+Scenario('1v1 full admit claimant and defendant response', async ({I, api_spec}) => {
+  await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'FULL_ADMISSION');
+  await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_ADMISSION');
+  await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_ADMISSION');
+});
+
+Scenario('1v1 part admit defence claimant and defendant response', async ({I, api_spec}) => {
+  await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'PART_ADMISSION');
+  await api_spec.defendantResponse(config.defendantSolicitorUser, 'PART_ADMISSION');
+  await api_spec.claimantResponse(config.applicantSolicitorUser, 'PART_ADMISSION');
 });
