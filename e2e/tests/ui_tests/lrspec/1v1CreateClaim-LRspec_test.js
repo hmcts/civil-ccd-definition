ef7aa859bd125e07d6ae0699547ca54c51563349
zakishaik <45819863+zakishaik@users.noreply.github.com>
2022-06-09 13:45:05 +0100

    Update 1v1CreateClaim-LRspec_test.js

--- e2e/tests/ui_tests/lrspec/1v1CreateClaim-LRspec_test.js
+++ e2e/tests/ui_tests/lrspec/1v1CreateClaim-LRspec_test.js
@@ -50,7 +50,7 @@ Scenario('1v1 Respond To Claim - Defendants solicitor rejects claim for defendan
 }).retry(3);
 
 
-Scenario('1v1 Claimant solicitor responds to defence - claimant Intention to proceed', async ({LRspec}) => {
+Scenario.skip('1v1 Claimant solicitor responds to defence - claimant Intention to proceed', async ({LRspec}) => {
   await LRspec.login(config.applicantSolicitorUser);
   await LRspec.respondToDefence({mpScenario: 'ONE_V_ONE', claimType: 'fast'});
   await LRspec.click('Sign out');
