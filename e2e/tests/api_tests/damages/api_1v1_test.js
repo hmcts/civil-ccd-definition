f8accbe1f593f1b37236d35186cd48db9991f9a2
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2022-06-09 14:17:54 +0100

    remove skip for lip test

--- e2e/tests/api_tests/damages/api_1v1_test.js
+++ e2e/tests/api_tests/damages/api_1v1_test.js
@@ -45,7 +45,7 @@ Scenario('Claimant response', async ({I, api}) => {
   await api.claimantResponse(config.applicantSolicitorUser, mpScenario);
 });
 
-Scenario.skip('Create claim where respondent is litigant in person', async ({I, api}) => {
+Scenario('Create claim where respondent is litigant in person', async ({I, api}) => {
   await api.createClaimWithRespondentLitigantInPerson(config.applicantSolicitorUser);
 });
 
