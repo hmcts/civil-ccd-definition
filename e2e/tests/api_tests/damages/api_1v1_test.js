e8f87f049a412367b5872a4996c8e3d84ae9e577
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2022-06-17 15:18:08 +0100

    testing

--- e2e/tests/api_tests/damages/api_1v1_test.js
+++ e2e/tests/api_tests/damages/api_1v1_test.js
@@ -36,34 +36,34 @@ Scenario('Acknowledge claim', async ({I, api}) => {
 Scenario('Inform agreed extension date', async ({I, api}) => {
   await api.informAgreedExtension(config.defendantSolicitorUser, mpScenario);
 });
-
-Scenario('Defendant response', async ({I, api}) => {
-  await api.defendantResponse(config.defendantSolicitorUser, mpScenario);
-});
-
-Scenario('Claimant response', async ({I, api}) => {
-  await api.claimantResponse(config.applicantSolicitorUser, mpScenario);
-});
-
-Scenario.skip('Create claim where respondent is litigant in person', async ({I, api}) => {
-  await api.createClaimWithRespondentLitigantInPerson(config.applicantSolicitorUser);
-});
-
-Scenario('Create claim where respondent solicitor is not registered in my hmcts', async ({I, api}) => {
-  await api.createClaimWithRespondentSolicitorFirmNotInMyHmcts(config.applicantSolicitorUser);
-});
-
-Scenario('Create claim and move it to caseman', async ({I, api}) => {
-  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
-  await api.moveCaseToCaseman(config.adminUser);
-});
-
-// This will be enabled when PAY-3817 issue of two minutes is fixed
-Scenario.skip('Resubmit claim after payment failure on PBA account ', async ({I, api}) => {
-  await api.createClaimWithFailingPBAAccount(config.applicantSolicitorUser);
-  await api.resubmitClaim(config.applicantSolicitorUser);
-});
-
-AfterSuite(async  ({api}) => {
-  await api.cleanUp();
-});
+//
+// Scenario('Defendant response', async ({I, api}) => {
+//   await api.defendantResponse(config.defendantSolicitorUser, mpScenario);
+// });
+//
+// Scenario('Claimant response', async ({I, api}) => {
+//   await api.claimantResponse(config.applicantSolicitorUser, mpScenario);
+// });
+//
+// Scenario.skip('Create claim where respondent is litigant in person', async ({I, api}) => {
+//   await api.createClaimWithRespondentLitigantInPerson(config.applicantSolicitorUser);
+// });
+//
+// Scenario('Create claim where respondent solicitor is not registered in my hmcts', async ({I, api}) => {
+//   await api.createClaimWithRespondentSolicitorFirmNotInMyHmcts(config.applicantSolicitorUser);
+// });
+//
+// Scenario('Create claim and move it to caseman', async ({I, api}) => {
+//   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
+//   await api.moveCaseToCaseman(config.adminUser);
+// });
+//
+// // This will be enabled when PAY-3817 issue of two minutes is fixed
+// Scenario.skip('Resubmit claim after payment failure on PBA account ', async ({I, api}) => {
+//   await api.createClaimWithFailingPBAAccount(config.applicantSolicitorUser);
+//   await api.resubmitClaim(config.applicantSolicitorUser);
+// });
+//
+// AfterSuite(async  ({api}) => {
+//   await api.cleanUp();
+// });
