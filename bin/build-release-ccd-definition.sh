9fbb5d6bd4ab90c614b88949aef049c1d967867b
Mark Drummond <mark.drummond@version1.com>
2022-06-29 13:16:08 +0100

    updated build-release-ccd-definition.sh

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -6,11 +6,11 @@ environment=${1:-prod}
 excludeNonProdFiles=${2:-true}
 
 if [ ${environment} == preview ]; then
-  excludedFilenamePatterns="-e *DJspec.json,*DJ.json,*-prod.json,*SDO-nonprod.json,*DJ-nonprod.json"
+  excludedFilenamePatterns="-e *DJspec.json,*DJ.json,*-prod.json,*DJ-SDO-nonprod.json,*DJ-nonprod.json"
 elif [ ${environment} == demo ]; then
   excludedFilenamePatterns="-e UserProfile.json,*-prod.json"
 elif [ ${excludeNonProdFiles} == true ]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*DJ.json,*DJspec.json,*SDO-nonprod.json,*DJ-nonprod.json"
+  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*DJ.json,*DJspec.json,*DJ-SDO-nonprod.json,*DJ-nonprod.json"
 else
   excludedFilenamePatterns="-e UserProfile.json,*GAspec.json,*-prod.json,*DJ.json,*DJspec.json"
 fi
