62915f288a3cdea7e6a29a80d64dee72b2254cc8
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2022-06-30 10:41:37 +0100

    Update report folder structure (#948)
    * update
    
    * update
    
    * Bumping chart version/ fixing aliases
    
    * update
    
    * Update report folder structure
    
    * Update
    
    * Update
    
    * Update
    
    * update
    
    Co-authored-by: Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
    Co-authored-by: Jenkins <nobody@nowhere>
    Co-authored-by: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
    (cherry picked from commit dbb813184e72483b414bdff472b0bc6e90f858aa)

--- e2e/config.js
+++ e2e/config.js
@@ -47,7 +47,7 @@ module.exports = {
     jurisdiction: 'CIVIL',
     caseType: 'CIVIL',
   },
-  TestOutputDir: process.env.E2E_OUTPUT_DIR || './output',
+  TestOutputDir: process.env.E2E_OUTPUT_DIR || 'test-results/functional',
   TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY === 'true',
   runningEnv: process.env.ENVIRONMENT,
   claimantSolicitorOrgId: process.env.ENVIRONMENT == 'demo' ? 'B04IXE4' : 'Q1KOKP2',
