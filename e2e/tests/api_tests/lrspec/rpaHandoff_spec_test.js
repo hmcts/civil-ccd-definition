f18a259d2417868edb7186b3a64e987658a3c141
jeejavr <jeejamol.vijayarajan@HMCTS.net>
2022-07-05 09:43:46 +0100

    fix lintUserInterfaceTests

--- e2e/tests/api_tests/lrspec/rpaHandoff_spec_test.js
+++ e2e/tests/api_tests/lrspec/rpaHandoff_spec_test.js
@@ -1,13 +1,8 @@
 const config = require('../../../config.js');
-const {unAssignAllUsers, assignCaseRoleToUser, addUserCaseMapping} = require('../../../api/caseRoleAssignmentHelper');
-const {waitForFinishedBusinessProcess} = require('../../../api/testingSupport');
-
-const caseId = () => `${caseNumber.split('-').join('').replace(/#/, '')}`;
-let caseNumber;
 
 Feature('RPA handoff points tests @rpa-handoff-tests-spec');
 
-Scenario('Defendant response - Full defence', async ({I, api_spec}) => {
+Scenario('Defendant response - Full defence', async ({api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
  /* await waitForFinishedBusinessProcess(caseId());
@@ -16,7 +11,7 @@ Scenario('Defendant response - Full defence', async ({I, api_spec}) => {
   await api_spec.signOut();*/
 }).retry(3);
 
-Scenario('Defendant response- Full admission', async ({I, api_spec}) => {
+Scenario('Defendant response- Full admission', async  ({api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_ADMISSION', 'ONE_V_TWO');
  /* await waitForFinishedBusinessProcess(caseId());
@@ -25,7 +20,7 @@ Scenario('Defendant response- Full admission', async ({I, api_spec}) => {
   await api_spec.signOut();*/
 }).retry(3);
 
-Scenario('Defendant response- Part admission', async ({I, api_spec}) => {
+Scenario('Defendant response- Part admission', async  ({api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'PART_ADMISSION', 'ONE_V_TWO');
 /*  await waitForFinishedBusinessProcess(caseId());
@@ -34,7 +29,7 @@ Scenario('Defendant response- Part admission', async ({I, api_spec}) => {
   await api_spec.signOut();*/
 }).retry(3);
 
-Scenario('Defendant response- Counter claim', async ({I, api_spec}) => {
+Scenario('Defendant response- Counter claim', async  ({api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'COUNTER_CLAIM', 'ONE_V_TWO');
   /*await waitForFinishedBusinessProcess(caseId());
