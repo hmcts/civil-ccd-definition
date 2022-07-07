eeab5eea5dcf5f1eb1aeeac39cc16cf2412362ed
Gulam-Hussain <gulam.hussain@HMCTS.NET>
2022-07-07 11:20:02 +0100

    test - DO NOT MERGE

--- e2e/tests/ui_tests/sdo/sdo_journey_test.js
+++ e2e/tests/ui_tests/sdo/sdo_journey_test.js
@@ -18,7 +18,7 @@ Scenario('Applicant solicitor creates claim @create-claim', async ({I}) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCase(claimant1, null, respondent1, null);
   caseNumber = await I.grabCaseNumber();
-  await I.see(`Case ${caseNumber} has been created.`);
+  // await I.see(`Case ${caseNumber} has been created.`);
 }).retry(3);
 
 Scenario('Judge initiate SDO with sum of damages and allocate small claims track', async ({I}) => {
