d3f08966fc9bef4aa3311c9b4335d1dd44499905
Ash Modi <ashish.modi@outlook.com>
2022-07-12 14:09:40 +0100

    Uncommented tests that need to be run

--- e2e/run-non-prod-features-flag-enabled-functional-tests.sh
+++ e2e/run-non-prod-features-flag-enabled-functional-tests.sh
@@ -5,6 +5,6 @@ echo "Run Non prod Functional tests with all feature flags enabled in ccd def fi
 
 dir=$(dirname ${0})
 
-#${dir}/run-spec-functional-tests.sh
-#${dir}/run-sdo-functional-tests.sh
+${dir}/run-spec-functional-tests.sh
+${dir}/run-sdo-functional-tests.sh
 ${dir}/run-dj-functional-tests.sh
