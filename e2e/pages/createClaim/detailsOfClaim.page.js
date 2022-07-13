6ac4bbbfa5ea5fafd182bcce99d55bca30626d57
Douglas Rice <douglas.rice@hmcts.net>
2022-07-13 19:33:08 +0100

    Fix test

--- e2e/pages/createClaim/detailsOfClaim.page.js
+++ e2e/pages/createClaim/detailsOfClaim.page.js
@@ -6,10 +6,10 @@ module.exports = {
     detailsOfClaim: '#detailsOfClaim'
   },
 
-  async enterDetailsOfClaim(details) {
+  async enterDetailsOfClaim() {
       I.waitForElement(this.fields.detailsOfClaim);
       await I.runAccessibilityTest();
-      I.fillField(this.fields.detailsOfClaim, details);
+      I.fillField(this.fields.detailsOfClaim, 'Test details of claim');
       await I.clickContinue();
   }
 };
