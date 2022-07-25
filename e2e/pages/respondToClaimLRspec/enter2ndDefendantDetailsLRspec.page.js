22477497935aa9080572892aa545df0b507d28a2
Zaki Shaik <zaki.shaik@hmcts.net>
2022-07-25 16:52:01 +0100

    2v1 diff sol automation

--- e2e/pages/respondToClaimLRspec/enter2ndDefendantDetailsLRspec.page.js
+++ e2e/pages/respondToClaimLRspec/enter2ndDefendantDetailsLRspec.page.js
@@ -12,7 +12,7 @@ module.exports = {
 
   },
 
- async confirmDetails(twoDefendants) {
+ async confirmDetails() {
 
     I.waitForElement(this.fields.solicitor1Reference.id);
     await I.runAccessibilityTest();
