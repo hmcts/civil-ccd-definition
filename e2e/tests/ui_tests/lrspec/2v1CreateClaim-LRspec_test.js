6589b7a149f051ddf5f6ec94186acc3df08b0ea2
Zaki Shaik <zaki.shaik@hmcts.net>
2022-07-26 14:42:10 +0100

    2v1 diff sol automation

--- e2e/tests/ui_tests/lrspec/2v1CreateClaim-LRspec_test.js
+++ e2e/tests/ui_tests/lrspec/2v1CreateClaim-LRspec_test.js
@@ -18,7 +18,7 @@ Scenario('Applicant solicitor creates 2v1 specified claim with 2 organisation vs
   //await LRspec.see(`Case ${caseNumber} has been created.`);
 }).retry(3);
 
-Scenario.skip('2v1 Respond To Claim - Defendants solicitor rejects claim for defendant', async ({LRspec}) => {
+Scenario('2v1 Respond To Claim - Defendants solicitor rejects claim for defendant', async ({LRspec}) => {
   console.log('2v1 Defendant solicitor reject the specified claim');
   await assignCaseToLRSpecDefendant(caseId());
   await LRspec.login(config.defendantSolicitorUser);
@@ -33,27 +33,8 @@ Scenario.skip('2v1 Respond To Claim - Defendants solicitor rejects claim for def
   await LRspec.click('Sign out');
 }).retry(3);
 
-Scenario.skip('2v1 Respond To Claim - Defendants solicitor Part Admit the claim and defendant wants to pay by repaymentPlan', async ({LRspec}) => {
-  await LRspec.login(config.defendantSolicitorUser);
-  await LRspec.respondToClaimPartAdmit({
-    defendant1Response: 'partAdmission',
-    claimType: 'fast',
-    defenceType: 'repaymentPlan'
-  });
-  // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
-  //await LRspec.see(caseEventMessage('Respond to claim'));
-  await LRspec.click('Sign out');
-}).retry(3);
-
-Scenario.skip('2v1 Respond To Claim - Defendants solicitor Admits the claim and defendant wants to pay by setDate', async ({LRspec}) => {
-  await LRspec.login(config.defendantSolicitorUser);
-  await LRspec.respondToClaimFullAdmit({
-    twoDefendants: false,
-    defendant1Response: 'fullAdmission',
-    claimType: 'fast',
-    defenceType: 'setDate'
-  });
-  // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
-  //await LRspec.see(caseEventMessage('Respond to claim'));
+Scenario('1v1 Claimant solicitor responds to defence - claimant Intention to proceed', async ({LRspec}) => {
+  await LRspec.login(config.applicantSolicitorUser);
+  await LRspec.respondToDefence({mpScenario: 'ONE_V_ONE', claimType: 'fast'});
   await LRspec.click('Sign out');
 }).retry(3);
