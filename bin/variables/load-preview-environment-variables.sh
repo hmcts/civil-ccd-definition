c4084aeb34b353eef5b02ef8748c1d37cae40856
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2022-08-09 08:45:41 +0100

    Update load-preview-environment-variables.sh

--- bin/variables/load-preview-environment-variables.sh
+++ bin/variables/load-preview-environment-variables.sh
@@ -10,9 +10,9 @@ echo 'export ENVIRONMENT=preview'
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
-echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://ccd-definition-store-civil-ccd-pr-${pr}.service.core-compute-preview.internal"
-echo "export CAMUNDA_BASE_URL=https://camunda-civil-ccd-pr-${pr}.service.core-compute-preview.internal"
+echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://civil-ccd-pr-1113-ccd-definition-store"
+echo "export CAMUNDA_BASE_URL=http://civil-ccd-pr-1113-camunda"
 
 # definition placeholders
-echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-ccd-pr-${pr}.service.core-compute-preview.internal"
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-ccd-pr-1113-java"
 echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-general-apps-ccd-definition-pr-${pr}.service.core-compute-preview.internal"
