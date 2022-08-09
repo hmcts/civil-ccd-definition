3b8603aa1971cb24df20d812fd290bb6547715d4
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2022-08-09 10:31:15 +0100

    update

--- e2e/config.js
+++ e2e/config.js
@@ -11,13 +11,13 @@ module.exports = {
     // ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
     // dmStore:process.env.DM_STORE_URL || 'http://dm-store-demo.service.core-compute-demo.internal',
     // idamApi: process.env.IDAM_API_URL || 'https://idam-api.demo.platform.hmcts.net',
-    // civilService: process.env.TEST_URL || 'http://civil-service-demo.service.core-compute-demo.internal'
+    // civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-demo.service.core-compute-demo.internal'
     manageCase: process.env.URL || 'http://localhost:3333',
     authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://localhost:4502',
     ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://localhost:4452',
     dmStore: process.env.DM_STORE_URL || 'http://dm-store:8080',
     idamApi: process.env.IDAM_API_URL || 'http://localhost:5000',
-    civilService: process.env.TEST_URL || 'http://localhost:4000',
+    civilService: process.env.CIVIL_SERVICE_URL || 'http://localhost:4000',
   },
   s2s: {
     microservice: 'civil_service',
