889e380ccbd218dfe26ae9edee2931b58fa30c91
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2022-08-23 13:28:45 +0100

    CIV-3263 rollback because it didn't solve anything

--- e2e/tests/ui_tests/sdo/sdo_journey_test.js
+++ e2e/tests/ui_tests/sdo/sdo_journey_test.js
@@ -1,5 +1,4 @@
 const config = require('../../../config.js');
-const {waitForFinishedBusinessProcess} = require('../../../api/testingSupport');
 
 const claimant1 = {
   litigantInPerson: true
@@ -11,17 +10,17 @@ const respondent1 = {
   representativeOrgNumber: 2
 };
 
-const caseId = () => `${caseNumber.split('-').join('').replace(/#/, '')}`;
-
-let caseNumber;
+// Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
+//let caseNumber;
 
 Feature('1v1 - Claim Journey and initiate SDO @e2e-sdo');
 
 Scenario('Applicant solicitor creates claim @create-claim', async ({I}) => {
   await I.login(config.applicantSolicitorUser);
   await I.createCase(claimant1, null, respondent1, null);
-  caseNumber = await I.grabCaseNumber();
-  await waitForFinishedBusinessProcess(caseId());
+  // Reinstate the lines below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
+  //caseNumber = await I.grabCaseNumber();
+  //await I.see(`Case ${caseNumber} has been created.`);
 }).retry(3);
 
 Scenario('Judge initiate SDO with sum of damages and allocate small claims track', async ({I}) => {
