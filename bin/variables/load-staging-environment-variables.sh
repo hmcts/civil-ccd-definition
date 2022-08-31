161ff915ebb339c0fd6c17a3aa4814c663baefc5
Sabah u Din Irfan <sabah.irfan@gmail.com>
2022-08-31 21:36:58 +0100

    Fix/fix master pipeline (#1208)

--- bin/variables/load-staging-environment-variables.sh
+++ bin/variables/load-staging-environment-variables.sh
@@ -13,4 +13,4 @@ echo "export CAMUNDA_BASE_URL=https://civil-ccd-camunda-staging-aat.service.core
 
 # definition placeholders
 echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-staging-java"
-echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-general-apps-ccd-staging.service.core-compute-aat.internal"
+echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-general-applications-staging-java"
