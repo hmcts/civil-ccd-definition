75d57ceb59a3b8f8e3b6109046d0cb09040a2e72
sampankumar <sampank@gmail.com>
2022-09-07 09:34:19 +0100

    Feat/civ 3263 b (#1230)
    * feat/CIV-3263 event to change state to judicial referral
    
    * feat/CIV-3263 I2P doesn't always send to offline
    
    * feat/CIV-3263 judicial referral open so that solicitors can read
    
    * feat/CIV-3263 judicial referral open so that solicitors can read
    
    * feat/CIV-3263 preparing api tests
    
    * feat/CIV-3263 tests fixed so they work with the new changes
    
    * feat/CIV-3263 tests fixed so they work with the new changes
    
    * CIV-3263 duplicated role/casestate key
    
    * CIV-3263 uncommented lines to ensure e2e tests wait until end of camunda process
    
    * CIV-3263 waiting for finished process before sdo
    
    * CIV-3263 waiting for finished process before sdo
    
    * CIV-3263 rollback because it didn't solve anything
    
    * Removing e2e-multiparty flag on 1v1 test
    
    As of now the tests are timing out on pr as they are exceeding 40 mins. So skipping 1v1 and keeping 1v2 test
    
    Co-authored-by: dtortolaV1 <dario.tortolanavarro@hmcts.net>
    Co-authored-by: cj1993 <chris.chandler@hmcts.net>
    Co-authored-by: mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>

--- e2e/tests/api_tests/lrspec/api_1v2_spec_fast_test.js
+++ e2e/tests/api_tests/lrspec/api_1v2_spec_fast_test.js
@@ -26,13 +26,15 @@ Scenario('1v2 different response no full defence', async ({I, api_spec_fast}) =>
 Scenario('1v2 full defence and claimant response', async ({I, api_spec_fast}) => {
   await api_spec_fast.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec_fast.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
-  await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
+  await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO',
+    'AWAITING_APPLICANT_INTENTION');
 });
 
 Scenario('1v2 fast claim full defence and not proceed', async ({I, api_spec_fast}) => {
   await api_spec_fast.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec_fast.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
-  await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'NOT_PROCEED', 'ONE_V_TWO');
+  await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'NOT_PROCEED', 'ONE_V_TWO',
+    'AWAITING_APPLICANT_INTENTION');
 });
 
 Scenario('1v2 fast claim fast claim full admit, defendant response', async ({I, api_spec_fast}) => {
@@ -48,11 +50,13 @@ Scenario('1v2 fast claim part admit, defendant response', async ({I, api_spec_fa
 Scenario('1v2 fast claim fast claim full admit, defendant and claimant response', async ({I, api_spec_fast}) => {
   await api_spec_fast.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec_fast.defendantResponse(config.defendantSolicitorUser, 'FULL_ADMISSION', 'ONE_V_TWO');
-  await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'FULL_ADMISSION', 'ONE_V_TWO');
+  await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'FULL_ADMISSION', 'ONE_V_TWO',
+    'AWAITING_APPLICANT_INTENTION');
 });
 
 Scenario('1v2 fast claim part admit, defendant and claimant response', async ({I, api_spec_fast}) => {
   await api_spec_fast.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec_fast.defendantResponse(config.defendantSolicitorUser, 'PART_ADMISSION', 'ONE_V_TWO');
-  await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'PART_ADMISSION', 'ONE_V_TWO');
+  await api_spec_fast.claimantResponse(config.applicantSolicitorUser, 'PART_ADMISSION', 'ONE_V_TWO',
+    'AWAITING_APPLICANT_INTENTION');
 });
