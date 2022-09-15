bb57c91743a9429802bf4c0258d5a2120730cdd6
douglasrice <douglasrice@yahoo.com>
2022-09-15 17:12:05 +0100

    Update chooseCourt.page.js

--- e2e/pages/createClaim/chooseCourt.page.js
+++ e2e/pages/createClaim/chooseCourt.page.js
@@ -17,7 +17,7 @@ module.exports = {
 
   async selectCourt() {
     let isCourtListEnabled = await checkCourtLocationDynamicListIsEnabled();
-    if (!isCourtListEnabled || config.runningEnv === "aat") {
+    if (!isCourtListEnabled || config.runningEnv === 'aat') {
       I.waitForElement(this.oldFields.courtLocation);
       await I.runAccessibilityTest();
       I.fillField(this.oldFields.courtLocation, '344');
