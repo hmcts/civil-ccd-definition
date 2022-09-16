18646c6d53cf4e9b6276844c4d834e9266a8bf1e
Astha Malviya <astha.malviya@HMCTS.NET>
2022-09-16 11:35:18 +0100

    remove respondentOrgRegistered field

--- e2e/pages/createClaim/defendantSolicitorOrganisation.page.js
+++ e2e/pages/createClaim/defendantSolicitorOrganisation.page.js
@@ -17,20 +17,12 @@ module.exports = {
   },
 
   async enterOrganisationDetails (organisationRegistered = true, respondentNumber, organisationNumber = 1) {
-    I.waitForElement(this.fields(respondentNumber).respondentOrgRepresented.id);
     await I.runAccessibilityTest();
-    await within(this.fields(respondentNumber).respondentOrgRepresented.id, () => {
-      const {yes, no} = this.fields(respondentNumber).respondentOrgRepresented.options;
-      I.click(organisationRegistered ? yes : no);
-    });
-
-    if (organisationRegistered) {
-      I.waitForElement(this.fields(respondentNumber).orgPolicyReference);
-      I.fillField(this.fields(respondentNumber).orgPolicyReference, 'Defendant policy reference');
-      I.waitForElement(this.fields(respondentNumber).searchText);
-      I.fillField(this.fields(respondentNumber).searchText, 'Civil');
-      I.click(`a[title="Select the organisation Civil - Organisation ${organisationNumber}"]`);
-    }
+    I.waitForElement(this.fields(respondentNumber).orgPolicyReference);
+    I.fillField(this.fields(respondentNumber).orgPolicyReference, 'Defendant policy reference');
+    I.waitForElement(this.fields(respondentNumber).searchText);
+    I.fillField(this.fields(respondentNumber).searchText, 'Civil');
+    I.click(`a[title="Select the organisation Civil - Organisation ${organisationNumber}"]`);
     await I.clickContinue();
   }
 };
