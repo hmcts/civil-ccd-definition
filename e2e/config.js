2eda7d0f29595c8c178a755895c348f5f536efc3
Max Meulendijks <max.meulendijks@version1.com>
2022-09-23 15:38:35 +0100

    different URLs

--- e2e/config.js
+++ e2e/config.js
@@ -6,18 +6,18 @@ module.exports = {
     url: 'http://localhost:5555'
   },
   url: {
+    //manageCase: 'https://xui-civil-ccd-pr-1287.service.core-compute-preview.internal',
+    //authProviderApi: 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
+    //ccdDataStore: 'https://ccd-data-store-api-civil-ccd-pr-1287.service.core-compute-preview.internal',
+    //dmStore: 'http://dm-store-aat.service.core-compute-aat.internal',
+    //idamApi: 'https://idam-api.aat.platform.hmcts.net',
+    //civilService: 'https://civil-ccd-pr-1287.service.core-compute-preview.internal'
     manageCase: 'https://xui-civil-ccd-pr-1287.service.core-compute-preview.internal',
     authProviderApi: 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
     ccdDataStore: 'https://ccd-data-store-api-civil-ccd-pr-1287.service.core-compute-preview.internal',
     dmStore: 'http://dm-store-aat.service.core-compute-aat.internal',
     idamApi: 'https://idam-api.aat.platform.hmcts.net',
     civilService: 'https://civil-ccd-pr-1287.service.core-compute-preview.internal'
-    // manageCase: process.env.URL || 'http://localhost:3333',
-    // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://localhost:4502',
-    // ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://localhost:4452',
-    // dmStore: process.env.DM_STORE_URL || 'http://dm-store:8080',
-    // idamApi: process.env.IDAM_API_URL || 'http://localhost:5000',
-    // civilService: process.env.CIVIL_SERVICE_URL || 'http://localhost:4000',
   },
   s2s: {
     microservice: 'civil_service',
