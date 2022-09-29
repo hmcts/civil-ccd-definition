cb0d432c433f808e4041855a1a817a5d524a8cc7
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2022-09-29 13:40:53 +0100

    Env variable
    Adding env variable for url process.env.WA_TASK_MGMT_URL

--- e2e/config.js
+++ e2e/config.js
@@ -13,14 +13,14 @@ module.exports = {
     // dmStore:process.env.DM_STORE_URL || 'http://dm-store-demo.service.core-compute-demo.internal',
     // idamApi: process.env.IDAM_API_URL || 'https://idam-api.demo.platform.hmcts.net',
     // civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-demo.service.core-compute-demo.internal',
-    // waTaskMgmtApi: 'http://wa-task-management-api-demo.service.core-compute-demo.internal'
+    // waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-demo.service.core-compute-demo.internal'
     manageCase: process.env.URL || 'http://localhost:3333',
     authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://localhost:4502',
     ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://localhost:4452',
     dmStore: process.env.DM_STORE_URL || 'http://dm-store:8080',
     idamApi: process.env.IDAM_API_URL || 'http://localhost:5000',
     civilService: process.env.CIVIL_SERVICE_URL || 'http://localhost:4000',
-    waTaskMgmtApi: 'http://wa-task-management-api-aat.service.core-compute-aat.internal'
+    waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-aat.service.core-compute-aat.internal'
   },
   s2s: {
     microservice: 'civil_service',
