e3d52a3f4e3893c551c9b76fbfe45a5ec0577621
Douglas Rice <douglas.rice@hmcts.net>
2022-10-04 13:02:01 +0100

    test

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -6,7 +6,7 @@ environment=${1:-prod}
 excludeNonProdFiles=${2:-true}
 
 if [ ${environment} == preview ]; then
-  excludedFilenamePatterns="-e *-prod.json"
+  excludedFilenamePatterns="-e *-prod.json,*-SDO-nonprod.json"
 elif [ ${environment} == demo ]; then
   excludedFilenamePatterns="-e UserProfile.json,*-prod.json"
 elif [ ${environment} == local ]; then
