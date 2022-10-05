0d5f9f3f07e73f14ff4cdf68f8323310bfedf6d9
Douglas Rice <douglas.rice@hmcts.net>
2022-10-05 11:01:27 +0100

    test

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -8,7 +8,7 @@ excludeNonProdFiles=${2:-true}
 if [ ${environment} == preview ]; then
    #excludedFilenamePatterns="-e *-prod.json,*-SDO-HNL-nonprod.json"
    #for testing HNL uplifting, comment the above line and uncomment below
-   excludedFilenamePatterns="-e *-prod.json,*-SDO-nonprod.json
+   excludedFilenamePatterns="-e *-prod.json,*-SDO-nonprod.json"
 elif [ ${environment} == demo ]; then
   excludedFilenamePatterns="-e UserProfile.json,*-prod.json,*-SDO-HNL-nonprod.json"
 elif [ ${environment} == local ]; then
