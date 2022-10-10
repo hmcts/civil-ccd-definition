9d67a0f63d49ab7f75e6b94de9b9ef450dedbe1d
sampankumar <sampank@gmail.com>
2022-10-10 11:43:11 +0100

    Adding SDO related fields in non-prod files (#1410)

--- e2e/api/steps.js
+++ e2e/api/steps.js
@@ -513,7 +513,7 @@ module.exports = {
       'The date cannot be in the past and must not be more than a year in the future');
 
     let validState = expectedCcdState || 'PROCEEDS_IN_HERITAGE_SYSTEM';
-    if (['demo'].includes(config.runningEnv)) {
+    if (['preview', 'demo'].includes(config.runningEnv)) {
       validState = 'JUDICIAL_REFERRAL';
     }
 
