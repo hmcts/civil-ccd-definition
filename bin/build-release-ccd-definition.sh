95ebe4c017b78eb50ef3fdc84da96b1bee478a68
Ash Modi <ashish.modi@outlook.com>
2022-10-20 17:20:41 +0100

    Testing CIV-4063

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -6,9 +6,9 @@ environment=${1:-prod}
 excludeNonProdFiles=${2:-true}
 
 if [ ${environment} == preview ]; then
-   excludedFilenamePatterns="-e *-prod.json,*-SDO-HNL-nonprod.json"
+   #excludedFilenamePatterns="-e *-prod.json,*-SDO-HNL-nonprod.json"
    #for testing HNL uplifting, comment the above line and uncomment below
-   #excludedFilenamePatterns="-e *-prod.json,*-SDO-nonprod.json
+   excludedFilenamePatterns="-e *-prod.json,*-SDO-nonprod.json"
 elif [ ${environment} == demo ]; then
   excludedFilenamePatterns="-e UserProfile.json,*-prod.json,*-SDO-HNL-nonprod.json"
 elif [ ${environment} == local ]; then
