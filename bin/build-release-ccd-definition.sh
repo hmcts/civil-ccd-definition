bdd8500d93ba2b10a66d1759be13cc50a1c8fd84
Kurt <kurtplaschke@hotmail.co.uk>
2022-10-24 10:31:55 +0100

    Update exclusions (#1539)
    * updated exclusions command to remove SDO-HNL to just HNL
    * updated file permission
    
    Co-authored-by: TurkingtonL <lillie.turkington@HMCTS.net>
    Co-authored-by: Harry H <33700332+HarryH96@users.noreply.github.com>

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -6,11 +6,11 @@ environment=${1:-prod}
 excludeNonProdFiles=${2:-true}
 
 if [ ${environment} == preview ]; then
-   excludedFilenamePatterns="-e *-prod.json,*-SDO-HNL-nonprod.json,*CUI.json,*CUI-nonprod.json,*CUI-prod.json"
+   excludedFilenamePatterns="-e *-prod.json,*HNL-nonprod.json,*CUI.json,*CUI-nonprod.json,*CUI-prod.json"
    #for testing HNL uplifting, comment the above line and uncomment below
    #excludedFilenamePatterns="-e *-prod.json,*-SDO-nonprod.json"
 elif [ ${environment} == demo ]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-prod.json,*-SDO-HNL-nonprod.json"
+  excludedFilenamePatterns="-e UserProfile.json,*-prod.json,*HNL-nonprod.json"
 elif [ ${environment} == local ]; then
   excludedFilenamePatterns="-e *-prod.json"
 elif [ ${excludeNonProdFiles} == true ]; then
