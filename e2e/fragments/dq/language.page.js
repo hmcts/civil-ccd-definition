8951ebd286943828d0a7acf2bdb9002a575af944
Kurt Plaschke <k.plaschke@hull.ac.uk>
2022-10-25 11:19:33 +0100

    updated language screen for spec unspec defendent and applicant, updated e2e

--- e2e/fragments/dq/language.page.js
+++ e2e/fragments/dq/language.page.js
@@ -1,4 +1,5 @@
 const {I} = inject();
+const {checkToggleEnabled} = require("../../api/testingSupport")
 
 module.exports = {
 
@@ -29,7 +30,11 @@ module.exports = {
     I.waitForElement(this.fields(party).evidence.id);
     await I.runAccessibilityTest();
 
-    I.click(this.fields(party).evidence.options.welsh);
+    let isHnlEnabled = await checkToggleEnabled("hearing-and-listing-sdo");
+
+    if (!isHnlEnabled) {
+      I.click(this.fields(party).evidence.options.welsh);
+    }
     I.click(this.fields(party).court.options.welsh);
     I.click(this.fields(party).documents.options.welsh);
 
