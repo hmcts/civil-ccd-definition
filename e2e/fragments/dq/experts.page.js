fe13cc4fdfe0183e829b738fbdaa863b2a16f130
Kurt Plaschke <k.plaschke@hull.ac.uk>
2022-11-01 10:14:40 +0000

    updated experts collection

--- e2e/fragments/dq/experts.page.js
+++ e2e/fragments/dq/experts.page.js
@@ -27,8 +27,17 @@ module.exports = {
       },
       expertDetails: {
         id: `#${party}DQExperts_details'`,
-        element: {
+        oldElements: {
           name: `#${party}DQExperts_details_0_name`,
+          fieldOfExpertise: `#${party}DQExperts_details_0_fieldOfExpertise`,
+          whyRequired: `#${party}DQExperts_details_0_whyRequired`,
+          estimatedCost: `#${party}DQExperts_details_0_estimatedCost`,
+        },
+        elements: {
+          firstName: `#${party}DQExperts_details_0_firstName`,
+          surname: `#${party}DQExperts_details_0_surname`,
+          emailAddress: `#${party}DQExperts_details_0_emailAddress`,
+          phoneNumber: `#${party}DQExperts_details_0_phoneNumber`,
           fieldOfExpertise: `#${party}DQExperts_details_0_fieldOfExpertise`,
           whyRequired: `#${party}DQExperts_details_0_whyRequired`,
           estimatedCost: `#${party}DQExperts_details_0_estimatedCost`,
@@ -59,9 +68,21 @@ module.exports = {
   async addExpert(party) {
     await I.addAnotherElementToCollection();
     I.waitForElement(this.fields(party).expertDetails.element.name);
-    I.fillField(this.fields(party).expertDetails.element.name, 'John Smith');
+    I.fillField(this.fields(party).expertDetails.element.firstName, 'John');
+    I.fillField(this.fields(party).expertDetails.element.surname, 'Smith');
+    I.fillField(this.fields(party).expertDetails.element.emailAddress, 'johnsmith@email.com');
+    I.fillField(this.fields(party).expertDetails.element.phoneNumber, '0711111111');
     I.fillField(this.fields(party).expertDetails.element.fieldOfExpertise, 'Science');
     I.fillField(this.fields(party).expertDetails.element.whyRequired, 'Reason why required');
     I.fillField(this.fields(party).expertDetails.element.estimatedCost, '100');
+  },
+
+  async addExpertOldFields(party) {
+    await I.addAnotherElementToCollection();
+    I.waitForElement(this.fields(party).expertDetails.element.name);
+    I.fillField(this.fields(party).expertDetails.oldElements.name, 'John Smith');
+    I.fillField(this.fields(party).expertDetails.oldElements.fieldOfExpertise, 'Science');
+    I.fillField(this.fields(party).expertDetails.oldElements.whyRequired, 'Reason why required');
+    I.fillField(this.fields(party).expertDetails.oldElements.estimatedCost, '100');
   },
 };
