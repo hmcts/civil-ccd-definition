4a376aa0abe1640e191c23d19867cc3ae837b856
Sampan Kumar <sampan.kumar@hmcts.net>
2022-11-02 00:25:07 +0000

    adding condition for FULL_DEfence

--- e2e/api/steps_LRspecFast.js
+++ e2e/api/steps_LRspecFast.js
@@ -193,7 +193,7 @@ module.exports = {
 
 
     let validState = expectedCcdState || 'PROCEEDS_IN_HERITAGE_SYSTEM';
-    if (['preview', 'demo'].includes(config.runningEnv) && response == 'FULL_DEFENCE') {
+    if (['preview', 'demo'].includes(config.runningEnv) && (response == 'FULL_DEFENCE' || response == 'NOT_PROCEED')) {
       validState = 'JUDICIAL_REFERRAL';
     }
     await assertSubmittedEvent(validState || 'PROCEEDS_IN_HERITAGE_SYSTEM');
