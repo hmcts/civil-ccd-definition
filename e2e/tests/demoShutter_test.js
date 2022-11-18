e3beaee8c4bafcf911aa2a5be59128601c90b71f
sankaviv1 <sankavi.mohanraj@version1.com>
2022-11-18 15:13:32 +0000

    add demo test for shutter

--- e2e/tests/demoShutter_test.js
+++ e2e/tests/demoShutter_test.js
@@ -1,6 +1,8 @@
 const config = require('../../../config.js');
 const apiRequest = require('./apiRequest.js');
 
+Feature('@api-unspec');
+
 Scenario('Login', async ({}) => {
   await apiRequest.setupTokens(config.applicantSolicitorUser);
 });
