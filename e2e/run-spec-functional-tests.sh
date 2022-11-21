10071f4fc44f1646f1244532da683892d946e206
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-11-21 08:53:56 +0000

    CIV-5369: Set Up API Test

--- e2e/run-spec-functional-tests.sh
+++ e2e/run-spec-functional-tests.sh
@@ -6,3 +6,6 @@ yarn test:api-spec
 
 echo "Run Api Functional tests for e2e-spec"
 yarn test:e2e-spec
+
+echo "Run Api Functional tests for api-spec-cui"
+yarn test:api-spec-cui
---
+++ e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_test.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+
+const config = require('../../../config.js');
+
+Feature('CCD 1v1 API test @api-spec @api-spec-cui');
+
+Scenario('1v1 full admit claimant and defendant response', async ({I, api_spec_cui}) => {
+  await api_spec_cui.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
+  await api_spec_cui.defendantResponse(config.defendantSolicitorUser, 'FULL_ADMISSION');
+  await api_spec_cui.claimantResponse(config.applicantSolicitorUser, 'FULL_ADMISSION', 'ONE_V_ONE',
+    'AWAITING_APPLICANT_INTENTION');
+});
