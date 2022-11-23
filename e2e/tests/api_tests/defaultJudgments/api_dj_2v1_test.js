9dd77eb3d6693dfb5aec8971a2ba11d8a097f14a
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2022-11-23 21:00:26 +0000

    update

--- e2e/tests/api_tests/defaultJudgments/api_dj_2v1_test.js
+++ e2e/tests/api_tests/defaultJudgments/api_dj_2v1_test.js
@@ -5,6 +5,8 @@ const mpScenario = 'TWO_V_ONE';
 
 Feature('CCD 2v1 API test @api-dj-2v1, @api-dj');
 
+let caseId;
+
 Scenario('Default Judgment claim', async ({I, api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
   await api.addCaseNote(config.adminUser);
@@ -12,12 +14,27 @@ Scenario('Default Judgment claim', async ({I, api}) => {
   await api.notifyClaimDetails(config.applicantSolicitorUser);
   await api.amendRespondent1ResponseDeadline(config.systemupdate);
   await api.defaultJudgment(config.applicantSolicitorUser);
+  caseId = api.getCaseId();
+});
+
+Scenario('Verify Direction order(summaryJudgmentDirectionsTask) Judge task', async ({I, api}) => {
+  if (config.runWAApiTest) {
+    const summaryJudgmentDirectionsTask = await api.retrieveTaskDetails(config.judgeUserWithRegionId1, caseId, config.waTaskIds.judgeUnspecDJTask);
+    console.log('summaryJudgmentDirectionsTask...' , summaryJudgmentDirectionsTask);
+  }
 });
 
 Scenario('Default Judgment claim SDO', async ({I, api}) => {
   await api.sdoDefaultJudgment(config.judgeUserWithRegionId1, 'TRIAL_HEARING');
 });
 
+Scenario('Verify Case progression caseProgressionTakeCaseOfflineTask hearing center admin task', async ({I, api}) => {
+  if (config.runWAApiTest) {
+    const caseProgressionTakeCaseOfflineTask = await api.retrieveTaskDetails(config.hearingCenterAdminWithRegionId4, caseId, config.waTaskIds.listingOfficerCaseProgressionTask);
+    console.log('caseProgressionTakeCaseOfflineTask...' , caseProgressionTakeCaseOfflineTask);
+  }
+});
+
 AfterSuite(async  ({api}) => {
-  //await api.cleanUp();
+  await api.cleanUp();
 });
