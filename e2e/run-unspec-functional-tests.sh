e505682ad4b64e491d8aa23cb80d37655cb7e258
sankaviv1 <sankavi.mohanraj@version1.com>
2022-11-28 15:26:08 +0000

    temporarily disable some tests

--- e2e/run-unspec-functional-tests.sh
+++ e2e/run-unspec-functional-tests.sh
@@ -4,5 +4,5 @@ set -ex
 echo "Run Api Functional tests for api-unspec"
 yarn test:api-unspec
 
-echo "Run Api Functional tests for e2e-unspec"
-yarn test:e2e-unspec
+#echo "Run Api Functional tests for e2e-unspec"
+#yarn test:e2e-unspec
