02109dc13ad03a4dd9461255c315bf138393efe6
sankaviv1 <sankavi.mohanraj@version1.com>
2022-12-01 09:31:21 +0000

    fix vars

--- e2e/generate-ccd-configs.sh
+++ e2e/generate-ccd-configs.sh
@@ -4,7 +4,7 @@ set -eu
 
 root_dir=$(realpath $(dirname ${0})/..)
 config_dir=${root_dir}/ccd-definition
-release_definition_output_file=${build_dir}/civil-ccd-${environment}.xlsx
+release_definition_output_file=${root_dir}/e2e/civil-ccd-test.xlsx
 
 # build the ccd definition file
 ${root_dir}/bin/utils/process-definition.sh ${config_dir} ${release_definition_output_file} "-e *-prod.json,*HNL-nonprod.json,*CUI.json,*CUI-nonprod.json,*-GAR2GAspec-nonprod.json"
