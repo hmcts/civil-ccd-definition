440b0fbb16df12751111b24702aecdc97eaf30d5
sankaviv1 <sankavi.mohanraj@version1.com>
2022-12-06 22:51:12 +0000

    add sonar exclusions

--- e2e/tests/unit/DemoUnitTest.test.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require('chai');
-describe('DemoUniTest', () => {
-
-  it('not contain duplicated definitions of the same field', () => {
-  expect('2').to.deep.equal('2');
-  });
-});
