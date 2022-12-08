cb5149f8a37a69d76c9d414110f080eb9aa64e20
Mark Drummond <mark.drummond@version1.com>
2022-12-08 12:33:14 +0000

    shuttering for migration

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,15 +1,15 @@
 exports.config = {
   tests: [
-    './e2e/tests/*_test.js',
-    './e2e/tests/*_test.js',
-    './e2e/tests/api_tests/damages/*_test.js',
-    './e2e/tests/api_tests/lrspec/*_test.js',
-    './e2e/tests/api_tests/defaultJudgments/*_test.js',
-    './e2e/tests/ui_tests/damages/*_test.js',
-    './e2e/tests/ui_tests/damages/nightly/*_test.js',
-    './e2e/tests/ui_tests/lrspec/*_test.js',
-    './e2e/tests/ui_tests/sdo/*_test.js',
-    './e2e/tests/ui_tests/default_judgement/*_test.js'
+    // './e2e/tests/*_test.js',
+    // './e2e/tests/*_test.js',
+    // './e2e/tests/api_tests/damages/*_test.js',
+    // './e2e/tests/api_tests/lrspec/*_test.js',
+    // './e2e/tests/api_tests/defaultJudgments/*_test.js',
+    // './e2e/tests/ui_tests/damages/*_test.js',
+    // './e2e/tests/ui_tests/damages/nightly/*_test.js',
+    // './e2e/tests/ui_tests/lrspec/*_test.js',
+    // './e2e/tests/ui_tests/sdo/*_test.js',
+    // './e2e/tests/ui_tests/default_judgement/*_test.js'
   ],
   output: 'test-results/functional',
   helpers: {
