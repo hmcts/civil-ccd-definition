d0ce706e9cf331824c04c8fbea544ada89117c4e
skhajuria <sankhajuria@gmail.com>
2022-12-08 14:43:56 +0000

    Shutterred release

--- codecept.conf.js
+++ codecept.conf.js
@@ -10,6 +10,15 @@ exports.config = {
     // './e2e/tests/ui_tests/lrspec/*_test.js',
     // './e2e/tests/ui_tests/sdo/*_test.js',
     // './e2e/tests/ui_tests/default_judgement/*_test.js'
+    './bin/tests/*_test.js',
+    './bin/tests/api_tests/damages/*_test.js',
+    './bin/tests/api_tests/lrspec/*_test.js',
+    './bin/tests/api_tests/defaultJudgments/*_test.js',
+    './bin/tests/ui_tests/damages/*_test.js',
+    './bin/tests/ui_tests/damages/nightly/*_test.js',
+    './bin/tests/ui_tests/lrspec/*_test.js',
+    './bin/tests/ui_tests/sdo/*_test.js',
+    './bin/tests/ui_tests/default_judgement/*_test.js'
   ],
   output: 'test-results/functional',
   helpers: {
