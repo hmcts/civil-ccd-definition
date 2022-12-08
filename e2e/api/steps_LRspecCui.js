a8e5fe769134a1e17024b9008da26e951e93e1e4
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2022-12-08 12:03:11 +0000

    CIV-5852: Update Testing from master

--- e2e/api/steps_LRspecCui.js
+++ e2e/api/steps_LRspecCui.js
@@ -13,6 +13,7 @@ const claimData = require('../fixtures/events/createClaimSpec.js');
 const expectedEvents = require('../fixtures/ccd/expectedEventsLRSpec.js');
 const {checkCourtLocationDynamicListIsEnabled, checkToggleEnabled} = require('./testingSupport');
 const {removeHNLFieldsFromClaimData} = require("../helpers/hnlFeatureHelper");
+const {HEARING_AND_LISTING} = require("../fixtures/featureKeys");
 
 let caseId, eventName;
 let caseData = {};
@@ -62,10 +63,12 @@ module.exports = {
 
     createClaimData = data.CREATE_CLAIM_AP(scenario);
 
+    // ToDo: Remove and delete function after hnl uplift released
     const hnlEnabled = await checkToggleEnabled('hearing-and-listing-sdo');
     if(!hnlEnabled) {
       removeHNLFieldsFromClaimData(createClaimData);
     }
+    //==============================================================
 
     await apiRequest.setupTokens(user);
     await apiRequest.startEvent(eventName);
@@ -97,6 +100,13 @@ module.exports = {
 
     let defendantResponseData = eventData['defendantResponses'][scenario][response];
     defendantResponseData = await replaceDefendantResponseWithCourtNumberIfCourtLocationDynamicListIsNotEnabled(defendantResponseData);
+
+    const hnlSdoEnabled = await checkToggleEnabled(HEARING_AND_LISTING);
+
+    //ToDo: Remove when hnlSdoEnabled feature toggle is removed
+    if ((['preview', 'demo'].includes(config.runningEnv)) && hnlSdoEnabled) {
+      defendantResponseData = adjustDataForHnl(defendantResponseData, response);
+    }
 
     caseData = returnedCaseData;
 
