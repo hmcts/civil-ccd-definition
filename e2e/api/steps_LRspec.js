375d31cf3395572cc7d9ac749ebb73b73f9bef6f
sankhajuria <sankhajuria@gmail.com>
2022-12-13 00:21:42 +0000

    Revert "Court location in prod" (#1857)
    Revert "Court location in prod (#1816)"
    
    This reverts commit a2ab147361c1caf84067b60edc84b3f4814a5966.

--- e2e/api/steps_LRspec.js
+++ e2e/api/steps_LRspec.js
@@ -492,7 +492,7 @@ async function replaceDefendantResponseWithCourtNumberIfCourtLocationDynamicList
   let isCourtListEnabled = await checkCourtLocationDynamicListIsEnabled();
   // work around for the api  tests
   console.log(`Court location selected in Env: ${config.runningEnv}`);
-  if (!isCourtListEnabled) {
+  if (!isCourtListEnabled || !(['preview', 'demo'].includes(config.runningEnv))) {
     responseData = {
       ...responseData,
       userInput: {
@@ -511,7 +511,7 @@ async function replaceClaimantResponseWithCourtNumberIfCourtLocationDynamicListI
   let isCourtListEnabled = await checkCourtLocationDynamicListIsEnabled();
   // work around for the api  tests
   console.log(`Court location selected in Env: ${config.runningEnv}`);
-  if (!isCourtListEnabled) {
+  if (!isCourtListEnabled || !(['preview', 'demo'].includes(config.runningEnv))) {
     responseData = {
       ...responseData,
       userInput: {
