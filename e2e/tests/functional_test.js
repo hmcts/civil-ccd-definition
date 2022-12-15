5fb18cd8182551d73a50d44a2dd8999b5e46d3d9
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2022-12-15 10:45:20 +0000

    revert test changes

--- e2e/tests/functional_test.js
+++ e2e/tests/functional_test.js
@@ -1,6 +1,6 @@
 const config = require('../config.js');
 
-Feature('@api-unspec @e2e-unspec-1v2DS @e2e-spec-1v2DS @e2e-sdo @e2e-1v1-djtesttest');
+Feature('@api-unspec @e2e-unspec-1v2DS @e2e-spec-1v2DS @e2e-sdo @e2e-1v1-dj');
 
 Scenario('Login', async ({I}) => {
   await I.retry(5).login(config.applicantSolicitorUser);
