4e4c25146037640f34cf2cb908b547a9a607f9e3
meulendijksm <max.meulendijks@hmcts.net>
2023-01-10 09:07:26 +0000

    removed unnecessary second check for PBAv3

--- e2e/tests/ui_tests/default_judgement/1v1specDJ_test.js
+++ e2e/tests/ui_tests/default_judgement/1v1specDJ_test.js
@@ -1,24 +1,13 @@
 /* eslint-disable no-unused-vars */
 
 const config = require('../../../config.js');
-const testingSupport = require('../../../api/testingSupport.js');
-const {checkToggleEnabled} = require('../../../api/testingSupport');
-const {PBAv3} = require('../../../fixtures/featureKeys');
-const serviceRequest = require('../../../pages/createClaim/serviceRequest.page');
 
 Feature('1v1 spec defaultJudgement @e2e-1v1-dj');
 
 Scenario('DefaultJudgement @create-claim ', async ({I, api_spec}) => {
 
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
-  const pbaV3 = await checkToggleEnabled(PBAv3);
-  console.log('Is PBAv3 toggle on?: ' + pbaV3);
   let caseid = await api_spec.getCaseId();
-
-  if (pbaV3) {
-    await serviceRequest.payFee(caseid);
-  }
-
   await api_spec.amendRespondent1ResponseDeadline(config.systemupdate);
 
   await I.login(config.applicantSolicitorUser);
