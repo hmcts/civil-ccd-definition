3b7db4240b6febee38cdb028d03e25ff8c8f14e2
meulendijksm <max.meulendijks@hmcts.net>
2023-01-10 15:35:55 +0000

    changed CCD_IDAM_REDIRECT_URL to .platform.hmcts.net format

--- bin/variables/load-preview-environment-variables.sh
+++ bin/variables/load-preview-environment-variables.sh
@@ -9,7 +9,7 @@ echo 'export ENVIRONMENT=preview'
 # urls
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
-echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
+echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.aat.platform.hmcts.net/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://ccd-definition-store-civil-ccd-pr-${pr}.preview.platform.hmcts.net"
 echo "export CAMUNDA_BASE_URL=https://camunda-civil-ccd-pr-${pr}.preview.platform.hmcts.net"
 
