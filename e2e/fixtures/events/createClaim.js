e8ef925adaeef6d1eec9630c93d085c12b7e5084
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-01-17 12:21:24 +0000

    Missing method added.

--- e2e/fixtures/events/createClaim.js
+++ e2e/fixtures/events/createClaim.js
@@ -418,6 +418,23 @@ module.exports = {
       }: {},
     };
   },
+  serviceUpdateDto: (caseId, paymentStatus) => {
+    return {
+      service_request_reference: '1324646546456',
+      ccd_case_number: caseId,
+      service_request_amount: '167.00',
+      service_request_status: paymentStatus,
+      payment: {
+        payment_amount: 167.00,
+
+
+
+        payment_reference: '13213223',
+        payment_method: 'by account',
+        case_reference: 'example of case ref'
+      }
+    };
+  },
   cosNotifyClaimDetails : (lip1, lip2) => {
     return {
       ...(lip1) ? {
