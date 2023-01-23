adef6da7377e1359ed3a181e49cec1b4930b8f4c
Filippo Possenti <filippo.possenti@version1.com>
2023-01-23 15:31:18 +0000

    Breaking a test on purpose

--- e2e/tests/functional_test.js
+++ e2e/tests/functional_test.js
@@ -4,5 +4,5 @@ Feature('@api-unspec @e2e-unspec-1v2DS @e2e-spec-1v2DS @e2e-sdo @e2e-1v1-dj');
 
 Scenario('Login', async ({I}) => {
   await I.retry(5).login(config.applicantSolicitorUser);
-  await I.retry(5).see('Case list');
+  await I.retry(5).see('+++Case list+++');
 });
