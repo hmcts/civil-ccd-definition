a0b3c5d38854d3b2ea37885779428938fc7e9a91
dtortolaV1 <93722198+dtortolaV1@users.noreply.github.com>
2023-01-26 11:33:40 +0000

    Feat/civ 2493 b (#2080)
    * CIV-2493 sdo tests working again 11/12
    
    * CIV-2493 sdo tests working again 11/12
    
    * CIV-2493 checking sdo test isolation
    
    * CIV-2493 changing value so that it goes to judicial referral
    
    * CIV-2493 removed debugging line
    
    * CIV-2493 paragraph removed from sdo document; fee changed to match returned by fee service
    
    Co-authored-by: dtortolaV1 <dario.tortolanavarro@hmcts.net>

--- e2e/tests/api_tests/sdo/sdo_test.js
+++ e2e/tests/api_tests/sdo/sdo_test.js
@@ -22,11 +22,13 @@ function legalAdvisorClaim(mpScenario) {
 
 function judgeClaim(mpScenario) {
   const data = claimData.createClaim(mpScenario);
+  // locally is calculated with a mock, can comment the following block
   data.midEventData.ClaimValue.claimFee = {
-    calculatedAmountInPence: '7000',
-    code: 'FEE0204',
-    version: '4'
+    calculatedAmountInPence: '100000',
+    code: 'FEE0209',
+    version: '3'
   };
+  // end of block to be commented for local run
   data.valid.ClaimValue.claimValue.statementOfValueInPennies = '2000000';
   return data;
 }
