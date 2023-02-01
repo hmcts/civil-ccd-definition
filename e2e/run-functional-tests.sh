b8085cac03533152f2f5f95e0cc9d94eb690414a
Sampan Kumar <sampan.kumar@hmcts.net>
2023-02-01 15:22:28 +0000

    Adding claim amount for SDO tests

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -4,10 +4,10 @@ set -ex
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ]; then
-  #yarn test:e2e-sdo
-  #yarn test:e2e-spec
+  yarn test:e2e-sdo
+  yarn test:e2e-spec
   yarn test:functional-unspec
-  #yarn test:e2e-dj
+  yarn test:e2e-dj
 else
   yarn test:e2e-spec
   yarn test:functional-unspec
