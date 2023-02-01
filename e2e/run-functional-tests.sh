1dd39494e51ed13fd9d2df1008b69fa0016b87c5
Sampan Kumar <sampan.kumar@hmcts.net>
2023-02-01 09:52:21 +0000

    Testing only small amount of FTs

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -4,10 +4,7 @@ set -ex
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ]; then
-  yarn test:e2e-sdo
-  yarn test:e2e-spec
   yarn test:functional-unspec
-  yarn test:e2e-dj
 else
   yarn test:e2e-spec
   yarn test:functional-unspec
