f502753dafbe47d8d5779867ec178cb444763136
Sampan Kumar <sampan.kumar@hmcts.net>
2023-02-07 12:52:55 +0000

    Adding import of standalone wa bpmn in the jenkins build

--- bin/pull-latest-camunda-wa-files.sh
+++ bin/pull-latest-camunda-wa-files.sh
@@ -11,7 +11,7 @@ git checkout ${branchName}
 cd ..
 
 #Copy camunda folder to civil-ccd-def which contians bpmn files
-cp -r ./wa-standalone-task-bpmn/src\main\resources .
+cp -r ./wa-standalone-task-bpmn/src/main/resources .
 rm -rf ./wa-standalone-task-bpmn
 
 #upload bpmn files to environment
