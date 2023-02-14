5fd0b8995346e28f670f66dfd1c0e38e9a60f29a
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2023-02-14 14:48:07 +0000

    Update tags and test

--- e2e/run-prod-functional-tests.sh
+++ e2e/run-prod-functional-tests.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -ex
 
-echo "Run Functional tests with prod ccd def file"
+echo "Run Api Functional tests for api-unspec"
+yarn test:api-unspec
+yarn test:api-spec
+
+echo "Run E2E Functional tests with prod ccd def file"
 
 yarn test:master-e2e-ft
 yarn test:e2e-nightly-prod
+
+
