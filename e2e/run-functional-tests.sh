ccf6e5e18f223ccb24672969b2fcb7cf6f89f718
sankaviv1 <sankavi.mohanraj@version1.com>
2023-02-22 12:39:42 +0000

    test

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -5,8 +5,8 @@ echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ]; then
 #  todo revert this
-  yarn test:api-noc-unspec
-  yarn test:api-noc-spec
+#  yarn test:api-noc-unspec
+#  yarn test:api-noc-spec
   yarn test:e2e-noc
 else
   yarn test:master-e2e-ft
