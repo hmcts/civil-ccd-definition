c7a2a3adb90c42eda16e0b92d4018eb060e7656a
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2023-03-01 11:16:47 +0000

    CIV-7037 Removed unused list elements

--- e2e/fixtures/events/createGeneralApplication.js
+++ e2e/fixtures/events/createGeneralApplication.js
@@ -1,7 +1,4 @@
 const {listElement} = require('../../api/dataHelper');
-let selectedPba = listElement('PBA0088192');
-const validPba = listElement('PBA0088192');
-const invalidPba = listElement('PBA0078095');
 
 module.exports = {
   createGAData: (isWithNotice, reasonWithoutNotice, calculatedAmount, code) => {
