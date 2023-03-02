f8d10537fadaf8ad14253dedb4ebfd3cf8a6ed24
Kurt Plaschke <k.plaschke@hull.ac.uk>
2023-03-02 17:00:00 +0000

    removed hard coded values

--- e2e/api/testingSupport.js
+++ e2e/api/testingSupport.js
@@ -112,10 +112,6 @@ module.exports =  {
       return false;
     }
 
-    if(toggle === 'pba-version-3-ways-to-pay'){
-      return true;
-    }
-
     return await restHelper.request(
         `${config.url.civilService}/testing-support/feature-toggle/${toggle}`,
         {
