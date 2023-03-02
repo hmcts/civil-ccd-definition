c2a9e6141a6efd2340282a9c2ec82a9224404c9f
Astha Malviya <astha.malviya@HMCTS.NET>
2023-03-02 14:55:45 +0000

    excluding shutter and unshuttered

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -3,6 +3,8 @@
 set -eu
 
 environment=${1:-prod}
+declare -a unshutterlist=["prod","staging"]
+
 
 # if any exclusions are updated here, please also update the exclusions map in e2e/tests/unit/utils/dataProvider.js
 if [ ${environment} == preview ]; then
@@ -16,16 +18,26 @@ elif [ ${environment} == local ]; then
   # upload doesn't currently work with this command due to SDO and SDO-HNL files
   excludedFilenamePatterns="-e *-prod.json"
 elif [ ${environment} == aat ]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*CUI.json"
+  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*DJ.json,*DJspec.json,*CUI.json"
 elif [ ${environment} == prod ]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*CUI.json"
+  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*DJ.json,*DJspec.json,*CUI.json"
 elif [ ${environment} == staging ]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*CUI.json"
+  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*DJ.json,*DJspec.json,*CUI.json"
 else
   echo "ERROR! You are passing an environment that is not known by the script!"
   echo "       Either add the new environment to the script or specify a supported environment!"
   exit 1
 fi
+
+# deciding if unshuttered or shuttered should be excluded
+if grep -q "${environment}" <<< $(j "${unshutterlist[@]}") ; then
+  excludedFilenamePatterns="${excludedFilenamePatterns} ,*-unshuttered.json"
+  echo "${excludedFilenamePatterns}"
+else
+  excludedFilenamePatterns="${excludedFilenamePatterns} ,*-shuttered.json"
+  echo "${excludedFilenamePatterns}"
+fi
+
 
 root_dir=$(realpath $(dirname ${0})/..)
 config_dir=${root_dir}/ccd-definition
