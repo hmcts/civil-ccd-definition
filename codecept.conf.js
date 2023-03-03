d9b181e01c589abdf1ed40056013f758ec16e1ba
Filippo Possenti <filippo.possenti@version1.com>
2023-03-03 15:57:14 +0000

    CIV-0000 Restoring previous codecept conf file

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,12 +1,5 @@
 exports.config = {
-  by: function(numberOfWorkers) {
-    return [
-      ['./e2e/tests/api_tests/lrspec/api_1v1_spec_test.js'],
-      ['./e2e/tests/api_tests/lrspec/api_1v2_spec_test.js']
-    ];
-  },
   tests: [
-    /*
     './e2e/tests/*_test.js',
     './e2e/tests/api_tests/defaultJudgments/*_test.js',
     './e2e/tests/api_tests/damages/*_test.js',
@@ -19,7 +12,6 @@ exports.config = {
     './e2e/tests/ui_tests/noticeofchange/*_test.js',
     './e2e/tests/ui_tests/sdo/*_test.js',
     './e2e/tests/ui_tests/default_judgement/*_test.js'
-    */
   ],
   output: 'test-results/functional',
   helpers: {
