194a01e9b6aeb2bf618d5669cafe7866098d338b
Astha Malviya <astha.malviya@HMCTS.NET>
2023-03-08 16:12:14 +0000

    extension test fix

--- e2e/fixtures/events/informAgreeExtensionDateSpec.js
+++ e2e/fixtures/events/informAgreeExtensionDateSpec.js
@@ -6,7 +6,7 @@ module.exports = {
     const extension = {
       userInput: {
         ExtensionDate: {
-          respondentSolicitor1AgreedDeadlineExtension: dateNoWeekends(20)
+          respondentSolicitor1AgreedDeadlineExtension: dateNoWeekends(40)
         }
       },
       midEventData: {
