30d8d61b5255561985a6f18ead1627612967bab7
Astha Malviya <astha.malviya@HMCTS.NET>
2023-03-08 17:27:07 +0000

    bin fix

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+
 set -eu
 
 environment=${1:-prod}
 
 # if any exclusions are updated here, please also update the exclusions map in e2e/tests/unit/utils/dataProvider.js
 if [ ${environment} == preview ]; then
-	@@ -16,17 +18,27 @@ elif [ ${environment} == local ]; then
+   excludedFilenamePatterns="-e *-prod.json,*HNL-nonprod.json,*CUI.json,*CUI-nonprod.json,*-GAR2GAspec-nonprod.json"
+   #for testing HNL uplifting, comment the above line and uncomment below
+   #excludedFilenamePatterns="-e *-prod.json,*-base-nonprod.json,*CUI.json,*CUI-nonprod.json"
+   #for testing ga enhancements please remove *-GAR2GAspec-nonprod.json which are not required for release 1
+elif [ ${environment} == demo ]; then
+  excludedFilenamePatterns="-e UserProfile.json,*-prod.json,*HNL-nonprod.json,*CUI-nonprod.json"
+elif [ ${environment} == local ]; then
   # upload doesn't currently work with this command due to SDO and SDO-HNL files
   excludedFilenamePatterns="-e *-prod.json"
 elif [ ${environment} == aat ]; then
@@ -22,3 +30,14 @@ fi
 root_dir=$(realpath $(dirname ${0})/..)
 config_dir=${root_dir}/ccd-definition
 build_dir=${root_dir}/build/ccd-release-config
+github_dir=${root_dir}/build/github-release
+release_definition_output_file=${build_dir}/civil-ccd-${environment}.xlsx
+github_file=${github_dir}/civil-ccd-${environment}.xlsx
+
+mkdir -p ${build_dir}
+mkdir -p ${github_dir}
+
+# build the ccd definition file
+${root_dir}/bin/utils/process-definition.sh ${config_dir} ${release_definition_output_file} "${excludedFilenamePatterns}"
+
+cp ${release_definition_output_file} ${github_file}
