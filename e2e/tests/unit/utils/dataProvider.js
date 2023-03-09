19ae144df257443bf9fd39f5195ce7d5e077d7ea
Maciej Modzelewski <maciej.modzelewski@hmcts.net>
2023-03-09 11:29:34 +0000

    CIV-6279 Exclusion removed because DJ is live

--- e2e/tests/unit/utils/dataProvider.js
+++ e2e/tests/unit/utils/dataProvider.js
@@ -14,7 +14,7 @@ const exclusions = new Map([
   ['previewGA', ['-prod.json','HNL-nonprod.json','CUI.json','CUI-nonprod.json']],
   ['demo', ['UserProfile.json','-prod.json','-HNL-nonprod.json','-GAR2GAspec-nonprod.json','-CUI-nonprod.json']],
   // ['local', ['-prod.json']], upload doesn't currently work with this command due to SDO and SDO-HNL files
-  ['prod', ['UserProfile.json','-nonprod.json','-COS-nonprod.json','GAspec.json','-GAR2GAspec-nonprod.json','DJspec.json','DJ-SDO-nonprod.json','DJ-nonprod.json','-HNL-nonprod.json','CUI.json','CUI-nonprod.json']],
+  ['prod', ['UserProfile.json','-nonprod.json','-COS-nonprod.json','GAspec.json','-GAR2GAspec-nonprod.json','DJ-SDO-nonprod.json','DJ-nonprod.json','-HNL-nonprod.json','CUI.json','CUI-nonprod.json']],
   ['default', ['UserProfile.json','GAspec.json','-GAR2GAspec-nonprod.json','-prod.json','-HNL-nonprod.json','CUI.json','CUI-nonprod.json']]
 ]);
 
