e152edd93b46f721369a777e4a531b79bd0e2343
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2023-03-14 14:55:56 +0000

    Update caseProgressionTakeCaseOfflineTask.js

--- wa/tasks/caseProgressionTakeCaseOfflineTask.js
+++ wa/tasks/caseProgressionTakeCaseOfflineTask.js
@@ -13,7 +13,7 @@ module.exports = {
     case_management_category: 'Civil',
     work_type_id: 'hearing_work',
     work_type_label: 'Hearing work',
-    permissions: { values: [ 'Read', 'Own', 'Manage', 'Cancel' ] },
+    permissions: { values: [ 'Read', 'Own', 'Manage', 'Cancel', 'Complete', 'Claim', 'Assign', 'Unassign' ] },
     description: '[Take case offline](/cases/case-details/${[CASE_REFERENCE]}/trigger/TAKE_CASE_OFFLINE/TAKE_CASE_OFFLINE)',
     role_category: 'ADMIN',
 };
