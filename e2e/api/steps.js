caddbc38affd5ea695f7529aca80663336c5e727
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2023-03-24 14:14:05 +0000

    updated header

--- e2e/api/steps.js
+++ e2e/api/steps.js
@@ -177,9 +177,9 @@ module.exports = {
 
     let bodyText = pbaV3 ? 'Your claim will not be issued until payment has been made via the Service Request Tab.'
       : 'Your claim will not be issued until payment is confirmed.';
-
+    let headerText = pbaV3 ? '#Please now pay your claim fee\n# using the link below' : 'Your claim has been received';
     await assertSubmittedEvent('PENDING_CASE_ISSUED', {
-      header: 'Your claim has been received',
+      header: headerText,
       body: bodyText
     });
 
