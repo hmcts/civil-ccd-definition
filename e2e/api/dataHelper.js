ecd50347519b3ddc3e257f7666bbda81b1b610f6
KumarSam <sampank@gmail.com>
2023-03-29 23:05:05 +0100

    CIV-0000- improving code

--- e2e/api/dataHelper.js
+++ e2e/api/dataHelper.js
@@ -25,23 +25,23 @@ module.exports = {
     let date_String = date.toISOString().slice(0, 10);
     let isDateABankHoliday = false;
     if (date.getDay() !== 6 && date.getDay() !== 0) {
-      return date_String;
-    } else {
       if(date.getDay() == 1 || date.getDay() == 5) {
         try {
+          console.log('------------------------here-----------------------');
           const rawBankHolidays = await fetch('https://www.gov.uk/bank-holidays.json');
           const ukbankholidays = await rawBankHolidays.json();
           isDateABankHoliday = JSON.stringify(ukbankholidays['england-and-wales'].events).includes(date_String);
+          if (!isDateABankHoliday) {
+            return date_String;
+          }
         } catch (err) {
           console.log('Error while fetching UK Bank Holidays...', err);
         }
-      }
-      if (!isDateABankHoliday) {
-        return date_String;
       } else {
-        return await dateNoWeekends(days - 1);
+        return date_String;
       }
-
+    } else {
+      return await dateNoWeekends(days - 1);
     }
   },
 
