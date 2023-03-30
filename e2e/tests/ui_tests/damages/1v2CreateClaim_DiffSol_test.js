bb3c03ee8e8d2cb49041dd7d1aad7dc451ce03d3
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2023-03-30 16:43:19 +0100

    Moved ga creation to the end

--- e2e/tests/ui_tests/damages/1v2CreateClaim_DiffSol_test.js
+++ e2e/tests/ui_tests/damages/1v2CreateClaim_DiffSol_test.js
@@ -62,12 +62,6 @@ Scenario('Claimant solicitor notifies defendant solicitors of claim details', as
   await I.click('Sign out');
 }).retry(3);
 
-Scenario('Make a general application', async ({api}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await api.initiateGeneralApplication(caseId(), config.applicantSolicitorUser, 'AWAITING_RESPONDENT_ACKNOWLEDGEMENT');
-  }
-}).retry(3);
-
 /*
 Scenario('Defendant 1 solicitor acknowledges claim', async ({I}) => {
   await I.login(config.defendantSolicitorUser);
@@ -167,6 +161,12 @@ Scenario('Defendant solicitor uploads evidence', async ({I}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     await I.login(config.defendantSolicitorUser);
     await I.evidenceUpload(caseId(), true);
+  }
+}).retry(3);
+
+Scenario('Make a general application', async ({api}) => {
+  if (['preview', 'demo'].includes(config.runningEnv)) {
+    await api.initiateGeneralApplication(caseId(), config.applicantSolicitorUser, 'AWAITING_RESPONDENT_ACKNOWLEDGEMENT');
   }
 }).retry(3);
 
