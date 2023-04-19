bb308640c664d7033df870fb70e09566d0d5c749
sankaviv1 <sankavi.mohanraj@version1.com>
2023-04-19 14:48:57 +0100

    skip e2e tests temporarily

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -6,7 +6,7 @@ echo "Running Functional tests on ${ENVIRONMENT} env"
 if [ ${ENVIRONMENT} == preview ]; then
 #  todo revert after QA
   yarn test:api-hearings
-  yarn test:e2e-hearings
+#  yarn test:e2e-hearings
 else
   yarn test:master-e2e-ft
 fi
