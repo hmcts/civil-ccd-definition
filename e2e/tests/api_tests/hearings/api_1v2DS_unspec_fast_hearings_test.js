f652c42b4b1a4d4006115fc95ad7e34cddbb8bf8
sankaviv1 <sankavi.mohanraj@version1.com>
2023-04-19 10:55:07 +0100

    change def court location

--- e2e/tests/api_tests/hearings/api_1v2DS_unspec_fast_hearings_test.js
+++ e2e/tests/api_tests/hearings/api_1v2DS_unspec_fast_hearings_test.js
@@ -3,19 +3,17 @@ const {getSupportWorkerFlag, getDetainedIndividualFlag,
   getDisruptiveIndividualFlag
 } = require('../../../api/caseFlagsHelper');
 
-const mpScenario = 'ONE_V_ONE';
+const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
 let caseId;
 
 Feature('CCD 1v1 Unspec small hearings API test @non-prod-e2e-ft @api-hearings-unspec @api-hearings');
 
 Scenario('1v2DS full defence defendant and claimant response', async ({api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, '11000');
-  await api.notifyClaim(config.applicantSolicitorUser);
-  // await api.notifyClaim(config.applicantSolicitorUser, mpScenario);
+  await api.notifyClaim(config.applicantSolicitorUser, mpScenario);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
-  await api.defendantResponse(config.defendantSolicitorUser, mpScenario);
-  // await api.defendantResponse(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
-  // await api.defendantResponse(config.secondDefendantSolicitorUser, mpScenario, 'solicitorTwo');
+  await api.defendantResponse(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
+  await api.defendantResponse(config.secondDefendantSolicitorUser, mpScenario, 'solicitorTwo');
   await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO');
 
   caseId = await api.getCaseId();
