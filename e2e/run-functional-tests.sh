a530bf40874006fa64116c84758946c2be573dd8
sankaviv1 <sankavi.mohanraj@version1.com>
2023-04-19 14:36:18 +0100

    add TODO

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -4,7 +4,9 @@ set -ex
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ]; then
+#  todo revert after QA
   yarn test:api-hearings
+  yarn test:e2e-hearings
 else
   yarn test:master-e2e-ft
 fi
