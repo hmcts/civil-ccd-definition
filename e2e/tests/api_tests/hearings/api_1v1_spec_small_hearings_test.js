13164dc588b169aeeb0d176b2814952f4c14dec3
sankaviv1 <sankavi.mohanraj@version1.com>
2023-04-19 11:24:03 +0100

    unskip spec tests

--- e2e/tests/api_tests/hearings/api_1v1_spec_small_hearings_test.js
+++ e2e/tests/api_tests/hearings/api_1v1_spec_small_hearings_test.js
@@ -4,27 +4,28 @@ const {getLanguageInterpreterFlag, getRAWheelchairFlag} = require('../../../api/
 
 const mpScenario = 'ONE_V_ONE';
 const hearingsApi = true;
+const serviceId = 'AAA6';
 let caseId;
 
 Feature('CCD 1v1 Spec small hearings API test @non-prod-e2e-ft @api-hearings-spec @api-hearings');
 
-// Scenario('1v1 full defence defendant and claimant response', async ({api_spec_small}) => {
-//   await api_spec_small.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, hearingsApi);
-//   await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', mpScenario, hearingsApi);
-//   await api_spec_small.claimantResponse(config.applicantSolicitorUser);
-//
-//   caseId = await api_spec_small.getCaseId();
-// });
-//
-// Scenario('Listing officer adds case flags', async ({hearings}) => {
-//   await hearings.createCaseFlags(config.hearingCenterAdminWithRegionId1, caseId, 'applicant1', getLanguageInterpreterFlag());
-//   await hearings.createCaseFlags(config.hearingCenterAdminWithRegionId1, caseId, 'respondent1', getRAWheelchairFlag());
-// });
-//
-// Scenario('Judge choose hearing in person', async ({api_spec_small}) => {
-//   await api_spec_small.createSDO(config.judgeUserWithRegionId1, 'CREATE_SMALL');
-// });
-//
-// Scenario('Hearing centre admin requests a hearing', async ({hearings}) => {
-//   await hearings.generateHearingsPayload(config.hearingCenterAdminWithRegionId1, caseId, 'AAA6');
-// });
+Scenario('1v1 full defence defendant and claimant response', async ({api_spec_small}) => {
+  await api_spec_small.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, hearingsApi);
+  await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', mpScenario, hearingsApi);
+  await api_spec_small.claimantResponse(config.applicantSolicitorUser);
+
+  caseId = await api_spec_small.getCaseId();
+});
+
+Scenario('Listing officer adds case flags', async ({hearings}) => {
+  await hearings.createCaseFlags(config.hearingCenterAdminWithRegionId1, caseId, 'applicant1', getLanguageInterpreterFlag());
+  await hearings.createCaseFlags(config.hearingCenterAdminWithRegionId1, caseId, 'respondent1', getRAWheelchairFlag());
+});
+
+Scenario('Judge choose hearing in person', async ({api_spec_small}) => {
+  await api_spec_small.createSDO(config.judgeUserWithRegionId1, 'CREATE_SMALL');
+});
+
+Scenario('Hearing centre admin requests a hearing', async ({hearings}) => {
+  await hearings.generateHearingsPayload(config.hearingCenterAdminWithRegionId1, caseId, serviceId);
+});
