071472fedd406314018ffeb59d648f79d5f95c0f
sankaviv1 <sankavi.mohanraj@version1.com>
2023-04-19 09:51:10 +0100

    try 1v1

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -4,7 +4,6 @@ set -ex
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ]; then
-  yarn test:api-sdo
   yarn test:api-hearings
 else
   yarn test:master-e2e-ft
