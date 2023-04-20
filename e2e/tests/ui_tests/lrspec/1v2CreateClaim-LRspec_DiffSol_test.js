e1730dc00799bacf3cd8480ce760768ed212e300
Mark Drummond <mark.drummond@version1.com>
2023-04-20 13:03:38 +0100

    testing

--- e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_DiffSol_test.js
+++ e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_DiffSol_test.js
@@ -3,7 +3,7 @@ const {assignCaseRoleToUser, addUserCaseMapping, unAssignAllUsers} = require('..
 const {checkToggleEnabled, checkCaseFlagsEnabled} = require('../../../api/testingSupport');
 const {PBAv3} = require('../../../fixtures/featureKeys');
 const serviceRequest = require('../../../pages/createClaim/serviceRequest.page');
-// const {PARTY_FLAGS} = require('../../../fixtures/caseFlags');
+const {PARTY_FLAGS} = require('../../../fixtures/caseFlags');
 const caseId = () => `${caseNumber.split('-').join('').replace(/#/, '')}`;
 
 const respondent1 = {
@@ -51,76 +51,81 @@ Scenario('Claimant solicitor uploads evidence', async ({LRspec}) => {
   }
 }).retry(3);
 
-// Scenario('1v2 Diff LRs Fast Track Claim  - First Defendant solicitor rejects claim', async ({LRspec}) => {
-//   await LRspec.login(config.defendantSolicitorUser);
-//   await LRspec.respondToClaimFullDefence({
-//     defendant1Response: 'fullDefence',
-//     claimType: 'fast',
-//     defenceType: 'dispute'
-//   });
-//   await LRspec.click('Sign out');
+Scenario('1v2 Diff LRs Fast Track Claim  - First Defendant solicitor rejects claim', async ({LRspec}) => {
+  await LRspec.login(config.defendantSolicitorUser);
+  await LRspec.respondToClaimFullDefence({
+    defendant1Response: 'fullDefence',
+    claimType: 'fast',
+    defenceType: 'dispute'
+  });
+  await LRspec.click('Sign out');
+}).retry(3);
+
+Scenario('1v2 Diff LRs Fast Track Claim  - Second Defendant solicitor rejects claim', async ({LRspec}) => {
+  await LRspec.login(config.secondDefendantSolicitorUser);
+  await LRspec.respond1v2DiffLR_FullDefence({
+    secondDefendant: true,
+    defendant1Response: 'fullDefence',
+    claimType: 'fast',
+    defenceType: 'dispute'
+  });
+  await LRspec.click('Sign out');
+}).retry(3);
+
+Scenario('1v2 Diff LRs Fast Track Claim  - claimant Intention to proceed', async ({LRspec}) => {
+  await LRspec.login(config.applicantSolicitorUser);
+  await LRspec.respondToDefence({mpScenario: 'ONE_V_ONE', claimType: 'fast'});
+  await LRspec.click('Sign out');
+}).retry(3);
+
+Scenario('Add case flags', async ({LRspec}) => {
+  if(checkCaseFlagsEnabled()) {
+    const caseFlags = [{
+      partyName: 'Example applicant1 company', roleOnCase: 'Applicant 1',
+      details: [PARTY_FLAGS.vulnerableUser.value]
+    }, {
+      partyName: 'Example respondent1 company', roleOnCase: 'Respondent 1',
+      details: [PARTY_FLAGS.unacceptableBehaviour.value]
+    }
+    ];
+
+    await LRspec.login(config.hearingCenterAdminWithRegionId1);
+    await LRspec.createCaseFlags(caseFlags);
+    await LRspec.validateCaseFlags(caseFlags);
+  }
+});
+
+Scenario('Judge triggers SDO', async ({LRspec}) => {
+   await LRspec.login(config.judgeUserWithRegionId1);
+   await LRspec.amOnPage(config.url.manageCase + '/cases/case-details/' + caseId());
+   await LRspec.waitForText('Summary');
+   await LRspec.initiateSDO('yes', 'yes', null, null);
+}).retry(3);
+
+// Scenario('Claimant solicitor uploads evidence', async ({LRspec}) => {
+//   if (['preview', 'demo'].includes(config.runningEnv)) {
+//     await LRspec.login(config.applicantSolicitorUser);
+//     await LRspec.evidenceUpload(caseId(), false);
+//   }
+// }).retry(3);
+//
+// Scenario('Defendant solicitor uploads evidence', async ({LRspec}) => {
+//   if (['preview', 'demo'].includes(config.runningEnv)) {
+//     await LRspec.login(config.defendantSolicitorUser);
+//     await LRspec.evidenceUpload(caseId(), true);
+//   }
 // }).retry(3);
 
-// Scenario('1v2 Diff LRs Fast Track Claim  - Second Defendant solicitor rejects claim', async ({LRspec}) => {
-//   await LRspec.login(config.secondDefendantSolicitorUser);
-//   await LRspec.respond1v2DiffLR_FullDefence({
-//     secondDefendant: true,
-//     defendant1Response: 'fullDefence',
-//     claimType: 'fast',
-//     defenceType: 'dispute'
-//   });
-//   await LRspec.click('Sign out');
-// }).retry(3);
-//
-// Scenario('1v2 Diff LRs Fast Track Claim  - claimant Intention to proceed', async ({LRspec}) => {
-//   await LRspec.login(config.applicantSolicitorUser);
-//   await LRspec.respondToDefence({mpScenario: 'ONE_V_ONE', claimType: 'fast'});
-//   await LRspec.click('Sign out');
-// }).retry(3);
-//
-// Scenario('Add case flags', async ({LRspec}) => {
-//   if(checkCaseFlagsEnabled()) {
-//     const caseFlags = [{
-//       partyName: 'Example applicant1 company', roleOnCase: 'Applicant 1',
-//       details: [PARTY_FLAGS.vulnerableUser.value]
-//     }, {
-//       partyName: 'Example respondent1 company', roleOnCase: 'Respondent 1',
-//       details: [PARTY_FLAGS.unacceptableBehaviour.value]
-//     }
-//     ];
-//
-//     await LRspec.login(config.hearingCenterAdminWithRegionId1);
-//     await LRspec.createCaseFlags(caseFlags);
-//     await LRspec.validateCaseFlags(caseFlags);
-//   }
-// });
-//
-// Scenario('Judge triggers SDO', async ({LRspec}) => {
-//    await LRspec.login(config.judgeUserWithRegionId1);
-//    await LRspec.amOnPage(config.url.manageCase + '/cases/case-details/' + caseId());
-//    await LRspec.waitForText('Summary');
-//    await LRspec.initiateSDO('yes', 'yes', null, null);
-// }).retry(3);
-//
-//
-// //
-// // Scenario('Defendant solicitor uploads evidence', async ({LRspec}) => {
-// //   if (['preview', 'demo'].includes(config.runningEnv)) {
-// //     await LRspec.login(config.defendantSolicitorUser);
-// //     await LRspec.evidenceUpload(caseId(), true);
-// //   }
-// // }).retry(3);
-//
-// // ToDo: Refactor to trigger create case flags event
-// Scenario.skip('Add case flags - validateCaseFlags', async ({LRspec}) => {
-//   await LRspec.login(config.adminUser);
-//   // await I.createCaseFlags();
-//   await LRspec.validateCaseFlags([
-//     { partyName: 'Example applicant1 company', details: [] },
-//     { partyName: 'Example respondent1 company', details: [] },
-//     { partyName: 'Example respondent2 company', details: [] }
-//   ]);
-// }).retry(3);
+// ToDo: Refactor to trigger create case flags event
+Scenario.skip('Add case flags - validateCaseFlags', async ({LRspec}) => {
+  await LRspec.login(config.adminUser);
+  // await I.createCaseFlags();
+  await LRspec.validateCaseFlags([
+    { partyName: 'Example applicant1 company', details: [] },
+    { partyName: 'Example respondent1 company', details: [] },
+    { partyName: 'Example respondent2 company', details: [] }
+  ]);
+}).retry(3);
 
 AfterSuite(async  () => {
   await unAssignAllUsers();
