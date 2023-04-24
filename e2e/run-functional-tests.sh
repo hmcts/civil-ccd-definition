74f70470b48ef3b9a86e97a6644c2c593b30ff70
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2023-04-24 09:12:12 +0100

    CIV-6915 remove sdo- hearing test

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -5,6 +5,7 @@ echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ]; then
   #yarn test:non-prod-e2e-ft
+  echo 'Not running test until hearing remaster is merged'
 else
   yarn test:master-e2e-ft
 fi
