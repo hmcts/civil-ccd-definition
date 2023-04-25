ed5625682ee6db6d021d8b0e91f79f080cfb503a
Miguel Molina <93722947+miguelMolina3691@users.noreply.github.com>
2023-04-25 09:36:04 +0100

    CIV-6915 Restore test (#2561)
    * CIV-6915 Restore test
    
    * CIV-6915 restore test

--- e2e/run-non-prod-features-flag-enabled-functional-tests.sh
+++ e2e/run-non-prod-features-flag-enabled-functional-tests.sh
@@ -4,7 +4,9 @@ set -ex
 echo "Run Api Functional tests with non prod ccd def "
 yarn test:api-unspec
 yarn test:api-spec
+yarn test:api-dj
 yarn test:api-cos
+yarn test:api-sdo
 
 echo "Run E2E Functional tests with non prod ccd def file"
 
