b5041ed944005dfb4706d128ce2d424d29314b1e
sankaviv1 <sankavi.mohanraj@version1.com>
2023-05-10 12:27:02 +0100

    update comment

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -6,7 +6,7 @@ echo "Running Functional tests on ${ENVIRONMENT} env"
 if [ ${ENVIRONMENT} == preview ]; then
 #  todo revert after QA
   yarn test:api-hearings
-#  yarn test:e2e-hearings
+#  yarn test:non-prod-e2e-ft
 else
   yarn test:master-e2e-ft
 fi
