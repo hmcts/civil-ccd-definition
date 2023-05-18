ba8a9bc8d8b9dd0a475e4768b8e47fe26cc1cdc4
sankaviv1 <sankavi.mohanraj@version1.com>
2023-05-18 11:50:40 +0100

    fix test

--- e2e/fragments/dq/requestedCourt.page.js
+++ e2e/fragments/dq/requestedCourt.page.js
@@ -1,5 +1,4 @@
 const {I} = inject();
-const {checkCourtLocationDynamicListIsEnabled} = require('./../../api/testingSupport');
 const config = require('./../../config');
 
 module.exports = {
@@ -32,15 +31,7 @@ module.exports = {
     I.waitForElement(this.fields(party).requestHearingAtSpecificCourt.id);
     await I.runAccessibilityTest();
 
-    let isCourtListEnabled = await checkCourtLocationDynamicListIsEnabled();
-    if (!isCourtListEnabled) {
-      await within(this.fields(party).requestHearingAtSpecificCourt.id, () => {
-        I.click(this.fields(party).requestHearingAtSpecificCourt.options.yes);
-      });
-      I.fillField(this.oldFields(party).responseCourtCode, '343');
-    } else {
-      I.selectOption(this.fields(party).courtLocation.id, this.fields(party).courtLocation.options.defendantPreferredCourt);
-    }
+    I.selectOption(this.fields(party).courtLocation.id, this.fields(party).courtLocation.options.defendantPreferredCourt);
 
     I.fillField(this.fields(party).reasonForHearingAtSpecificCourt, 'A reason for the court');
     await I.clickContinue();
