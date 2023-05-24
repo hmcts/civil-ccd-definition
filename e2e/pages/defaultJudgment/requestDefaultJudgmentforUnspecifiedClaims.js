bf9dc8a8920157ee4a450f88c00bfd097fed564f
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2023-05-24 13:07:08 +0100

    update

--- e2e/pages/defaultJudgment/requestDefaultJudgmentforUnspecifiedClaims.js
+++ e2e/pages/defaultJudgment/requestDefaultJudgmentforUnspecifiedClaims.js
@@ -181,8 +181,6 @@ module.exports = {
   async performAndVerifyTransferCaseOffline(caseId) {
     await I.amOnPage(config.url.manageCase + '/cases/case-details/' + caseId);
     await I.waitForText('Summary');
-    await I.amOnPage(config.url.manageCase + '/cases/case-details/' + caseId + '#History');
-    await I.waitForText('Case Progression');
     await I.amOnPage(config.url.manageCase + '/cases/case-details/' + caseId + '/trigger/TAKE_CASE_OFFLINE/submit');
     await I.waitForText('Take case offline');
     await I.click('Submit');
