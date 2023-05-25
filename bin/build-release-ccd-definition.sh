a9020faa37b02e063a834a1ed439f01973a8dea1
Sabah u Din Irfan <sabah.irfan@gmail.com>
2023-05-25 15:56:26 +0100

    Revert "CIV-5011 excluding shutter and unshuttered (#2560)"
    This reverts commit 8beced83dfecd4454cfa12d831fdcc417089345e.

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -3,8 +3,6 @@
 set -eu
 
 environment=${1:-prod}
-activateShutter=${2:-false}
-
 
 # if any exclusions are updated here, please also update the exclusions map in e2e/tests/unit/utils/dataProvider.js
 if [ ${environment} == preview ]; then
@@ -28,18 +26,6 @@ else
   echo "       Either add the new environment to the script or specify a supported environment!"
   exit 1
 fi
-
-# deciding which enviornment should be excluded for unshuttered/shuttered
-if [ "$activateShutter" = true ] ; then
-  echo "We are activating shuttered file for $environment"
-  excludedFilenamePatterns="${excludedFilenamePatterns},AuthorisationCaseType-unshuttered.json"
-  echo "${excludedFilenamePatterns}"
-else
-  echo "We are activating unshuttered file for $environment"
-  excludedFilenamePatterns="${excludedFilenamePatterns},AuthorisationCaseType-shuttered.json"
-  echo "${excludedFilenamePatterns}"
-fi
-
 
 root_dir=$(realpath $(dirname ${0})/..)
 config_dir=${root_dir}/ccd-definition
