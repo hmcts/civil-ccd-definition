1dd067df4891bd9b3fd9346caf399b264ed8015d
KumarSam <sampank@gmail.com>
2023-05-30 16:22:50 +0100

    Running api tests in PR

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -4,7 +4,9 @@ set -ex
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ]; then
-  yarn test:non-prod-e2e-ft
+  yarn --silent test:api-unspec
+  yarn --silent test:api-dj
+  yarn --silent test:api-spec
 else
   yarn test:master-e2e-ft
 fi
