7a940bc31a433c7378c08fc0ea6bc94d0fed674e
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2023-05-31 12:33:29 +0100

    update api tests

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,17 +1,8 @@
 exports.config = {
-  tests: [
-    './e2e/tests/*_test.js',
-    './e2e/tests/api_tests/defaultJudgments/*_test.js',
-    './e2e/tests/api_tests/damages/*_test.js',
-    './e2e/tests/api_tests/lrspec_cui/*_test.js',
-    './e2e/tests/api_tests/sdo/*_test.js',
-    './e2e/tests/ui_tests/damages/*_test.js',
-    './e2e/tests/ui_tests/damages/nightly/*_test.js',
-    './e2e/tests/ui_tests/lrspec/*_test.js',
-    './e2e/tests/ui_tests/noticeofchange/*_test.js',
-    './e2e/tests/api_tests/lrspec/*_test.js',
-    './e2e/tests/ui_tests/sdo/*_test.js',
-    './e2e/tests/ui_tests/default_judgement/*_test.js'
+  tests: process.env.RUN_API_TESTS == 'true' ? [
+    './e2e/tests/api_tests/**/*_test.js'
+  ] : [
+    './e2e/tests/ui_tests/**/*_test.js'
   ],
   output: 'test-results/functional',
   helpers: {
