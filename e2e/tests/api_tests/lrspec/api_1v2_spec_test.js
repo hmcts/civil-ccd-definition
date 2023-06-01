8bdb0008e253d53e9a1c75d1be7466400a771b8c
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2023-06-01 17:17:43 +0100

    update

--- e2e/tests/api_tests/lrspec/api_1v2_spec_test.js
+++ e2e/tests/api_tests/lrspec/api_1v2_spec_test.js
@@ -2,7 +2,7 @@
 
 const config = require('../../../config.js');
 
-Feature('CCD 1v2 API test @api-spec @api-spec-1v2 @api-specified @api-prod-specified');
+Feature('CCD 1v2 API test @api-spec @api-spec-1v2 @api-specified @api-nightly-prod');
 
 Scenario('Create claim spec 1v2', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO');
@@ -46,11 +46,13 @@ Scenario('1v2 small claim different response no full defence', async ({I, api_sp
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'DIFF_NOT_FULL_DEFENCE', 'ONE_V_TWO');
 });
 
-Scenario('1v2 small claim full defence, claimant response', async ({I, api_spec}) => {
+Scenario('1v2 small claim full defence, claimant response @api-prod-specified', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
   await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO',
     'JUDICIAL_REFERRAL');
+  await api_spec.createCaseFlags(config.hearingCenterAdminWithRegionId1);
+  await api_spec.manageCaseFlags(config.hearingCenterAdminWithRegionId1);
 });
 
 Scenario('1v2 small claim full defence, claimant response not proceed', async ({I, api_spec}) => {
