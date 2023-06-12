a6197e75aea2c80ac25135c1fdde378eb7092626
TurkingtonL <lillie.turkington@HMCTS.net>
2023-06-12 15:58:44 +0100

    reverting additional changes

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -4,9 +4,7 @@ set -ex
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
 if [ ${ENVIRONMENT} == preview ]; then
-#  todo revert after QA
-  yarn test:api-hearings
-#  yarn test:non-prod-e2e-ft
+  yarn test:non-prod-e2e-ft
 else
   yarn test:master-e2e-ft
 fi
