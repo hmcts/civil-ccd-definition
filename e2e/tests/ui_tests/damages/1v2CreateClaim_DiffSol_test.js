9f39bb722acd14e60b1dd29e5945d12606feb1b4
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2023-06-26 10:31:28 +0100

    CIV-5768 modify test

--- e2e/tests/ui_tests/damages/1v2CreateClaim_DiffSol_test.js
+++ e2e/tests/ui_tests/damages/1v2CreateClaim_DiffSol_test.js
@@ -156,7 +156,7 @@ Scenario('Add case flags', async ({I}) => {
 
 Scenario('Judge triggers SDO', async ({I}) => {
    await I.login(config.judgeUserWithRegionId1);
-   await I.amOnPage(config.url.manageCase + '/cases/case-details/' + caseId());
+   await I.amOnPage(config.url.manageCase + '/cases/case-details/' + caseNumber);
    await I.waitForText('Summary');
    await I.initiateSDO('yes', 'yes', null, null);
 }).retry(3);
