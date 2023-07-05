e2c8ce2efa87fbdab522e9d3970735c7605a765b
Azam <muhammad.ghulam@hmcts.net>
2023-07-05 22:30:24 +0100

    CIV-9513 updated configuration

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -8,14 +8,14 @@ activateShutter=${2:-false}
 
 # if any exclusions are updated here, please also update the exclusions map in e2e/tests/unit/utils/dataProvider.js
 if [ ${environment} == preview ]; then
-   excludedFilenamePatterns="-e *-prod.json,*ClaimantResponseLRspec-nonprod.json"
+   excludedFilenamePatterns="-e *-prod.json"
 elif [ ${environment} == demo ]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-prod.json,*ClaimantResponseLRspec-nonprod.json"
+  excludedFilenamePatterns="-e UserProfile.json,*-prod.json"
 elif [ ${environment} == local ]; then
   # upload doesn't currently work with this command due to CUI files
   excludedFilenamePatterns="-e *-prod.json"
 elif [ ${environment} == aat ]; then
-  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*CUI.json,*ClaimantResponseLRspec-nonprod.json"
+  excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*CUI.json"
 elif [ ${environment} == prod ]; then
   excludedFilenamePatterns="-e UserProfile.json,*-nonprod.json,*GAspec.json,*CUI.json"
 elif [ ${environment} == staging ]; then
