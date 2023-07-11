75e16a6802e5f3d7720ed50913f7dda3753ad999
dtortolaV1 <dario.tortolanavarro@hmcts.net>
2023-07-11 10:56:44 +0100

    CIV-9230 extension date not editable anymore

--- e2e/fixtures/events/informAgreeExtensionDate.js
+++ e2e/fixtures/events/informAgreeExtensionDate.js
@@ -3,17 +3,7 @@ const {date} = require('../../api/dataHelper');
 module.exports = {
   valid: {
     ExtensionDate: {
-      respondentSolicitor1AgreedDeadlineExtension: date(40)
-    }
-  },
-  invalid: {
-    ExtensionDate: {
-      past: {
-        respondentSolicitor1AgreedDeadlineExtension: date(-1)
-      },
-      beforeCurrentDeadline: {
-        respondentSolicitor1AgreedDeadlineExtension: date(10)
-      }
+
     }
   }
 };
