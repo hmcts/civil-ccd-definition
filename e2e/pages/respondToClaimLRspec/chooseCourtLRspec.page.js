4e0430b02136b65568636bcf2bf17d1361fee7c3
MMNycz <maciej.nycz@version1.com>
2023-07-11 21:30:05 +0100

    Update chooseCourtLRspec.page.js

--- e2e/pages/respondToClaimLRspec/chooseCourtLRspec.page.js
+++ e2e/pages/respondToClaimLRspec/chooseCourtLRspec.page.js
@@ -95,7 +95,8 @@ module.exports = {
     I.selectOption(this.fields(mpScenario).fields.responseCourtLocations.id,
       this.fields(mpScenario).fields.responseCourtLocations.options.preferredCourt);
     I.fillField(this.fields(mpScenario).fields.reasonForHearingAtSpecificCourt, 'Some reason');
-    I.click(this.fields(mpScenario).remoteHearingRequested, "Yes");
+    I.selectOption(this.fields(mpScenario).remoteHearingRequested.id,
+      this.fields(mpScenario).fields.remoteHearingRequested.options.yes);
     I.fillField(this.fields(mpScenario).fields.reasonForRemoteHearing, 'Some reason');
     await I.clickContinue();
   }
