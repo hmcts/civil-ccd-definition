c593c4952b2d8e1f5f3d74219db58cde894bf2f3
Mark Drummond <mark.drummond@version1.com>
2023-07-11 09:18:51 +0100

    CCD api test for submitting new case as caseworker

--- e2e/tests/api_tests/lrspec/api_spec_bulk_test.js
+++ e2e/tests/api_tests/lrspec/api_spec_bulk_test.js
@@ -5,10 +5,11 @@ const config = require('../../../config.js');
 Feature('CCD API test @api-spec-submit-new-claim-as-caseworker @api-specified');
 
 Scenario('Submit a new claim as a caseworker', async ({I, api_spec}) => {
-  await api_spec.createNewClaimWithCaseworker(config.applicantSolicitorUser, 'ONE_V_ONE');
-  await api_spec.createNewClaimWithCaseworker(config.applicantSolicitorUser, 'ONE_V_TWO');
+  if (['preview', 'demo'].includes(config.runningEnv)) {
+    await api_spec.createNewClaimWithCaseworker(config.applicantSolicitorUser, 'ONE_V_ONE');
+    await api_spec.createNewClaimWithCaseworker(config.applicantSolicitorUser, 'ONE_V_TWO');
+  }
 });
-
 
 AfterSuite(async  ({api_spec}) => {
   await api_spec.cleanUp();
