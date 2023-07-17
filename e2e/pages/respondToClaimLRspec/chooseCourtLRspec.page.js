7f02ee478a948fb72490d060490fd6b08eee5db1
MMNycz <maciej.nycz@version1.com>
2023-07-17 11:58:48 +0100

    Update chooseCourtLRspec.page.js

--- e2e/pages/respondToClaimLRspec/chooseCourtLRspec.page.js
+++ e2e/pages/respondToClaimLRspec/chooseCourtLRspec.page.js
@@ -76,13 +76,13 @@ module.exports = {
             },
             reasonForHearingAtSpecificCourt: 'textarea[id$="reasonForHearingAtSpecificCourt"]',
             remoteHearingRequested: {
-              id: 'select[id$="respondent1DQRemoteHearingLRspec_remoteHearingRequested_radio"]',
+              id: 'respondent1DQRemoteHearingLRspec_remoteHearingRequested_radio',
               options: {
                 yes: 'Yes',
                 no: 'No'
               }
             },
-            reasonForRemoteHearing: 'textarea[id$="respondent1DQRemoteHearingLRspec_reasonForRemoteHearing"]'
+            reasonForRemoteHearing: 'respondent1DQRemoteHearingLRspec_reasonForRemoteHearing'
           }
         };
       }
