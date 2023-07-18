102c09960c309c61e4f0de9580e0e68233465d92
sankaviv1 <sankavi.mohanraj@version1.com>
2023-07-18 16:42:12 +0100

    add new page for e2e

---
+++ e2e/fragments/dq/fixedRecoverableCosts.page.js
@@ -0,0 +1,51 @@
+const {I} = inject();
+
+module.exports = {
+
+  fields: function (party) {
+    return {
+      isSubjectToFixedRecoverableCostRegime: {
+        id: `#${party}DQFixedRecoverableCosts_isSubjectToFixedRecoverableCostRegime`,
+        options: {
+          yes: 'Yes',
+          no: 'No'
+        }
+      },
+      band: {
+        id: `#${party}DQFixedRecoverableCosts_band`,
+        options: {
+          band1: 'BAND_1',
+          band2: 'BAND_2',
+          band3: 'BAND_3',
+          band4: 'BAND_4'
+        }
+      },
+      complexityBandingAgreed: {
+        id: `#${party}DQFixedRecoverableCosts_complexityBandingAgreed`,
+        options: {
+          yes: 'Yes',
+          no: 'No'
+        }
+      },
+      reasons: `#${party}DQFixedRecoverableCosts_reasons`,
+    };
+  },
+
+  async fixedRecoverableCosts(party) {
+    I.waitForElement(this.fields(party).isSubjectToFixedRecoverableCostRegime.id);
+    await I.runAccessibilityTest();
+    I.click(this.fields(party).isSubjectToFixedRecoverableCostRegime.options.yes);
+
+    await within(this.fields(party).band.id, () => {
+      I.click(this.fields(party).band.options.band2);
+    });
+
+    await within(this.fields(party).complexityBandingAgreed.id, () => {
+      I.click(this.fields(party).complexityBandingAgreed.options.yes);
+    });
+
+    I.fillField(this.fields(party).reasons, 'Some good reasons');
+
+    await I.clickContinue();
+  }
+};
