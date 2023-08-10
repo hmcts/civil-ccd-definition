bc91d0434dd59ed6f539c0296968bfe7c2117153
MMNycz <94067802+MMNycz@users.noreply.github.com>
2023-08-10 16:35:40 +0100

    CIV-8610 fix functional test (#3070)

--- e2e/fixtures/events/defendantResponseSpecCui.js
+++ e2e/fixtures/events/defendantResponseSpecCui.js
@@ -67,11 +67,11 @@ module.exports = {
                 value: listElement(config.defendantSelectedCourt)
               },
               reasonForHearingAtSpecificCourt: 'Reasons'
+            },
+            respondent1DQRemoteHearingLRspec: {
+              remoteHearingRequested: 'Yes',
+              reasonForRemoteHearing: 'Some reason'
             }
-          },
-          respondent1DQRemoteHearingLRspec: {
-            remoteHearingRequested: 'Yes',
-            reasonForRemoteHearing: 'Some reason'
           },
           HearingSupport: {
             respondent1DQHearingSupport: {
