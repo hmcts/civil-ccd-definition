66b17fcba192502ddacaffbf3f94aeaab8b1fd59
Astha Malviya <astha.malviya@HMCTS.NET>
2023-08-15 15:08:46 +0100

    commenting Body and header change for test

--- e2e/api/steps.js
+++ e2e/api/steps.js
@@ -181,10 +181,7 @@ module.exports = {
 
     console.log('Is PBAv3 toggle on?: ' + pbaV3);
 
-    let bodyText = pbaV3 ? 'Your claim will not be issued until payment is confirmed.'
-      : 'Your claim will not be issued until payment is confirmed.';
-    let headerText = pbaV3 ? '# Please now pay your claim fee\n# using the link below' : 'Your claim has been received';
-    await assertSubmittedEvent('PENDING_CASE_ISSUED', {
+     await assertSubmittedEvent('PENDING_CASE_ISSUED', {
       //header: headerText,
      // body: bodyText
     });
