ac79861645b5f086fd78fb7d38a02d06e59fb7cf
sankaviv1 <sankavi.mohanraj@version1.com>
2023-08-21 17:18:59 +0100

    add wait

--- e2e/pages/selectSDO/unspecClaimsSDOOrderDetails.page.js
+++ e2e/pages/selectSDO/unspecClaimsSDOOrderDetails.page.js
@@ -87,6 +87,7 @@ module.exports = {
       await I.click(this.fields.selectOrderAndHearingDetailsForSDOTask.hearingMethodOptions.inPerson);
       await I.click(this.fields.selectOrderAndHearingDetailsForSDOTask.hearingBundleTypeDocs);
     } else if (orderType == 'decideDamages' || trackType == 'fastTrack') {
+      I.wait(60);
       let fastTrackUpliftsEnabled = await checkFastTrackUpliftsEnabled();
       if (fastTrackUpliftsEnabled) {
         await within(this.fields.fastTrackAllocation.assignComplexityBand.id, () => {
