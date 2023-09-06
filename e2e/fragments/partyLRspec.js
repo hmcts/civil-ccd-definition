a77bfa2114605461e8d8a33db7c519d55b54d3ec
TortolaNavarroD <dario.tortolanavarro@version1.com>
2023-09-06 12:14:15 +0200

    CIV-8818 IT allow not including a correspondence address

--- e2e/fragments/partyLRspec.js
+++ e2e/fragments/partyLRspec.js
@@ -4,20 +4,23 @@ const postcodeLookup = require('./addressPostCodeLookupLRspec');
 module.exports = {
   fields: function (party) {
     return {
-      id: `#spec${party}CorrespondenceAddressRequired_Yes`,
+      idYes: `#spec${party}CorrespondenceAddressRequired_Yes`,
+      idNo: `#spec${party}CorrespondenceAddressRequired_No`,
       container: `#spec${party}CorrespondenceAddressdetails_spec${party}CorrespondenceAddressdetails`,
     };
   },
 
   async enterSpecParty(party, address) {
-    I.waitForElement(this.fields(party).id);
-    await I.runAccessibilityTest();
     if (address) {
+      I.waitForElement(this.fields(party).idYes);
+      await I.runAccessibilityTest();
       await I.click('Yes');
       await within(this.fields(party).container, () => {
         postcodeLookup.enterAddressManually(party, address);
       });
     } else {
+      I.waitForElement(this.fields(party).idNo);
+      await I.runAccessibilityTest();
       await I.click('No');
     }
 
