faf4aa3d891a958bedb4e3aaceefb63b21d8491b
sankaviv1 <sankavi.mohanraj@version1.com>
2023-09-22 11:54:12 +0100

    CIV-10382 case flags updates for experts and witnesses after MCI

--- e2e/tests/ui_tests/damages/1v2CreateClaim_DiffSol_test.js
+++ e2e/tests/ui_tests/damages/1v2CreateClaim_DiffSol_test.js
@@ -139,10 +139,10 @@ Scenario('Claimant solicitor responds to defence', async ({I}) => {
 Scenario('Add case flags', async ({I}) => {
   if(await checkCaseFlagsEnabled()) {
     const caseFlags = [{
-      partyName: 'Example applicant1 company', roleOnCase: 'Applicant 1',
+      partyName: 'Example applicant1 company', roleOnCase: 'Claimant 1',
       details: [PARTY_FLAGS.vulnerableUser.value]
     }, {
-      partyName: 'John Smith', roleOnCase: 'Respondent solicitor 1 witness',
+      partyName: 'John Smith', roleOnCase: 'Defendant solicitor 1 witness',
       details: [PARTY_FLAGS.unacceptableBehaviour.value]
     }
     ];
