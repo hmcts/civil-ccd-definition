d84dbfc134e4de6bbece2d78feea6bf4a5d7077c
Mark Drummond <mark.drummond@version1.com>
2023-10-02 12:48:01 +0100

    Testing api bulk claim

--- e2e/tests/api_tests/lrspec/api_spec_bulk_test.js
+++ e2e/tests/api_tests/lrspec/api_spec_bulk_test.js
@@ -2,9 +2,9 @@
 
 const config = require('../../../config.js');
 
-Feature('CCD API test @api-spec-bulk-claim @api-nonprod');
+Feature('CCD API test @api-spec-bulk-claim');
 
-Scenario.skip('Submit a new claim as a caseworker', async ({I, api_spec}) => {
+Scenario('Submit a new claim as a caseworker', async ({I, api_spec}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     await api_spec.createNewClaimWithCaseworker(config.applicantSolicitorUser, 'ONE_V_ONE');
     await api_spec.createNewClaimWithCaseworker(config.applicantSolicitorUser, 'ONE_V_TWO');
