ddd013c3088eeae1df0e1c47caa86969ede255a3
Azam <muhammad.ghulam@hmcts.net>
2023-10-04 17:03:51 +0100

    CIV-10810 clean code

--- e2e/tests/api_tests/judgmentOnline/api_judgment_online_1v1_test.js
+++ e2e/tests/api_tests/judgmentOnline/api_judgment_online_1v1_test.js
@@ -9,19 +9,15 @@ const claimAmountJudge = '11000';
 
 Feature('Record Judgment 1v1 API test unspec @api-unspec @api-tests-1v1 @api-jo @api-nonprod');
 
-async function prepareClaim(api, claimAmount) {
-  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmount);
-  await api.amendClaimDocuments(config.applicantSolicitorUser);
-  await api.notifyClaim(config.applicantSolicitorUser);
-  await api.notifyClaimDetails(config.applicantSolicitorUser);
-  await api.defendantResponse(config.defendantSolicitorUser, mpScenario, null, 'FAST_CLAIM');
-  await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
-  await api.createFinalOrderJO(config.judgeUserWithRegionId1, 'FREE_FORM_ORDER');
-}
-
 Scenario('1v1 full defence unspecified - caseworker records judgment with mark judgment paid (Det.of means - pay instalments)', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api, claimAmountJudge);
+    await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmountJudge);
+    await api.amendClaimDocuments(config.applicantSolicitorUser);
+    await api.notifyClaim(config.applicantSolicitorUser);
+    await api.notifyClaimDetails(config.applicantSolicitorUser);
+    await api.defendantResponse(config.defendantSolicitorUser, mpScenario, null, 'FAST_CLAIM');
+    await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
+    await api.createFinalOrderJO(config.judgeUserWithRegionId1, 'FREE_FORM_ORDER');
     await api.recordJudgment(caseWorkerUser, mpScenario, 'DETERMINATION_OF_MEANS', 'PAY_IN_INSTALMENTS');
     await api.markJudgmentPaid(caseWorkerUser);
   }
@@ -29,39 +25,39 @@ Scenario('1v1 full defence unspecified - caseworker records judgment with mark j
 
 Scenario('1v1 full defence unspecified - caseworker records judgment with mark judgment paid (Det.of means - pay by date)', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api, claimAmountJudge);
+    await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmountJudge);
+    await api.amendClaimDocuments(config.applicantSolicitorUser);
+    await api.notifyClaim(config.applicantSolicitorUser);
+    await api.notifyClaimDetails(config.applicantSolicitorUser);
+    await api.defendantResponse(config.defendantSolicitorUser, mpScenario, null, 'FAST_CLAIM');
+    await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
+    await api.createFinalOrderJO(config.judgeUserWithRegionId1, 'FREE_FORM_ORDER');
     await api.recordJudgment(caseWorkerUser, mpScenario, 'DETERMINATION_OF_MEANS', 'PAY_BY_DATE');
-    await api.markJudgmentPaid(caseWorkerUser);
-  }
-});
-
-Scenario('1v1 full defence unspecified - caseworker records judgment with mark judgment paid (Det.of means - pay immediately)', async ({api}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api, claimAmountJudge);
-    await api.recordJudgment(caseWorkerUser, mpScenario, 'DETERMINATION_OF_MEANS', 'PAY_IMMEDIATELY');
     await api.markJudgmentPaid(caseWorkerUser);
   }
 });
 
 Scenario('1v1 full defence unspecified - caseworker records judgment (Judge Order - pay instalments)', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api, claimAmountJudge);
+    await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmountJudge);
+    await api.amendClaimDocuments(config.applicantSolicitorUser);
+    await api.notifyClaim(config.applicantSolicitorUser);
+    await api.notifyClaimDetails(config.applicantSolicitorUser);
+    await api.defendantResponse(config.defendantSolicitorUser, mpScenario, null, 'FAST_CLAIM');
+    await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
     await api.recordJudgment(caseWorkerUser, mpScenario, 'JUDGE_ORDER', 'PAY_IN_INSTALMENTS');
-    await api.setAsideJudgment(caseWorkerUser);
-  }
-});
-
-Scenario('1v1 full defence unspecified - caseworker records judgment with set aside (Judge Order - pay by date)', async ({api}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api, claimAmountJudge);
-    await api.recordJudgment(caseWorkerUser, mpScenario, 'JUDGE_ORDER', 'PAY_BY_DATE');
     await api.setAsideJudgment(caseWorkerUser);
   }
 });
 
 Scenario('1v1 full defence unspecified - caseworker records judgment with set aside (Judge Order - pay immediately)', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api, claimAmountJudge);
+    await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmountJudge);
+    await api.amendClaimDocuments(config.applicantSolicitorUser);
+    await api.notifyClaim(config.applicantSolicitorUser);
+    await api.notifyClaimDetails(config.applicantSolicitorUser);
+    await api.defendantResponse(config.defendantSolicitorUser, mpScenario, null, 'FAST_CLAIM');
+    await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
     await api.recordJudgment(caseWorkerUser, mpScenario, 'JUDGE_ORDER', 'PAY_IMMEDIATELY');
     await api.setAsideJudgment(caseWorkerUser);
   }
--- e2e/tests/api_tests/judgmentOnline/api_judgment_online_1v2_test.js
+++ e2e/tests/api_tests/judgmentOnline/api_judgment_online_1v2_test.js
@@ -9,20 +9,16 @@ const claimAmountJudge = '11000';
 
 Feature('Record Judgment 1v2 API test unspec @api-unspec @api-tests-1v2SS @api-jo @api-nonprod');
 
-async function prepareClaim(api, claimAmount) {
-  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmount);
-  await api.amendClaimDocuments(config.applicantSolicitorUser);
-  await api.notifyClaim(config.applicantSolicitorUser, mpScenario);
-  await api.notifyClaimDetails(config.applicantSolicitorUser);
-  await api.defendantResponse(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
-  await api.defendantResponse(config.secondDefendantSolicitorUser, mpScenario, 'solicitorTwo');
-  await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
-  await api.createFinalOrderJO(config.judgeUserWithRegionId1, 'FREE_FORM_ORDER');
-}
-
 Scenario('1v2 full defence unspecified - caseworker records judgment with set aside (Det.of means - pay instalments)', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api, claimAmountJudge);
+    await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmountJudge);
+    await api.amendClaimDocuments(config.applicantSolicitorUser);
+    await api.notifyClaim(config.applicantSolicitorUser, mpScenario);
+    await api.notifyClaimDetails(config.applicantSolicitorUser);
+    await api.defendantResponse(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
+    await api.defendantResponse(config.secondDefendantSolicitorUser, mpScenario, 'solicitorTwo');
+    await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
+    await api.createFinalOrderJO(config.judgeUserWithRegionId1, 'FREE_FORM_ORDER');
     await api.recordJudgment(caseWorkerUser, mpScenario, 'DETERMINATION_OF_MEANS', 'PAY_IN_INSTALMENTS');
     await api.setAsideJudgment(caseWorkerUser);
   }
@@ -30,7 +26,14 @@ Scenario('1v2 full defence unspecified - caseworker records judgment with set as
 
 Scenario('1v2 full defence unspecified - caseworker records judgment with mark judgment paid (Det.of means - pay instalments)', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api, claimAmountJudge);
+    await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmountJudge);
+    await api.amendClaimDocuments(config.applicantSolicitorUser);
+    await api.notifyClaim(config.applicantSolicitorUser, mpScenario);
+    await api.notifyClaimDetails(config.applicantSolicitorUser);
+    await api.defendantResponse(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
+    await api.defendantResponse(config.secondDefendantSolicitorUser, mpScenario, 'solicitorTwo');
+    await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
+    await api.createFinalOrderJO(config.judgeUserWithRegionId1, 'FREE_FORM_ORDER');
     await api.recordJudgment(caseWorkerUser, mpScenario, 'DETERMINATION_OF_MEANS', 'PAY_IN_INSTALMENTS');
     await api.markJudgmentPaid(caseWorkerUser);
   }
