de81d96fa640bfe3cff62f172ac74aa802513f2b
UshaPanneerselvam1 <122611188+UshaPanneerselvam1@users.noreply.github.com>
2023-10-06 19:05:31 +0100

    CIV-3883 CIV-8942 CIV-3955 and CIV-8942 (#3320)
    * CIV-3883 and CIV-8942
    
    * CIV-3955_CIV-8942 changes
    
    * pointing to civil-service PR
    
    * Fixing test
    
    * Reverting pointing to PR
    
    ---------
    
    Co-authored-by: Astha Malviya <astha.malviya@HMCTS.NET>
    Co-authored-by: kdaHMCTS <128375235+kdaHMCTS@users.noreply.github.com>
    Co-authored-by: asthamalviya <104994907+asthamalviya@users.noreply.github.com>

--- e2e/fixtures/events/defendantResponse.js
+++ e2e/fixtures/events/defendantResponse.js
@@ -135,7 +135,11 @@ module.exports = {
             },
             reasonForHearingAtSpecificCourt: 'No reasons',
             requestHearingAtSpecificCourt: 'Yes'
-          }
+          },
+          respondent1DQRemoteHearing: {
+            remoteHearingRequested: 'Yes',
+            reasonForRemoteHearing: 'No reasons'
+          },
         },
         HearingSupport: {
           respondent1DQHearingSupport: {
