1627129acddf9f0988d60cc6c27b89821d8fef20
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-10-16 11:38:27 +0100

    test CIV-10919

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -6,5 +6,5 @@ echo "Running Functional tests on ${ENVIRONMENT} env"
 if [ ${ENVIRONMENT} == preview ]; then
   yarn test:non-prod-e2e-ft
 else
-  yarn test:master-e2e-ft
+  #yarn test:master-e2e-ft
 fi
