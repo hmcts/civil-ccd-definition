c3b8fe7dec2c4eaf9ff11479a7600d221baccce9
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2023-10-20 16:03:31 +0100

    CIV-11087 Add response content

--- e2e/fixtures/events/transferOnlineCase.js
+++ e2e/fixtures/events/transferOnlineCase.js
@@ -32,19 +32,24 @@ module.exports = {
         valid: otherReasons()
       };
     }
-  }
-};
+  },
 
-module.exports = {
   transferCase : () => {
       return {
         valid: {
-          TransferOnlineCase: {
-            notSuitableSdoOptions: 'CHANGE_LOCATION',
-            tocTransferCaseReason: {
-              reasonForCaseTransferJudgeTxt: 'Reason for transferring case'
+          transferCourtLocationList: {
+            value: {
+              code: '8dfefcda-ffc6-480a-80b5-ccf0c0152b6c',
+              label: 'Liverpool Civil and Family Court - 35, Vernon Street, City Square - L2 2BX'
+            },
+          list_items: [
+            {
+              code: '13f10d84-6f22-11ee-ae64-719def063ef9',
+              label: 'Barnet Civil and Family Centre - St Mary\'s Court, Regents Park Road - N3 1BQ'
             }
-          }
+            ]
+          },
+          reasonForTransfer: 'allocated court location is not appropriate'
         }
       };
     }
