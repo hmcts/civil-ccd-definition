eee73145cbe966a6f8b6240b857f6cd0c7de4c30
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2023-10-27 13:16:56 +0100

    need to commit before updating with master

--- e2e/config.js
+++ e2e/config.js
@@ -9,22 +9,22 @@ module.exports = {
     url: 'http://localhost:5555'
   },
   url: {
-    // manageCase: process.env.URL || 'https://manage-case.aat.platform.hmcts.net/',
-    // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
-    // ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-aat.service.core-compute-aat.internal',
-    // dmStore:process.env.DM_STORE_URL || 'http://dm-store-aat.service.core-compute-aat.internal',
-    // idamApi: process.env.IDAM_API_URL || 'https://idam-api.aat.platform.hmcts.net',
-    // civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-aat.service.core-compute-aat.internal',
-    // waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-aat.service.core-compute-aat.internal'
+    manageCase: process.env.URL || 'https://manage-case.aat.platform.hmcts.net/',
+    authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
+    ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-aat.service.core-compute-aat.internal',
+    dmStore:process.env.DM_STORE_URL || 'http://dm-store-aat.service.core-compute-aat.internal',
+    idamApi: process.env.IDAM_API_URL || 'https://idam-api.aat.platform.hmcts.net',
+    civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-aat.service.core-compute-aat.internal',
+    waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-aat.service.core-compute-aat.internal'
 
-// manageCase: 'https://xui-civil-service-pr-3273.preview.platform.hmcts.net',
+// manageCase: 'https://xui-civil-service-pr-3423.preview.platform.hmcts.net',
 //      authProviderApi:'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
-//      ccdDataStore: 'https://ccd-data-store-api-civil-service-pr-3273.preview.platform.hmcts.net',
+//      ccdDataStore: 'https://ccd-data-store-api-civil-service-pr-3423.preview.platform.hmcts.net',
 //      dmStore: 'http://dm-store-aat.service.core-compute-aat.internal',
 //      idamApi: 'https://idam-api.aat.platform.hmcts.net',
-//      civilService: 'https://civil-service-pr-3273.preview.platform.hmcts.net'
+//      civilService: 'https://civil-service-pr-3423.preview.platform.hmcts.net'
 
-    // manageCase: process.env.URL || 'https://manage-case.demo.platform.hmcts.net/',
+    // manageCase: process.env.URL || 'https://manage-case-int.demo.platform.hmcts.net/',
     // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
     // ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
     // dmStore:process.env.DM_STORE_URL || 'http://dm-store-demo.service.core-compute-demo.internal',
@@ -32,12 +32,12 @@ module.exports = {
     // civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-demo.service.core-compute-demo.internal',
     // waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-demo.service.core-compute-demo.internal'
 
-    manageCase: 'https://xui-civil-ccd-pr-3397.preview.platform.hmcts.net',
-    authProviderApi:'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
-    ccdDataStore: 'https://ccd-data-store-api-civil-ccd-pr-3397.preview.platform.hmcts.net',
-    dmStore: 'http://dm-store-aat.service.core-compute-aat.internal',
-    idamApi: 'https://idam-api.aat.platform.hmcts.net',
-    civilService: 'https://civil-ccd-pr-3397.preview.platform.hmcts.net' 
+    // manageCase: 'https://xui-civil-ccd-pr-3415.preview.platform.hmcts.net',
+    // authProviderApi:'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
+    // ccdDataStore: 'https://ccd-data-store-api-civil-ccd-pr-3415.preview.platform.hmcts.net',
+    // dmStore: 'http://dm-store-aat.service.core-compute-aat.internal',
+    // idamApi: 'https://idam-api.aat.platform.hmcts.net',
+    // civilService: 'https://civil-ccd-pr-3415.preview.platform.hmcts.net' 
 
     // manageCase: process.env.URL || 'http://localhost:3333',
     // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://localhost:4502',
--- e2e/tests/api_tests/sdo/api_transfer_online_case_test.js
+++ e2e/tests/api_tests/sdo/api_transfer_online_case_test.js
@@ -10,80 +10,77 @@ const caseWorkerUser = config.hearingCenterAdminWithRegionId1;
 // const caseWorkerUser = config.tribunalCaseworkerWithRegionId1Local;
 const fastClaimAmount = '11000';
 
-Feature('Transfer Online Case 1v1 API test - fast claim - unspec @api-unspec @api-tests-1v1 @api-nonprod');
+// Feature('Transfer Online Case 1v1 API test - fast claim - unspec @api-unspec @api-tests-1v1 @api-nonprod');
 
-async function prepareClaim(api) {
-  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario1v1, fastClaimAmount);
-  await api.amendClaimDocuments(config.applicantSolicitorUser);
-  await api.notifyClaim(config.applicantSolicitorUser);
-  await api.notifyClaimDetails(config.applicantSolicitorUser);
-  await api.defendantResponse(config.defendantSolicitorUser, mpScenario1v1, null, 'SMALL_CLAIMS');
-  await api.claimantResponse(config.applicantSolicitorUser, mpScenario1v1, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
-}
+// async function prepareClaim(api) {
+//   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario1v1, fastClaimAmount);
+//   await api.amendClaimDocuments(config.applicantSolicitorUser);
+//   await api.notifyClaim(config.applicantSolicitorUser);
+//   await api.notifyClaimDetails(config.applicantSolicitorUser);
+//   await api.defendantResponse(config.defendantSolicitorUser, mpScenario1v1, null, 'SMALL_CLAIMS');
+//   await api.claimantResponse(config.applicantSolicitorUser, mpScenario1v1, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
+// }
 
-async function prepareClaimSpec(api_spec) {
-await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
-await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', mpScenario1v2Spec);
-await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO',
-  'JUDICIAL_REFERRAL');
-}
+// async function prepareClaimSpec(api_spec) {
+// await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
+// await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', mpScenario1v2Spec);
+// await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO',
+//   'JUDICIAL_REFERRAL');
+// }
 
-Scenario('1v1 full defence unspecified - not suitable SDO - Transfer Case)', async ({api}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api);
-    await api.notSuitableSDO(judgeUser, 'CHANGE_LOCATION');
-    await api.transferCase(caseWorkerUser);
-  }
-});
+// Scenario('1v1 full defence unspecified - judge user - not suitable SDO - Transfer Case)', async ({api}) => {
+//   if (['preview', 'demo'].includes(config.runningEnv)) {
+//     await prepareClaim(api);
+//     await api.transferCase(judgeUser, 'CHANGE_LOCATION');
+//   }
+// });
 
-Scenario('1v1 full defence unspecified - not suitable SDO - Other reasons)', async ({api}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api);
-    await api.notSuitableSDO(judgeUser, 'OTHER_REASONS');
-  }
-});
+// Scenario('1v1 full defence unspecified - judge user - not suitable SDO - Other reasons)', async ({api}) => {
+//   if (['preview', 'demo'].includes(config.runningEnv)) {
+//     await prepareClaim(api);
+//     await api.transferCase(judgeUser, 'OTHER_REASONS');
+//   }
+// });
 
-AfterSuite(async ({api}) => {
-  await api.cleanUp();
-});
+// AfterSuite(async ({api}) => {
+//   await api.cleanUp();
+// });
 
-Feature('Transfer Online Case 1v2 API test - fast claim - unspec @api-unspec @api-tests-1v2SS @api-nonprod');
+// Feature('Transfer Online Case 1v2 API test - fast claim - unspec @api-unspec @api-tests-1v2SS @api-nonprod');
 
-Scenario('1v2 full defence unspecified - not suitable SDO - Transfer Case)', async ({api}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario1v2, fastClaimAmount);
-    await api.amendClaimDocuments(config.applicantSolicitorUser);
-    await api.notifyClaim(config.applicantSolicitorUser, mpScenario1v2);
-    await api.notifyClaimDetails(config.applicantSolicitorUser);
-    await api.defendantResponse(config.defendantSolicitorUser, mpScenario1v2, 'solicitorOne');
-    await api.defendantResponse(config.secondDefendantSolicitorUser, mpScenario1v2, 'solicitorTwo');
-    await api.claimantResponse(config.applicantSolicitorUser, mpScenario1v2, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
-    await api.notSuitableSDO(judgeUser, 'CHANGE_LOCATION');
-    await api.transferCase(caseWorkerUser);
-  }
-});
+// Scenario('1v2 full defence unspecified - judge user - not suitable SDO - Transfer Case)', async ({api}) => {
+//   if (['preview', 'demo'].includes(config.runningEnv)) {
+//     await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario1v2, fastClaimAmount);
+//     await api.amendClaimDocuments(config.applicantSolicitorUser);
+//     await api.notifyClaim(config.applicantSolicitorUser, mpScenario1v2);
+//     await api.notifyClaimDetails(config.applicantSolicitorUser);
+//     await api.defendantResponse(config.defendantSolicitorUser, mpScenario1v2, 'solicitorOne');
+//     await api.defendantResponse(config.secondDefendantSolicitorUser, mpScenario1v2, 'solicitorTwo');
+//     await api.claimantResponse(config.applicantSolicitorUser, mpScenario1v2, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
+//     await api.transferCase(judgeUser, 'CHANGE_LOCATION');
+//   }
+// });
 
-AfterSuite(async ({api}) => {
-  await api.cleanUp();
-});
+// AfterSuite(async ({api}) => {
+//   await api.cleanUp();
+// });
 
-Feature('Transfer Online Case 1v2 API test - small claim - spec @api-spec-1v2 @api-nonprod');
+// Feature('Transfer Online Case 1v2 API test - small claim - spec @api-spec-1v2 @api-nonprod');
 
-Scenario('Transfer Online Spec claim 1v2 - not suitable SDO - Transfer Case', async ({api_spec}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaimSpec(api_spec);
-    await api_spec.notSuitableSDOspec(judgeUser, 'CHANGE_LOCATION');
-    await api_spec.transferCaseSpec(caseWorkerUser);
-  }
-});
+// Scenario('Transfer Online Spec claim 1v2 - judge user - not suitable SDO - Transfer Case', async ({api_spec}) => {
+//   if (['preview', 'demo'].includes(config.runningEnv)) {
+//     await prepareClaimSpec(api_spec);
+//     await api_spec.transferCase(judgeUser, 'CHANGE_LOCATION');
+//   }
+// });
 
-Scenario('Transfer Online Spec claim 1v2 - not suitable SDO - Other reasons', async ({api_spec}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaimSpec(api_spec);
-    await api_spec.notSuitableSDOspec(judgeUser, 'OTHER_REASONS');
-  }
-});
+// Scenario('Transfer Online Spec claim 1v2 - judge user - not suitable SDO - Other reasons', async ({api_spec}) => {
+//   if (['preview', 'demo'].includes(config.runningEnv)) {
+//     await prepareClaimSpec(api_spec);
+//     await api_spec.transferCase(judgeUser, 'OTHER_REASONS');
+//   }
+// });
 
-AfterSuite(async ({api_spec}) => {
-  await api_spec.cleanUp();
-});
+// AfterSuite(async ({api_spec}) => {
+//   await api_spec.cleanUp();
+// });
