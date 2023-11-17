638cac557aa94a6cb58887f5cfda205d1b767b21
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2023-11-17 12:49:22 +0000

    CIV-0000 Enable EA court testing for dj (#3584)
    * Enable EA court testing for dj
    
    * update lint
    
    * update lint
    
    ---------
    
    Co-authored-by: Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>

--- wa/tasks/summaryJudgmentDirectionsTask.js
+++ wa/tasks/summaryJudgmentDirectionsTask.js
@@ -1,21 +1,36 @@
 module.exports = {
     name: 'Directions after Judgment (Damages)',
     type: 'summaryJudgmentDirections',
+    task_state: 'unassigned',
+    task_system: 'SELF',
+    security_classification: 'PUBLIC',
     task_title: 'Directions after Judgment (Damages)',
-    location_name: 'Central London County Court',
-    location: '20262',
+    location_name: 'Nottingham County Court and Family Court (and Crown)',
+    location: '424213',
     execution_type: 'Case Management Task',
     jurisdiction: 'CIVIL',
-    region: '1',
+    region: '2',
     case_type_id: 'CIVIL',
     case_category: 'Civil',
+    case_name: 'Test Inc',
     auto_assigned: false,
+    warnings: false,
+    warning_list: { values: [] },
     case_management_category: 'Civil',
     work_type_id: 'decision_making_work',
     work_type_label: 'Decision-making work',
-    permissions: { values: [ 'Read', 'Own', 'Manage', 'Cancel', 'Complete', 'Claim', 'Assign', 'Unassign' ] },
-    description: '[Directions - Provisional Summary Judgment](/cases/case-details/${[CASE_REFERENCE]}/trigger/STANDARD_DIRECTION_ORDER_DJ/STANDARD_DIRECTION_ORDER_DJCaseManagementOrder)',
+    permissions: {
+      values: [
+        'Read',          'Own',
+        'Manage',        'Cancel',
+        'CompleteOwn',   'CancelOwn',
+        'Claim',         'Unclaim',
+        'Assign',        'Unassign',
+        'UnclaimAssign'
+      ]
+    },
+    description: '[Directions after Judgment (Damages)](/cases/case-details/${[CASE_REFERENCE]}/trigger/STANDARD_DIRECTION_ORDER_DJ/STANDARD_DIRECTION_ORDER_DJCaseManagementOrder)<br /><br />[Not Suitable for SDO](/cases/case-details/${[CASE_REFERENCE]}/trigger/NotSuitable_SDO/NotSuitable_SDONotSuitableSDO)',
     role_category: 'JUDICIAL',
     minor_priority: 500,
     major_priority: 5000
-};
+  }
