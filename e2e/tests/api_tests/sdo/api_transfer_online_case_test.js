7c445df834b1a733c80eda35c8c5cba45663aa37
Astha Malviya <astha.malviya@HMCTS.NET>
2023-11-29 17:58:34 +0000

    running a PR without functional test

--- e2e/tests/api_tests/sdo/api_transfer_online_case_test.js
+++ e2e/tests/api_tests/sdo/api_transfer_online_case_test.js
@@ -1,29 +1,4 @@
-const config = require('../../../config.js');
-
-const mpScenario1v1 = 'ONE_V_ONE';
-const mpScenario1v2Spec = 'ONE_V_TWO';
-// To use on local because the idam images are different
-// const judgeUser = config.judgeUserWithRegionId1Local;
-// const caseWorkerUser = config.tribunalCaseworkerWithRegionId1Local;
-const fastClaimAmount = '11000';
-
 Feature('Transfer Online Case 1v1 API test - fast claim - unspec @api-unspec @api-tests-1v1 @api-nonprod');
-
-async function prepareClaim(api) {
-  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario1v1, fastClaimAmount);
-  await api.amendClaimDocuments(config.applicantSolicitorUser);
-  await api.notifyClaim(config.applicantSolicitorUser);
-  await api.notifyClaimDetails(config.applicantSolicitorUser);
-  await api.defendantResponse(config.defendantSolicitorUser, mpScenario1v1, null, 'SMALL_CLAIMS');
-  await api.claimantResponse(config.applicantSolicitorUser, mpScenario1v1, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
-}
-
-async function prepareClaimSpec(api_spec) {
-await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
-await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', mpScenario1v2Spec);
-await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO',
-  'JUDICIAL_REFERRAL');
-}
 
 /*Scenario('1v1 full defence unspecified - not suitable SDO - Transfer Case)', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
