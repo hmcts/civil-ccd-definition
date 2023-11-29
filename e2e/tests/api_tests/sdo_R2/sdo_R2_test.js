f6499b5b93a5a030aca253c739ba14f31cc1b60d
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2023-11-29 12:50:36 +0000

    CIV-11494 - Fixing error from unused variables

--- e2e/tests/api_tests/sdo_R2/sdo_R2_test.js
+++ e2e/tests/api_tests/sdo_R2/sdo_R2_test.js
@@ -1,7 +1,6 @@
 const config = require('../../../config.js');
 
 const mpScenario = 'ONE_V_ONE_FLIGHT_DELAY';
-const judgeUser = config.judgeUserWithRegionId1;
 const claimAmountJudge = '11000';
 
 Feature('CCD 1v1 API test flight delay claim @api-tests-1v1 @api-nonprod');
@@ -17,17 +16,6 @@ Scenario.only('1v1 specified - flight delay claim', async ({api_spec}) => {
   // sdo requires judicial_referral, which is not past preview
   if (['preview', 'demo'].includes(config.runningEnv)) {
      await prepareClaim(api_spec, claimAmountJudge);
-     // We should add
-    // await api_spec.createSDO(judgeUser, 'CREATE_SMALL');
-    // await api.evidenceUploadApplicant(config.applicantSolicitorUser);
-    //  await api.evidenceUploadRespondent(config.defendantSolicitorUser, mpScenario);
-    // await api.scheduleHearing(config.hearingCenterAdminWithRegionId1, 'SMALL_CLAIMS');
-    // await api.amendHearingDueDate(config.systemupdate);
-    // await api.hearingFeePaid(config.hearingCenterAdminWithRegionId1);
-    // if (['demo'].includes(config.runningEnv)) {
-    //   await api.triggerBundle(config.systemupdate);
-    // }
-    // await api.createFinalOrder(config.judgeUserWithRegionId1, 'FREE_FORM_ORDER');
   }
 });
 
