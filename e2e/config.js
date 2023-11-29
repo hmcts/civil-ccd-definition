e25f94c49e3b5371155ed49f0f891089be4660bb
skhajuria <sankhajuria@gmail.com>
2023-11-29 22:44:36 +0000

    Enable tests

--- e2e/config.js
+++ e2e/config.js
@@ -310,7 +310,7 @@ module.exports = {
   },
   TestOutputDir: process.env.E2E_OUTPUT_DIR || 'test-results/functional',
   TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY === 'true',
-  runningEnv: 'aat',
+  runningEnv: process.env.ENVIRONMENT,
   runWAApiTest: process.env.RUN_WA_API_TEST == 'true' || false,
   testEarlyAdopterCourts: process.env.TEST_EA_COURTS == 'true' || false,
   claimantSolicitorOrgId: process.env.ENVIRONMENT === 'demo' ? 'B04IXE4' : 'Q1KOKP2',
