d50c61bc94d79f2f0e5af5afdd82fb14825e3944
Azam <muhammad.ghulam@hmcts.net>
2023-12-12 19:04:31 +0000

    CIV-11494 corrected tabulation

--- e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_DiffSol_FlightDelay_test.js
+++ e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_DiffSol_FlightDelay_test.js
@@ -25,25 +25,26 @@ Feature('Claim creation 1v2 Diff Solicitor with flight delay @e2e-spec @e2e-spec
 
 Scenario('Applicant solicitor creates 1v2 Diff LRs specified claim defendant Different LRs for flight delay @create-claim-spec', async ({LRspec}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
-  console.log('AApplicant solicitor creates 1v2 Diff LRs specified claim defendant Different LRs for flight delay @create-claim-spec');
-  var user = config.applicantSolicitorUser;
-  await LRspec.login(config.applicantSolicitorUser);
-  await LRspec.createCaseSpecifiedForFlightDelay('1v2 Different LRs fast claim','organisation', null, respondent1, respondent2, 15450);
-  caseNumber = await LRspec.grabCaseNumber();
+    console.log('AApplicant solicitor creates 1v2 Diff LRs specified claim defendant Different LRs for flight delay @create-claim-spec');
+    var user = config.applicantSolicitorUser;
+    await LRspec.login(config.applicantSolicitorUser);
+    await LRspec.createCaseSpecifiedForFlightDelay('1v2 Different LRs fast claim','organisation', null, respondent1, respondent2, 15450);
+    caseNumber = await LRspec.grabCaseNumber();
 
-  const pbaV3 = await checkToggleEnabled(PBAv3);
-  console.log('Is PBAv3 toggle on?: ' + pbaV3);
+    const pbaV3 = await checkToggleEnabled(PBAv3);
+    console.log('Is PBAv3 toggle on?: ' + pbaV3);
 
-  if (pbaV3) {
-    await apiRequest.setupTokens(user);
-    await serviceRequest.openServiceRequestTab();
-    await serviceRequest.payFee(caseId());
-    await paymentUpdate(caseId(), '/service-request-update-claim-issued',
-      claimData.serviceUpdateDto(caseId(), 'paid'));
-    console.log('Service request update sent to callback URL');
+    if (pbaV3) {
+      await apiRequest.setupTokens(user);
+      await serviceRequest.openServiceRequestTab();
+      await serviceRequest.payFee(caseId());
+      await paymentUpdate(caseId(), '/service-request-update-claim-issued',
+        claimData.serviceUpdateDto(caseId(), 'paid'));
+      console.log('Service request update sent to callback URL');
+    }
+
+    addUserCaseMapping(caseId(), config.applicantSolicitorUser);
   }
-
-  addUserCaseMapping(caseId(), config.applicantSolicitorUser);}
 }).retry(3);
 
 AfterSuite(async  () => {
