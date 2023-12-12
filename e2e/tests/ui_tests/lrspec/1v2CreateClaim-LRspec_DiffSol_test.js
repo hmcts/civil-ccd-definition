8d76b551baf734cbc4cda5062879af8d3a3b806d
Krishna Kishore Nuthalapati <krishnakishorenuthalapati@Krishnas-MacBook-Pro.local>
2023-12-12 16:09:25 +0000

    Adding UI tests

--- e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_DiffSol_test.js
+++ e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_DiffSol_test.js
@@ -46,6 +46,28 @@ Scenario('Applicant solicitor creates 1v2 Diff LRs specified claim defendant Dif
   addUserCaseMapping(caseId(), config.applicantSolicitorUser);
 }).retry(3);
 
+Scenario('Applicant solicitor creates 1v2 Diff LRs specified claim defendant Different LRs for flight delay @create-claim-spec', async ({LRspec}) => {
+  console.log('AApplicant solicitor creates 1v2 Diff LRs specified claim defendant Different LRs for flight delay @create-claim-spec');
+  var user = config.applicantSolicitorUser;
+  await LRspec.login(config.applicantSolicitorUser);
+  await LRspec.createCaseSpecifiedForFlightDelay('1v2 Different LRs fast claim','organisation', null, respondent1, respondent2, 15450);
+  caseNumber = await LRspec.grabCaseNumber();
+
+  const pbaV3 = await checkToggleEnabled(PBAv3);
+  console.log('Is PBAv3 toggle on?: ' + pbaV3);
+
+  if (pbaV3) {
+    await apiRequest.setupTokens(user);
+    await serviceRequest.openServiceRequestTab();
+    await serviceRequest.payFee(caseId());
+    await paymentUpdate(caseId(), '/service-request-update-claim-issued',
+      claimData.serviceUpdateDto(caseId(), 'paid'));
+    console.log('Service request update sent to callback URL');
+  }
+
+  addUserCaseMapping(caseId(), config.applicantSolicitorUser);
+}).retry(3);
+
 Scenario('1v2 Diff LRs Fast Track Claim  - Assign roles to defendants', async () => {
     await assignCaseRoleToUser(caseId(), 'RESPONDENTSOLICITORONE', config.defendantSolicitorUser);
     await assignCaseRoleToUser(caseId(),  'RESPONDENTSOLICITORTWO', config.secondDefendantSolicitorUser);
