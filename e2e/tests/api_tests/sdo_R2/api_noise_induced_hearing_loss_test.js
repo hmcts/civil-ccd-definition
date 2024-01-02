e953c110ee14a35b4a49be68eac6c4d7f62541bd
Azam <muhammad.ghulam@hmcts.net>
2024-01-02 15:56:04 +0000

    CIV-8327 updated judge permissions

--- e2e/tests/api_tests/sdo_R2/api_noise_induced_hearing_loss_test.js
+++ e2e/tests/api_tests/sdo_R2/api_noise_induced_hearing_loss_test.js
@@ -2,10 +2,10 @@ const config = require('../../../config.js');
 
 const mpScenario1v1 = 'ONE_V_ONE';
 const mpScenario1v1Nihl = 'ONE_V_ONE_NIHL';
-const legalAdvUser = config.tribunalCaseworkerWithRegionId4;
+const legalAdvUser = config.judgeUserWithRegionId1;
 // To use on local because the idam images are different
 // const legalAdvUser = config.judgeUserWithRegionId1Local;
-const smallClaimAmount = '100';
+const smallClaimAmount = '11000';
 
 Feature('Noise Induced Hearing Loss Case 1v1 API test - fast claim - unspec @api-unspec @api-tests-1v1 @api-nonprod');
 
