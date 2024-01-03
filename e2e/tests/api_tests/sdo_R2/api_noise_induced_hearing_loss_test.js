6969ff41425c1cafa9cfad2ce495849c2f9b8f2a
Azam <muhammad.ghulam@hmcts.net>
2024-01-03 09:27:54 +0000

    CIV-8327 refactored api test

--- e2e/tests/api_tests/sdo_R2/api_noise_induced_hearing_loss_test.js
+++ e2e/tests/api_tests/sdo_R2/api_noise_induced_hearing_loss_test.js
@@ -5,12 +5,12 @@ const mpScenario1v1Nihl = 'ONE_V_ONE_NIHL';
 const judgeUser = config.testEarlyAdopterCourts ? config.judgeUser2WithRegionId2 : config.judgeUserWithRegionId1;
 // To use on local because the idam images are different
 // const legalAdvUser = config.judgeUserWithRegionId1Local;
-const smallClaimAmount = '11000';
+const claimAmount = '11000';
 
-Feature('Noise Induced Hearing Loss Case 1v1 API test - fast claim - unspec @api-unspec @api-tests-1v1 @api-nonprod');
+Feature('Noise Induced Hearing Loss API test - fast claim - unspec @api-unspec @api-tests-1v1 @api-nonprod');
 
 async function prepareClaim(api) {
-  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario1v1Nihl, smallClaimAmount);
+  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario1v1Nihl, claimAmount);
   await api.amendClaimDocuments(config.applicantSolicitorUser);
   await api.notifyClaim(config.applicantSolicitorUser);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
@@ -18,7 +18,7 @@ async function prepareClaim(api) {
   await api.claimantResponse(config.applicantSolicitorUser, mpScenario1v1, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
 }
 
-Scenario('1v1 unspec request for reconsideration', async ({api}) => {
+Scenario('1v1 unspec create SDO for Noise Induced Hearing Loss', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     await prepareClaim(api);
     await api.createSDO(judgeUser, 'CREATE_FAST');
