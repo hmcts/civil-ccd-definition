31c90daae696678a4f7270b6ffe7673f85cb4c37
Mark Drummond <mark.drummond@version1.com>
2024-01-05 15:46:07 +0000

    test case file view moving documents

--- src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
@@ -49,6 +49,7 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
         new CcdRoleConfig("cui-admin-profile", "PUBLIC"),
         new CcdRoleConfig("cui-nbc-profile", "PUBLIC"),
         new CcdRoleConfig("citizen-profile", "PUBLIC"),
+        new CcdRoleConfig("hearing-viewer", "PUBLIC"),
         new CcdRoleConfig("caseworker-civil-citizen-ui-pcqextractor", "PUBLIC")
     };
 
