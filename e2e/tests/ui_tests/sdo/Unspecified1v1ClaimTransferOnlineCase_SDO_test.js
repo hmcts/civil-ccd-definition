663b93fe3c44b4b3afecae395e05f43cdef688d2
Krishna Kishore Nuthalapati <krishnakishorenuthalapati@Krishnas-MacBook-Pro.local>
2024-01-08 19:39:02 +0000

    Added TOC UI tests

--- e2e/tests/ui_tests/sdo/Unspecified1v1ClaimTransferOnlineCase_SDO_test.js
+++ e2e/tests/ui_tests/sdo/Unspecified1v1ClaimTransferOnlineCase_SDO_test.js
@@ -20,7 +20,7 @@ const respondent1 = {
 
 let caseNumber;
 
-Feature('1v1 - Claim Journey @e2e-unspec @toc @e2e-nightly-prod');
+Feature('1v1 - Claim Journey @toc @e2e-nightly-prod @non-prod-e2e-ft');
 
 Scenario('Applicant solicitor creates claim @create-claim', async ({I}) => {
   await I.login(config.applicantSolicitorUser);
