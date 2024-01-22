f772e00cfb88268d01b58036105e9385fc90d5e2
sankaviv1 <sankavi.mohanraj@version1.com>
2024-01-22 09:13:58 +0000

    address review comment

--- e2e/helpers/carmHelper.js
+++ e2e/helpers/carmHelper.js
@@ -7,15 +7,13 @@ module.exports = {
     if (carmEnabled) {
       console.log('carm enabled, updating submitted date');
       await apiRequest.setupTokens(config.systemupdate);
-      let submittedDate ={};
-      submittedDate = {'submittedDate':'2024-05-10T15:59:50'};
+      const submittedDate = {'submittedDate':'2024-05-10T15:59:50'};
       await testingSupport.updateCaseData(caseId, submittedDate);
       console.log('submitted date update to after carm date');
     } else {
       console.log('carm not enabled, updating submitted date');
       await apiRequest.setupTokens(config.systemupdate);
-      let submittedDate ={};
-      submittedDate = {'submittedDate':'2022-05-10T15:59:50'};
+      const submittedDate = {'submittedDate':'2022-05-10T15:59:50'};
       await testingSupport.updateCaseData(caseId, submittedDate);
       console.log('submitted date update to before carm date');
     }
