ae15d5e610a626db0967b82dabb2b984e7978898
Douglas Rice <douglas.rice@hmcts.net>
2024-01-23 21:47:12 +0000

    minor tweek

--- e2e/config.js
+++ e2e/config.js
@@ -11,31 +11,31 @@ module.exports = {
     url: 'http://localhost:5555'
   },
   url: {
-  //   // manageCase: process.env.URL || 'https://manage-case-wa-int.demo.platform.hmcts.net',
-  //   // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
-  //   // ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
-  //   // dmStore:process.env.DM_STORE_URL || 'http://dm-store-demo.service.core-compute-demo.internal',
-  //   // idamApi: process.env.IDAM_API_URL || 'https://idam-api.demo.platform.hmcts.net',
-  //   // civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-demo.service.core-compute-demo.internal',
-  //   // // waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-demo.service.core-compute-demo.internal'
-  //
-  //   // for preview
-  //   // manageCase: 'https://xui-civil-ccd-pr-3186.preview.platform.hmcts.net',
-  //   // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
-  //   // ccdDataStore: 'https://ccd-data-store-api-civil-ccd-pr-3186.preview.platform.hmcts.net',
-  //   // dmStore: 'http://dm-store-aat.service.core-compute-aat.internal',
-  //   // idamApi: 'https://idam-api.aat.platform.hmcts.net',
-  //   // civilService: 'https://civil-ccd-pr-3186.preview.platform.hmcts.net',
-  //
-  //
-  //   /*
-  //   manageCase:  'https://manage-case.aat.platform.hmcts.net/',
-  //   authProviderApi:  'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
-  //   ccdDataStore: 'http://ccd-data-store-api-aat.service.core-compute-aat.internal',
-  //   dmStore:'http://dm-store-aat.service.core-compute-aat.internal',
-  //   idamApi:  'https://idam-api.aat.platform.hmcts.net',
-  //   civilService: 'http://civil-service-aat.service.core-compute-aat.internal',
-  //   waTaskMgmtApi: 'http://wa-task-management-api-aat.service.core-compute-aat.internal',*/
+    // manageCase: process.env.URL || 'https://manage-case-wa-int.demo.platform.hmcts.net',
+    // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
+    // ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
+    // dmStore:process.env.DM_STORE_URL || 'http://dm-store-demo.service.core-compute-demo.internal',
+    // idamApi: process.env.IDAM_API_URL || 'https://idam-api.demo.platform.hmcts.net',
+    // civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-demo.service.core-compute-demo.internal',
+    // // waTaskMgmtApi: process.env.WA_TASK_MGMT_URL || 'http://wa-task-management-api-demo.service.core-compute-demo.internal'
+
+    // for preview
+    // manageCase: 'https://xui-civil-ccd-pr-3186.preview.platform.hmcts.net',
+    // authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
+    // ccdDataStore: 'https://ccd-data-store-api-civil-ccd-pr-3186.preview.platform.hmcts.net',
+    // dmStore: 'http://dm-store-aat.service.core-compute-aat.internal',
+    // idamApi: 'https://idam-api.aat.platform.hmcts.net',
+    // civilService: 'https://civil-ccd-pr-3186.preview.platform.hmcts.net',
+
+
+    /*
+    manageCase:  'https://manage-case.aat.platform.hmcts.net/',
+    authProviderApi:  'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
+    ccdDataStore: 'http://ccd-data-store-api-aat.service.core-compute-aat.internal',
+    dmStore:'http://dm-store-aat.service.core-compute-aat.internal',
+    idamApi:  'https://idam-api.aat.platform.hmcts.net',
+    civilService: 'http://civil-service-aat.service.core-compute-aat.internal',
+    waTaskMgmtApi: 'http://wa-task-management-api-aat.service.core-compute-aat.internal',*/
 
     manageCase: process.env.URL || 'http://localhost:3333',
     authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://localhost:4502',
--- e2e/pages/hearing/requestHearing.page.js
+++ e2e/pages/hearing/requestHearing.page.js
@@ -52,8 +52,7 @@ module.exports = {
   async selectParticipantAttendance() {
     await I.waitForText('Participant attendance');
     await I.waitForElement(this.fields.selectAttendanceType);
-    await I.checkOption(this.fields.selectAttendanceType);
-    //await I.click(this.fields.selectAttendanceType);
+    await I.click(this.fields.selectAttendanceType);
     let noOfIndividuals = await I.grabNumberOfVisibleElements('.party-row');
 
     for (let i=0; i<noOfIndividuals; i++) {
