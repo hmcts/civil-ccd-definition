35e3c3b1d68f52a5719e0b55746ba211b7c5a353
Douglas Rice <douglas.rice@hmcts.net>
2024-01-31 09:52:45 +0000

    add create/delete account code

--- e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_mediation_test.js
+++ e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_mediation_test.js
@@ -1,8 +1,11 @@
 const config = require('../../../config.js');
+const {createAccount, deleteAccount} = require('../../specClaimHelpers/api/idamHelper');
 
 const claimType = 'SmallClaims';
 let carmEnabled = false;
 let claimRef;
+let caseData;
+let claimNumber;
 
 let mediationAdmin = config.localMediationTests ? config.nbcUserLocal : config.nbcUserWithRegionId1;
 
@@ -15,6 +18,17 @@ async function prepareClaim(api_spec_cui, carmEnabled) {
 
 // set config.localMediationTests to true to run locally
 Feature('Unsuccessful mediation for spec small claim with unrepresented defendant @cui-carm @api-nonprod');
+
+Before(async ({api}) => {
+  if (['preview', 'demo'  ].includes(config.runningEnv)) {
+    await createAccount(config.defendantCitizenUser.email, config.defendantCitizenUser.password);
+    claimRef = await api.createSpecifiedClaim(config.applicantSolicitorUser, '', claimType, carmEnabled, 'Company');
+    console.log('claimRef has been created Successfully    <===>  '  , claimRef);
+    caseData = await api.retrieveCaseData(config.adminUser, claimRef);
+    claimNumber =  caseData.legacyCaseReference;
+    console.log('claim number', claimNumber);
+  }
+});
 
 Scenario('CARM enabled', async ({api_spec_cui}) => {
   carmEnabled = true;
@@ -30,6 +44,7 @@ Scenario('CARM not enabled', async ({api_spec_cui}) => {
 
 AfterSuite(async ({api_spec_cui}) => {
   await api_spec_cui.cleanUp();
+  await deleteAccount(config.defendantCitizenUser.email);
 });
 
 
