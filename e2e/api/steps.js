5e09bc9d3745945fb369158ede60ae619361d13d
pliao-hmcts <113367232+pliao-hmcts@users.noreply.github.com>
2024-02-09 13:28:54 +0000

    CIV-12451 No hearing fee check for 'other' hearings (#3962)
    * CIV-12451 No hearing fee check for 'other' hearings
    
    * CIV-12537 temp change for hearing
    
    ---------
    
    Co-authored-by: mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
    Co-authored-by: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>

--- e2e/api/steps.js
+++ e2e/api/steps.js
@@ -1107,8 +1107,11 @@ module.exports = {
     for (let pageId of Object.keys(scheduleData.valid)) {
       await assertValidData(scheduleData, pageId);
     }
-
-    await assertSubmittedEvent('HEARING_READINESS', null, false);
+    let expectedState = 'HEARING_READINESS';
+    if (allocatedTrack === 'OTHER') {
+      expectedState = 'PREPARE_FOR_HEARING_CONDUCT_HEARING';
+    }
+    await assertSubmittedEvent(expectedState, null, false);
     await waitForFinishedBusinessProcess(caseId);
   },
 
