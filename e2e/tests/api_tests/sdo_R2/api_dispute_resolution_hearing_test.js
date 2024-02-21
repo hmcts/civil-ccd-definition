a26108062681abf60bc894c24c143e4062a9ca74
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2024-02-21 09:17:55 +0000

    CIV-12409 add case progression tests

--- e2e/tests/api_tests/sdo_R2/api_dispute_resolution_hearing_test.js
+++ e2e/tests/api_tests/sdo_R2/api_dispute_resolution_hearing_test.js
@@ -2,6 +2,7 @@ const config = require('../../../config.js');
 
 const mpScenario1v1 = 'ONE_V_ONE';
 const judgeUser = config.testEarlyAdopterCourts ? config.judgeUser2WithRegionId2 : config.judgeUserWithRegionId1;
+const hearingCenterAdminToBeUsed = config.testEarlyAdopterCourts ? config.hearingCenterAdminWithRegionId2 : config.hearingCenterAdminWithRegionId1;
 // To use on local because the idam images are different
 //const judgeUser = config.judgeUserWithRegionId1Local;
 const claimAmount = '100';
@@ -13,14 +14,23 @@ async function prepareClaim(api) {
   await api.amendClaimDocuments(config.applicantSolicitorUser);
   await api.notifyClaim(config.applicantSolicitorUser);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
-  await api.defendantResponse(config.defendantSolicitorUser, mpScenario1v1, null, 'FAST_CLAIM_NIHL');
-  await api.claimantResponse(config.applicantSolicitorUser, mpScenario1v1, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
+  await api.defendantResponse(config.defendantSolicitorUser, mpScenario1v1, null);
+  await api.claimantResponse(config.applicantSolicitorUser, mpScenario1v1, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO');
 }
 
 Scenario('1v1 unspec create SDO for DRH', async ({api}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     await prepareClaim(api);
     await api.createSDO(judgeUser, 'CREATE_SMALL_DRH');
+    await api.evidenceUploadApplicant(config.applicantSolicitorUser);
+    await api.evidenceUploadRespondent(config.defendantSolicitorUser, mpScenario1v1);
+    await api.scheduleHearing(hearingCenterAdminToBeUsed, 'SMALL_CLAIMS');
+    await api.amendHearingDueDate(config.systemupdate);
+    await api.hearingFeePaid(hearingCenterAdminToBeUsed);
+    if (['demo'].includes(config.runningEnv)) {
+      await api.triggerBundle(config.systemupdate);
+    }
+    await api.createFinalOrder(judgeUser, 'FREE_FORM_ORDER');
   }
 });
 
