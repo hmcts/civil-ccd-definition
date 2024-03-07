0e5299a4e8706d776be920528f186ec3c8cfdcfc
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2024-03-07 10:30:45 +0000

    Update run-functional-tests.sh

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -3,9 +3,9 @@ set -ex
 
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
-export CCD_UI_TESTS=true
+export CCD_UI_TESTS=false
 if [ ${ENVIRONMENT} == preview ]; then
-  yarn test:non-prod-e2e-ft
+  yarn test:api-nonprod
 else
-  yarn test:master-e2e-ft
+  yarn test:api-prod
 fi
