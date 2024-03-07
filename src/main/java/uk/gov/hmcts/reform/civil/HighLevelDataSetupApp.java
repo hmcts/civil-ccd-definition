e17f346cb5d9f1600ba169e44c0bcd44f8a64784
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2024-03-07 14:59:46 +0000

    Update HighLevelDataSetupApp.java

--- src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
@@ -100,23 +100,10 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     }
 
     @Override
-    protected boolean shouldTolerateDataSetupFailure(Throwable e) {
-        int httpStatusCode504 = 504;
-        String[] lowerEnv = {"preview", "aat", "demo", "perftest", "ithc"};
-        if (Arrays.asList(lowerEnv).contains(System.getenv("ENVIRONMENT"))) {
-            return true;
-        }
-        if (e instanceof ImportException) {
-            ImportException importException = (ImportException) e;
-            return importException.getHttpStatusCode() == httpStatusCode504;
-        }
-        if(e instanceof SSLException){
-            return true;
-        }
-        if(e instanceof AEADBadTagException){
+    protected boolean shouldTolerateDataSetupFailure(){
+        if((BeftaMain.getConfig().getDefinitionStoreUrl().contains("-preview.")){
             return true;
         }
         return false;
     }
-
 }
