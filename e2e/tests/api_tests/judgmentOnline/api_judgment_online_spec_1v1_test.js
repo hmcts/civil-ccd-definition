8518e6b0ca55b335446266989ef7f945e1e01228
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-03-08 16:31:47 +0000

    CIV-12915 Test changes for Refer To Judge (Defence Received) option list name changed

--- e2e/tests/api_tests/judgmentOnline/api_judgment_online_spec_1v1_test.js
+++ e2e/tests/api_tests/judgmentOnline/api_judgment_online_spec_1v1_test.js
@@ -2,7 +2,8 @@
 
 const config = require('../../../config.js');
 const mpScenario = 'ONE_V_ONE';
-const caseWorkerUser = config.hearingCenterAdminWithRegionId1;
+const judgeUser = config.testEarlyAdopterCourts ? config.judgeUser2WithRegionId2 : config.judgeUserWithRegionId1;
+const caseWorkerUser = config.testEarlyAdopterCourts ? config.hearingCenterAdminWithRegionId2 : config.hearingCenterAdminWithRegionId1;
 // to use on local because the idam images are different
 // const caseWorkerUser = config.judgeUserWithRegionId1Local;
 // const legalAdvUser = config.tribunalCaseworkerWithRegionId1Local;
@@ -43,9 +44,9 @@ Scenario('Record Judgment Spec claim 1v1 with mark paid in full', async ({I, api
     await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', mpScenario,
       'AWAITING_APPLICANT_INTENTION');
     console.log('--sdo--');
-    await api_spec.createSDO(config.judgeUserWithRegionId1Local, 'CREATE_FAST_NO_SUM');
+    await api_spec.createSDO(config.judgeUserWithRegionId1, 'CREATE_FAST_NO_SUM');
     console.log('--createFinalOrderJO--');
-    await api_spec.createFinalOrderJO(config.judgeUserWithRegionId1Local, 'FREE_FORM_ORDER');
+    await api_spec.createFinalOrderJO(config.judgeUserWithRegionId1, 'FREE_FORM_ORDER');
     console.log('--recordJudgment--');
     await api_spec.recordJudgment(caseWorkerUser, mpScenario, 'DETERMINATION_OF_MEANS', 'PAY_IN_INSTALMENTS');
     console.log('--markJudgmentPaid--');
