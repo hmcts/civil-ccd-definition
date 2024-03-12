620ae46952145d63005532c03435d65675e6a7e8
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2024-03-12 10:17:48 +0000

    Update api_1v1_spec_cui_mediation_test.js

--- e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_mediation_test.js
+++ e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_mediation_test.js
@@ -14,7 +14,7 @@ async function prepareClaim(api_spec_cui, carmEnabled) {
 }
 
 // set config.localMediationTests to true to run locally
-Feature('Unsuccessful mediation for spec small claim with unrepresented defendant @cui-carm @api-nonprod');
+Feature('Unsuccessful mediation for spec small claim with unrepresented defendant @cui-carm @api-nonprod @non-prod-e2e-ft');
 
 Before(async () => {
   await createAccount(config.defendantCitizenUser2.email, config.defendantCitizenUser2.password);
