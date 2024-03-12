82a4c25acfaffddd8213a654179d8aee7eb6f014
Azam <muhammad.ghulam@hmcts.net>
2024-03-12 10:16:58 +0000

    CIV-0000 refactored api test

--- e2e/tests/api_tests/sdo_R2/sdo_R2_test.js
+++ e2e/tests/api_tests/sdo_R2/sdo_R2_test.js
@@ -1,7 +1,6 @@
 const config = require('../../../config.js');
 
 const mpScenario = 'ONE_V_ONE_FLIGHT_DELAY';
-const claimAmountFastTrack = '11000';
 const claimAmountSmallTrack = '9000';
 
 Feature('CCD 1v1 API test flight delay claim @api-tests-1v1 @api-nonprod');
@@ -27,13 +26,6 @@ Scenario('1v1 specified - flight delay Small-claim', async ({api_spec}) => {
   }
 });
 
-Scenario('1v1 specified - flight delay Fast-claim', async ({api_spec}) => {
-  // sdo requires judicial_referral, which is not past preview
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api_spec, claimAmountFastTrack);
-  }
-});
-
 Scenario('1v1 specified - flight delay other option Small-claim', async ({api_spec}) => {
   // sdo requires judicial_referral, which is not past preview
   if (['preview', 'demo'].includes(config.runningEnv)) {
@@ -41,13 +33,6 @@ Scenario('1v1 specified - flight delay other option Small-claim', async ({api_sp
   }
 });
 
-Scenario('1v1 specified - flight delay other option Fast-claim', async ({api_spec}) => {
-  // sdo requires judicial_referral, which is not past preview
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaimOtherOption(api_spec, claimAmountFastTrack);
-  }
-});
-
-AfterSuite(async ({api}) => {
-  await api.cleanUp();
+AfterSuite(async ({api_spec}) => {
+  await api_spec.cleanUp();
 });
