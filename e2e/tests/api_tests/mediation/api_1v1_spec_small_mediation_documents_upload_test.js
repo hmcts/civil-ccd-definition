8391f2c81891764285d1973c81afe869b0445e4d
sankaviv1 <sankavi.mohanraj@version1.com>
2024-04-11 12:29:29 +0100

    revert last commit

--- e2e/tests/api_tests/mediation/api_1v1_spec_small_mediation_documents_upload_test.js
+++ e2e/tests/api_tests/mediation/api_1v1_spec_small_mediation_documents_upload_test.js
@@ -72,7 +72,7 @@ Scenario('1v2 different solicitor claimant and defendant upload mediation docume
   await api_spec.uploadMediationDocuments(config.secondDefendantSolicitorUser);
 });
 
-Scenario.only('1v2 different solicitor claimant and defendant upload mediation documents - CARM enabled', async ({api_spec}) => {
+Scenario('1v2 different solicitor claimant and defendant upload mediation documents - CARM enabled', async ({api_spec}) => {
   await prepareClaim1v2DiffSol(api_spec, true);
   await api_spec.mediationUnsuccessful(mediationAdminRegion1, true);
   await api_spec.uploadMediationDocuments(config.applicantSolicitorUser);
