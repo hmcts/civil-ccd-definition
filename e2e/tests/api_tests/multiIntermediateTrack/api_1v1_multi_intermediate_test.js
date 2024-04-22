5cc5ef32b682b39945fb533b6b53b6f8a2b4571c
Mark Drummond <mark.drummond@version1.com>
2024-04-22 15:14:30 +0100

    refactored

--- e2e/tests/api_tests/multiIntermediateTrack/api_1v1_multi_intermediate_test.js
+++ e2e/tests/api_tests/multiIntermediateTrack/api_1v1_multi_intermediate_test.js
@@ -8,11 +8,11 @@ const intermediateTrack = '99000';
 
 Feature('CCD 1v1 API test unspec intermediate and multi track @api-unspec-multi-intermediate @api-nonprod');
 
-Scenario('Create Multi Track claim', async ({api}) => {
+Scenario('1v1 Create Unspecified Multi Track claim', async ({api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, multiTrack);
 });
 
-Scenario('Create Intermediate Track claim', async ({api}) => {
+Scenario('1v1 Create Unspecified Intermediate Track claim', async ({api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, intermediateTrack);
 });
 
