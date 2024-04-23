3b1c4fa9f3a455119365e20aca0cd38c1ddcd08f
Gareth Lancaster <90632240+Gareth40343@users.noreply.github.com>
2024-04-23 16:44:30 +0100

    Reverted functional script

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -3,10 +3,9 @@ set -ex
 
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
-export CCD_UI_TESTS=false
+export CCD_UI_TESTS=true
 if [ ${ENVIRONMENT} == preview ]; then
-#  yarn test:non-prod-e2e-ft
-  yarn test:api-nonprod
+  yarn test:non-prod-e2e-ft
 else
   yarn test:master-e2e-ft
 fi
