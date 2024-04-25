384268f8186f1bb92a6dd07b2f4196bb1a77a3e1
bhagyashreesharma90 <bhagyashree.sharma@hmcts.net>
2024-04-25 16:32:37 +0100

    CIV-10495 update test

--- e2e/api/steps_LRspec.js
+++ e2e/api/steps_LRspec.js
@@ -1128,7 +1128,11 @@ module.exports = {
           id: '9f30e576-f5b7-444f-8ba9-27dabb21d966' } ],
           registrationTypeRespondentTwo: []
       };
-      state = 'PROCEEDS_IN_HERITAGE_SYSTEM';
+      if (isJudgmentOnlineLive) {
+        state = 'All_FINAL_ORDERS_ISSUED';
+      } else {
+        state = 'PROCEEDS_IN_HERITAGE_SYSTEM';
+      }
       await validateEventPagesDefaultJudgments(data.DEFAULT_JUDGEMENT_SPEC_2V1, scenario,isDivergent);
     } else {
       registrationData = {
