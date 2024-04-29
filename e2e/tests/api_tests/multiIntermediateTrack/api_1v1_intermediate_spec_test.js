05135facd89dcc3c872aed516f7b994f19700d3e
Mark Drummond <mark.drummond@version1.com>
2024-04-29 17:02:38 +0100

    API tests for claimant response

--- e2e/tests/api_tests/multiIntermediateTrack/api_1v1_intermediate_spec_test.js
+++ e2e/tests/api_tests/multiIntermediateTrack/api_1v1_intermediate_spec_test.js
@@ -6,7 +6,7 @@ const claimAmountPenniesIntermediate = '9900000';
 const claimAmountIntermediate = '99000';
 const defense = 'FULL_DEFENCE';
 
-Feature('CCD 1v1 API test spec intermediate  track @api-spec-multi-intermediate @api-nonprod');
+Feature('CCD 1v1 API test spec intermediate  track @api-spec-multi-intermediate @api-nonprod @SAME');
 
 async function prepareClaim(api_spec, mpScenario) {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, false, true, claimAmountPenniesIntermediate);
@@ -14,10 +14,10 @@ async function prepareClaim(api_spec, mpScenario) {
 Scenario('1v1 full defence Intermediate claim Specified @api-nonprod-specified', async ({I, api_spec}) => {
   const mpScenario = 'ONE_V_ONE';
   await prepareClaim(api_spec, mpScenario, defense);
-  await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_ONE', 'AWAITING_APPLICANT_INTENTION', false, true, claimAmountIntermediate);
+  //await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_ONE', 'AWAITING_APPLICANT_INTENTION', false, true, claimAmountIntermediate);
 });
 
-AfterSuite(async  ({api_spec}) => {
-  await api_spec.cleanUp();
-});
+// AfterSuite(async  ({api_spec}) => {
+//   await api_spec.cleanUp();
+// });
 
