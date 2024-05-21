a097b5ce54dcf03156b0d54b0f7a3b8ee0d9735f
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2024-05-21 14:25:31 +0100

    update

--- e2e/pages/respondToClaim/responseType.page.js
+++ e2e/pages/respondToClaim/responseType.page.js
@@ -1,12 +1,12 @@
 
 const {I} = inject();
 
-const options = {
+/*const options = {
   fullDefence: 'Reject all of the claim',
   fullAdmission: 'Admit all of the claim',
   partAdmission: 'Admit part of the claim',
   counterClaim: 'Reject all of the claim and wants to counterclaim'
-};
+};*/
 
 module.exports = {
   fields: (party) => ({
