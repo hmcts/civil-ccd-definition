0588ef122adccfb4bf10f639f83adfb54b3548e5
Sherlyn Khaw <sherlyn.khaw@evoco.co.uk>
2024-05-21 18:05:54 +0100

    add update to api test

--- e2e/tests/api_tests/multiIntermediateTrack/api_intermediate_unspec_test.js
+++ e2e/tests/api_tests/multiIntermediateTrack/api_intermediate_unspec_test.js
@@ -27,7 +27,7 @@ async function prepareClaim(api, mpScenario, claimAmount) {
   await api.notifyClaim(config.applicantSolicitorUser);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
   await api.defendantResponse(config.defendantSolicitorUser, mpScenario, null, track);
-  await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', '', track);
+  await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'JUDICIAL_REFERRAL', 'FOR_SDO', track);
 }
 
 // AfterSuite(async  ({api}) => {
