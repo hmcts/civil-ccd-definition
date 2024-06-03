a00bbad93e9bd943c66a1314ab322bc03a7d99a5
miguelMolina3691 <miguel.eduardomolinachaparro@version1.com>
2024-06-03 11:26:57 +0100

    CIV-13698-api-test check test

--- e2e/api/steps_LRspec.js
+++ e2e/api/steps_LRspec.js
@@ -279,6 +279,10 @@ const assertValidDataForEvidenceUpload = async (data, pageId, solicitor) => {
       caseData.sdoOrderDocument = responseBody.data.sdoOrderDocument;
     }
 
+    if (responseBody.data.requestForReconsiderationDeadline) {
+      caseData.requestForReconsiderationDeadline = responseBody.data.requestForReconsiderationDeadline;
+    }
+
     // noinspection EqualityComparisonWithCoercionJS
     if (caseData.drawDirectionsOrder && caseData.drawDirectionsOrder.judgementSum
       && responseBody.data.drawDirectionsOrder && responseBody.data.drawDirectionsOrder.judgementSum
@@ -1254,7 +1258,6 @@ module.exports = {
       delete caseData['sdoR2FastTrackCreditHire'];
       delete caseData['sdoDJR2TrialCreditHire'];
     }
-    delete caseData['requestForReconsiderationDeadline'];
     caseData = returnedCaseData;
     assertContainsPopulatedFields(returnedCaseData);
     if (response === 'CREATE_SMALL') {
