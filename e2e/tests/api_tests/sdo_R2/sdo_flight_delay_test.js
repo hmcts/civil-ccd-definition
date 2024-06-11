13addc37ae10b9d75b3e6ef1ddb792f1ae232ca9
ShwetaTandel-hmcts <130586258+ShwetaTandel-hmcts@users.noreply.github.com>
2024-06-11 19:40:17 +0100

    CIV-13835 SDO R2 Release Changes (#4380)
    * SDO R2 Release Changes
    
    * revert local changes
    
    * Add searchable fields
    
    * Fix Feature Toggle flag
    
    * Merge Master
    
    * Remove duplicate fields
    
    * remove duplicate field
    
    * Move to prod tags
    
    * Rename
    
    * Updated non prod test
    
    * CIV-13835 updated Searchable to N
    
    * Remove prod tag
    
    * Skipping this test
    
    ---------
    
    Co-authored-by: Azam <106387766+Azam-Hmcts@users.noreply.github.com>
    Co-authored-by: Azam <muhammad.ghulam@hmcts.net>

--- e2e/tests/api_tests/sdo_R2/sdo_flight_delay_test.js
+++ e2e/tests/api_tests/sdo_R2/sdo_flight_delay_test.js
@@ -20,11 +20,10 @@ async function prepareClaimOtherOption(api_spec, claimAmount) {
   await api_spec.claimantResponseForFlightDelay(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_ONE', 'AWAITING_APPLICANT_INTENTION');
 }
 
-Feature('Create SDO SmallTrack- Flight Delay - 1v1 - spec @api-specified @api-nonprod');
+Feature('Create SDO SmallTrack- Flight Delay - 1v1 - spec @api-specified @api-prod  @api-r2-sdo');
 
 Scenario('1v1 full defence unspecified - judge draws small claims WITHOUT sum of damages - flight delay @api-r2-sdo', async ({api_spec}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
-    await prepareClaim(api_spec, claimAmountSmallTrack);
+      await prepareClaim(api_spec, claimAmountSmallTrack);
     await api_spec.createSDO(judgeUser, 'CREATE_SMALL');
     await api_spec.evidenceUploadApplicant(config.applicantSolicitorUser, mpScenario1v1);
     await api_spec.evidenceUploadRespondent(config.defendantSolicitorUser, mpScenario1v1);
@@ -35,15 +34,11 @@ Scenario('1v1 full defence unspecified - judge draws small claims WITHOUT sum of
       await api_spec.triggerBundle(config.systemupdate);
     }
     await api_spec.createFinalOrderJO(judgeUser, 'FREE_FORM_ORDER');
-  }
 });
 
 Scenario('1v1 specified - flight delay other option Small-claim @api-spec-full-defence', async ({api_spec}) => {
-  if (['preview', 'demo'].includes(config.runningEnv)) {
     await prepareClaimOtherOption(api_spec, claimAmountSmallTrack);
-  }
 });
-
 
 AfterSuite(async ({api_spec}) => {
   await api_spec.cleanUp();
