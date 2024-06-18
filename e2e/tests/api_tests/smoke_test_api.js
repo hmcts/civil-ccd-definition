8b005cff19d5669d5c7dede93f045171ac5aa976
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2024-06-18 11:58:52 +0100

    update

--- e2e/tests/api_tests/smoke_test_api.js
+++ e2e/tests/api_tests/smoke_test_api.js
@@ -7,3 +7,7 @@ Feature('Smoke tests @smoke-tests-unspec');
 Scenario('Create unspec claim to make sure ccd and bpmn are working fine', async ({api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
 });
+
+AfterSuite(async  ({api}) => {
+  await api.cleanUp();
+});
