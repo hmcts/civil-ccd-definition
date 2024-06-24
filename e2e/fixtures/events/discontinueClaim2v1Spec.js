9f57caa45cb95f97046d780ac6dbcf4cda808e28
JamiS <santoshini.jami@hmcts.net>
2024-06-24 17:38:04 +0100

    CIV-14128: Added API test for Discontinue Claim

---
+++ e2e/fixtures/events/discontinueClaim2v1Spec.js
@@ -0,0 +1,24 @@
+const {date, element} = require("../../api/dataHelper");
+module.exports = {
+  discontinueClaim: () => {
+    const data = {
+      userInput: {
+        MultipleClaimant:{
+          addApplicant2 : 'Yes',
+          claimantWhoIsDiscontinuing : {
+            value: {
+              code: 'bfcf6412-1b23-45c6-b451-224ab5ec1703',
+              label: 'Test Inc'
+            }
+          }
+        },
+        claimantConsent:{
+          selectedClaimantForDiscontinuance : 'Test Inc',
+          claimantsConsentToDiscontinuance : 'Yes'
+        }
+      }
+    };
+
+    return data;
+  }
+};
