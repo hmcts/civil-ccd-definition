581b5ab213f0e20e669465c0aab1e471e9e09ae5
marianadpereira <mariana.pereira@HMCTS.NET>
2024-08-06 17:08:42 +0100

    changing config

--- codecept.conf.js
+++ codecept.conf.js
@@ -32,7 +32,7 @@ exports.config = {
     Playwright: {
       url: process.env.URL || 'http://localhost:3333',
       show: process.env.SHOW_BROWSER_WINDOW === 'true' || false,
-      waitForTimeout: parseInt(process.env.WAIT_FOR_TIMEOUT_MS || 30000),
+      waitForTimeout: parseInt(process.env.WAIT_FOR_TIMEOUT_MS || 50000),
       windowSize: '1280x960',
       browser: 'chromium',
       timeout: 10000,
