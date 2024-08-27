2ad4c89f1d46cfa7da5fc6f3bc12af2ac42172ac
Mike Lemos <gobii@me.com>
2024-08-27 12:33:05 +0100

    DTSCCI-763 preview dev wa (#4760)
    * DTSCCI-763 adding scripts to import definitions when creating dev env in preview
    
    * DTSCCI-763 adding scripts to import definitions when creating dev env in preview
    
    * DTSCCI-763 adding scripts to import definitions when creating dev env in preview

---
+++ bin/setup-devuser-preview-env.sh
@@ -0,0 +1,21 @@
+camundaBranch=${1:-master}
+ccdBranch=${2:-master}
+dmnBranch=${2:-master}
+waStandaloneBranch=${2:-master}
+
+echo "Loading Environment Variables"
+source ./bin/variables/load-dev-user-preview-environment-variables.sh
+echo "Importing Roles to the CCD pod"
+./bin/add-roles.sh
+echo "Importing Camunda definitions"
+./bin/pull-latest-camunda-files.sh ${camundaBranch}
+echo "Importing CCD definitions"
+./bin/build-release-ccd-definition.sh preview
+ccdDefinitionFilePath="$(realpath $(dirname ${0})/..)/build/ccd-release-config/civil-ccd-preview.xlsx"
+./bin/utils/ccd-import-definition.sh ${ccdDefinitionFilePath}
+
+./bin/pull-latest-dmn-files.sh ${dmnBranch}
+./bin/pull-latest-camunda-wa-files.sh ${waStandaloneBranch}
+
+echo "ENV variables set for devuser-preview environment."
+echo "CDAM_REDIRECT_URL: $CCD_IDAM_REDIRECT_URL"
