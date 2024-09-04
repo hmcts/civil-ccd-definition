c6d75d7a6362269bd9c3e17e4e79ce2f420f322f
mfallonhmcts <114912573+mfallonhmcts@users.noreply.github.com>
2024-09-04 13:51:48 +0100

    Update api_request_for_reconsideration_test.js

--- e2e/tests/api_tests/sdo_R2/api_request_for_reconsideration_test.js
+++ e2e/tests/api_tests/sdo_R2/api_request_for_reconsideration_test.js
@@ -12,14 +12,14 @@ await api_spec_small.claimantResponse(config.applicantSolicitorUser, true);
 
 Feature('Request for reconsideration - 1v1 - spec @api-specified @api-nonprod @api-r2-sdo'); // reinstate @api-nightly-prod tag when issue described on CIV-14871 is resolved 
 
-Scenario.skip('1v1 spec request for reconsideration for uphold previous order', async ({api_spec_small}) => {
+Scenario('1v1 spec request for reconsideration for uphold previous order', async ({api_spec_small}) => {
     await prepareClaimSpec(api_spec_small);
     await api_spec_small.createSDO(legalAdvUser, 'CREATE_SMALL_NO_SUM');
     await api_spec_small.requestForReconsideration(config.applicantSolicitorUser, 'Applicant');
     await api_spec_small.judgeDecisionOnReconsiderationRequest(judgeUserReg1, 'YES');
 });
 
-Scenario.skip('1v1 spec request for reconsideration for create new SDO', async ({api_spec_small}) => {
+Scenario('1v1 spec request for reconsideration for create new SDO', async ({api_spec_small}) => {
     await prepareClaimSpec(api_spec_small);
     await api_spec_small.createSDO(legalAdvUser, 'CREATE_SMALL_NO_SUM');
     await api_spec_small.requestForReconsideration(config.defendantSolicitorUser, 'Respondent1');
@@ -28,7 +28,7 @@ Scenario.skip('1v1 spec request for reconsideration for create new SDO', async (
     await api_spec_small.createSDO(judgeUserReg1, 'CREATE_SMALL_NO_SUM');
 });
 
-Scenario.skip('1v1 spec request for reconsideration for create general order', async ({api_spec_small}) => {
+Scenario('1v1 spec request for reconsideration for create general order', async ({api_spec_small}) => {
     await prepareClaimSpec(api_spec_small);
     await api_spec_small.createSDO(legalAdvUser, 'CREATE_SMALL_NO_SUM');
     await api_spec_small.requestForReconsideration(config.defendantSolicitorUser,'Respondent1');
