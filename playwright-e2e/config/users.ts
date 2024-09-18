e72e069a3a31047a43ba144d4a5d4a855697cef0
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-09-18 09:09:20 +0100

    add more users

--- playwright-e2e/config/users.ts
+++ playwright-e2e/config/users.ts
@@ -11,56 +11,135 @@ const iacDefaultPassword = process.env.IAC_DEFAULT_PASSWORD;
 const defaultPasswordSystemUser = process.env.SYSTEM_USER_PASSWORD;
 
 const getUser = (user: User): User => {
-  return (
-    UserStateHelper.getUserFromState(user.key) ?? user
-  );
+  return UserStateHelper.getUserFromState(user.key) ?? user;
 };
 
-export const claimantSolicitor: User = getUser(
-  {
-    email: "hmcts.civil+organisation.1.solicitor.1@gmail.com",
-    password: defaultPassword,
-    key: UserKey.CLAIMANT_SOLICITOR,
-    role: UserRole.CASEWORKER,
-    orgId: config.environment === Environment.DEMO ? "B04IXE4" : "Q1KOKP2"
-  }
-);
+export const claimantSolicitorUser: User = getUser({
+  email: "hmcts.civil+organisation.1.solicitor.1@gmail.com",
+  password: defaultPassword,
+  key: UserKey.CLAIMANT_SOLICITOR,
+  role: UserRole.CASEWORKER,
+  orgId: config.environment === Environment.DEMO ? "B04IXE4" : "Q1KOKP2",
+});
 
-export const claimantSolicitorBulkScan: User = getUser(
-  {
-    email: "hmcts.civil+organisation.1.solicitor.2@gmail.com",
-    password: defaultPassword,
-    key: UserKey.CLAIMANT_SOLICITOR_BULK_SCAN,
-    role: UserRole.CASEWORKER,
-    orgId: config.environment === Environment.DEMO ? "B04IXE4" : "Q1KOKP2"
-  }
-);
+export const claimantSolicitorBulkScanUser: User = getUser({
+  email: "hmcts.civil+organisation.1.solicitor.2@gmail.com",
+  password: defaultPassword,
+  key: UserKey.CLAIMANT_SOLICITOR_BULK_SCAN,
+  role: UserRole.CASEWORKER,
+  orgId: config.environment === Environment.DEMO ? "B04IXE4" : "Q1KOKP2",
+});
 
-export const defendantSolicitor1: User = getUser(
-  {
-    email: "hmcts.civil+organisation.2.solicitor.1@gmail.com",
-    password: defaultPassword,
-    key: UserKey.DEFENDANT_SOLICITOR_1,
-    role: UserRole.CASEWORKER,
-    orgId: process.env.ENVIRONMENT === Environment.DEMO ? "DAWY9LJ" : "79ZRSOU"
-  }
-);
+export const defendantSolicitor1User: User = getUser({
+  email: "hmcts.civil+organisation.2.solicitor.1@gmail.com",
+  password: defaultPassword,
+  key: UserKey.DEFENDANT_SOLICITOR_1,
+  role: UserRole.CASEWORKER,
+  orgId: process.env.ENVIRONMENT === Environment.DEMO ? "DAWY9LJ" : "79ZRSOU",
+});
 
-export const defendantSolicitor2: User = getUser(
-  {
-    email: "hmcts.civil+organisation.3.solicitor.1@gmail.com",
-    password: defaultPassword,
-    key: UserKey.DEFENDANT_SOLICITOR_2,
-    role: UserRole.CASEWORKER,
-    orgId: process.env.ENVIRONMENT === Environment.DEMO ? "LCVTI1I" : "H2156A0"
-  }
-);
+export const defendantSolicitor2User: User = getUser({
+  email: "hmcts.civil+organisation.3.solicitor.1@gmail.com",
+  password: defaultPassword,
+  key: UserKey.DEFENDANT_SOLICITOR_2,
+  role: UserRole.CASEWORKER,
+  orgId: process.env.ENVIRONMENT === Environment.DEMO ? "LCVTI1I" : "H2156A0",
+});
 
-export const admin: User = getUser(
-  {
-    email: "civil-admin@mailnesia.com",
-    password: defaultPassword,
-    key: UserKey.CIVIL_ADMIN,
-    role: UserRole.CASEWORKER,
-  }
-);
+export const adminUser: User = getUser({
+  email: "civil-admin@mailnesia.com",
+  password: defaultPassword,
+  key: UserKey.CIVIL_ADMIN,
+  role: UserRole.CASEWORKER,
+});
+
+export const nbcRegion1User: User = getUser({
+  email: "nbc_admin_region1@justice.gov.uk",
+  password: defaultPassword,
+  key: UserKey.NBC_REGION_1,
+  role: UserRole.CASEWORKER,
+});
+
+export const nbcRegion2User: User = getUser({
+  email: "nbc_admin_region2@justice.gov.uk",
+  password: defaultPassword,
+  key: UserKey.NBC_REGION_2,
+  role: UserRole.CASEWORKER,
+});
+
+export const nbcRegion4User: User = getUser({
+  email: "nbc_admin_region4@justice.gov.uk",
+  password: defaultPassword,
+  key: UserKey.NBC_REGION_4,
+  role: UserRole.CASEWORKER,
+});
+
+export const nbcLocalUser: User = getUser({
+  email: "nbc-team-leader@mailnesia.com",
+  password: defaultPassword,
+  key: UserKey.NBC_LOCAL,
+  role: UserRole.CASEWORKER,
+});
+
+export const judgeRegion1User: User = getUser({
+  email: "4917924EMP-@ejudiciary.net",
+  password: judgeDefaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.JUDGE_REGION_1,
+});
+
+export const judgeRegion2User: User = getUser({
+  email: "EMP42506@ejudiciary.net",
+  password: judgeDefaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.JUDGE_REGION_2,
+});
+
+export const judgeRegion4User: User = getUser({
+  email: "4924246EMP-@ejudiciary.net",
+  password: judgeDefaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.JUDGE_REGION_4,
+});
+
+export const hearingCenterAdminLocalUser: User = getUser({
+  email: "hearing-centre-admin-01@example.com",
+  password: defaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.HEARING_CENTER_ADMIN_LOCAL,
+});
+
+export const hearingCenterAdminRegion1User: User = getUser({
+  email: "hearing_center_admin_reg1@justice.gov.uk",
+  password: defaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.HEARING_CENTER_ADMIN_REGION_1,
+});
+
+export const hearingCenterAdminRegion2User: User = getUser({
+  email: "hearing_center_admin_reg2@justice.gov.uk",
+  password: defaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.HEARING_CENTER_ADMIN_REGION_2,
+});
+
+export const hearingCenterAdminRegion4User: User = getUser({
+  email: "hearing_center_admin_reg4@justice.gov.uk",
+  password: defaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.HEARING_CENTER_ADMIN_REGION_4,
+});
+
+export const tribunalCaseworkerWithRegionId4User: User = getUser({
+  email: "tribunal_legal_caseworker_reg4@justice.gov.uk",
+  password: defaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.TRIBUNAL_CASEWORKER_REGION_4,
+});
+
+export const ctscAdminUser: User = getUser({
+  email: "ctsc_admin@justice.gov.uk",
+  password: defaultPassword,
+  role: UserRole.CASEWORKER,
+  key: UserKey.CTSC_ADMIN,
+});
--- playwright-e2e/enums/user-key.ts
+++ playwright-e2e/enums/user-key.ts
@@ -4,6 +4,19 @@ enum UserKey {
   DEFENDANT_SOLICITOR_1 = "defendant-solicitor-1",
   DEFENDANT_SOLICITOR_2 = "defendant-solicitor-2",
   CIVIL_ADMIN = "civil-admin",
+  NBC_REGION_1 = "nbc-region-1",
+  NBC_REGION_2 = "nbc-region-2",
+  NBC_REGION_4 = "nbc-region-4",
+  NBC_LOCAL = "nbc-local",
+  JUDGE_REGION_1 = "judge-region-1",
+  JUDGE_REGION_2 = "judge-region-2",
+  JUDGE_REGION_4 = "judge-region-4",
+  HEARING_CENTER_ADMIN_LOCAL = "hearing-admin-local",
+  HEARING_CENTER_ADMIN_REGION_1 = "hearing-center-admin-region-1",
+  HEARING_CENTER_ADMIN_REGION_2 = "hearing-center-admin-region-2",
+  HEARING_CENTER_ADMIN_REGION_4 = "hearing-center-admin-region-4",
+  TRIBUNAL_CASEWORKER_REGION_4 = "tribunal-caseworker-region-4",
+  CTSC_ADMIN = "ctsc-admin"
 }
 
 export default UserKey;
