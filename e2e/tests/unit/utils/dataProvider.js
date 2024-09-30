27b7bc2febef5afc73be9ced634b9fce2be20c89
meulendijksm <max.meulendijks@hmcts.net>
2024-09-30 13:05:01 +0100

    CIV-15204 - update dataprovider for unit tests

--- e2e/tests/unit/utils/dataProvider.js
+++ e2e/tests/unit/utils/dataProvider.js
@@ -9,9 +9,9 @@ const loadFile = file => {
 // Please update this map whenever exclusions are updated in build-release-definition.sh
 // without the *
 const exclusions = new Map([
-  ['preview', ['-prod.json']],
-  ['demo', ['UserProfile.json','-prod.json']],
-  ['aat', ['UserProfile.json','-prod.json']],
+  ['preview', ['-prod.json','-beforeAmendBundle-nonprod.json']],
+  ['demo', ['UserProfile.json','-prod.json','-beforeAmendBundle-nonprod.json']],
+  ['aat', ['UserProfile.json','-prod.json','-beforeAmendBundle-nonprod.json']],
   ['prod', ['UserProfile.json','-nonprod.json','GAspec.json']],
   ['staging', ['UserProfile.json','-nonprod.json','GAspec.json']]
 ]);
