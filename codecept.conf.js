5db7020878c2e9ecc1f14e2826e86fc1f6bd3216
Douglas Rice <douglas.rice@hmcts.net>
2024-10-01 17:00:36 +0100

    update codecept config file

--- codecept.conf.js
+++ codecept.conf.js
@@ -31,8 +31,7 @@ exports.config = {
   helpers: {
     Playwright: {
       url: process.env.URL || 'http://localhost:3333',
-      //show: process.env.SHOW_BROWSER_WINDOW === 'true' || false,
-      show: true,
+      show: process.env.SHOW_BROWSER_WINDOW === 'true' || false,
       waitForTimeout: parseInt(process.env.WAIT_FOR_TIMEOUT_MS || 90000),
       windowSize: '1280x960',
       browser: 'chromium',
