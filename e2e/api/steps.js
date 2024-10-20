ecb9ab5787c9585065db8149dc0902e02d9948c2
Azam <muhammad.ghulam@hmcts.net>
2024-10-20 13:56:05 +0100

    CIV-0000 updated for testing

--- e2e/api/steps.js
+++ e2e/api/steps.js
@@ -272,6 +272,7 @@ module.exports = {
     }
 
     console.log('Is PBAv3 toggle on?: ' + pbaV3);
+    console.log('Is PBAv3 toggle on?: ' + pbaV3);
 
     let bodyText = 'Your claim will not be issued until payment is confirmed.';
     let headerText = '# Please now pay your claim fee\n# using the link below';
@@ -1478,7 +1479,6 @@ const assertValidData = async (data, pageId, solicitor) => {
 
   assert.equal(response.status, 200);
 
-   
   let claimValue;
   if (data.valid && data.valid.ClaimValue && data.valid.ClaimValue.claimValue
     && data.valid.ClaimValue.claimValue.statementOfValueInPennies) {
@@ -1778,7 +1778,7 @@ function checkCalculated(calculated, responseBodyData) {
 
 function removeUuidsFromDynamicList(data, dynamicListField) {
   const dynamicElements = data[dynamicListField].list_items;
-   
+
   return dynamicElements.map(({code, ...item}) => item);
 }
 
