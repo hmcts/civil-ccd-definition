690b900e106ee30efa3acc623c10308631a9046a
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2024-10-28 13:54:53 +0000

    add case filter page object

--- playwright-e2e/playwright-fixtures/ui/page-factory-fixtures.ts
+++ playwright-e2e/playwright-fixtures/ui/page-factory-fixtures.ts
@@ -2,11 +2,13 @@ import { test as base } from '../api/api-steps-fixtures';
 import PageUtilsFactory from '../../pages/utils/page-utils-factory';
 import IdamPageFactory from '../../pages/idam/idam-page-factory';
 import ExuiDashboardPageFactory from '../../pages/exui/exui-dashboard/exui-dashboard-page-factory';
+import CreateClaimPageFactory from '../../pages/exui/solicitor-events/create-claim/create-claim-page-factory';
 
 type PageFactoryFixtures = {
   _pageUtilsFactory: PageUtilsFactory;
   _idamPageFactory: IdamPageFactory;
   _exuiDashboardPageFactory: ExuiDashboardPageFactory;
+  _createClaimPageFactory: CreateClaimPageFactory;
 };
 
 export const test = base.extend<PageFactoryFixtures>({
@@ -18,5 +20,8 @@ export const test = base.extend<PageFactoryFixtures>({
   },
   _exuiDashboardPageFactory: async ({ page }, use) => {
     await use(new ExuiDashboardPageFactory(page));
+  },
+  _createClaimPageFactory: async ({ page }, use) => {
+    await use(new CreateClaimPageFactory(page));
   }
 });
