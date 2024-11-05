1318d412a982db7ebbb002d680c597605ef88a27
sankaviv1 <sankavi.mohanraj@version1.com>
2024-11-05 13:54:28 +0000

    fix payload for non carm response

--- e2e/helpers/carmHelper.js
+++ e2e/helpers/carmHelper.js
@@ -4,13 +4,7 @@ const testingSupport = require('./../api/testingSupport');
 
 module.exports = {
   adjustCaseSubmittedDateForCarm: async (caseId, carmEnabled = false, isMintiEnabled = false) => {
-    if (carmEnabled) {
-      console.log('carm enabled, updating submitted date');
-      await apiRequest.setupTokens(config.systemupdate);
-      const submittedDate = {'submittedDate':'2024-11-10T15:59:50'};
-      await testingSupport.updateCaseData(caseId, submittedDate);
-      console.log('submitted date update to after carm date');
-    } else if (!carmEnabled && !isMintiEnabled) {
+    if (!carmEnabled && !isMintiEnabled) {
       console.log('carm not enabled, minti not enabled updating submitted date');
       await apiRequest.setupTokens(config.systemupdate);
       const submittedDate = {'submittedDate':'2024-10-10T15:59:50'};
