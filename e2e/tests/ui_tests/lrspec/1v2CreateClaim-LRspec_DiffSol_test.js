2928fb34d09714c7e06653d0a63da6f19c1242f4
sankaviv1 <sankavi.mohanraj@version1.com>
2024-11-05 09:43:25 +0000

    add wait

--- e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_DiffSol_test.js
+++ e2e/tests/ui_tests/lrspec/1v2CreateClaim-LRspec_DiffSol_test.js
@@ -4,6 +4,7 @@ const {checkCaseFlagsEnabled} = require('../../../api/testingSupport');
 const {PARTY_FLAGS} = require('../../../fixtures/caseFlags');
 const {adjustCaseSubmittedDateForCarm} = require('../../../helpers/carmHelper');
 const apiRequest = require('../../../api/apiRequest');
+const {waitForFinishedBusinessProcess} = require('../../../api/testingSupport');
 
 let caseNumber;
 
@@ -73,6 +74,7 @@ Scenario('Judge triggers SDO', async ({LRspec}) => {
 }).retry(3);
 
 Scenario('Update submitted date after assignment to small claims track in SDO', async ({LRspec}) => {
+  await waitForFinishedBusinessProcess(caseNumber);
   await apiRequest.setupTokens(config.applicantSolicitorUser);
   await adjustCaseSubmittedDateForCarm(caseNumber, false);
 }).retry(3);
