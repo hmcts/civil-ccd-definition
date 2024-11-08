354850ff3d0ae420e1b6500a6f483f5d00bbb2a2
gmmagruder <gabrielle.magruder@gmail.com>
2024-11-08 16:26:59 +0000

    Add notify claim page for defendant 1

---
+++ playwright-e2e/pages/exui/solicitor-events/notify-claim/notify-claim/notify-claim-page.ts
@@ -0,0 +1,30 @@
+import { Page } from 'playwright-core';
+import BasePage from '../../../../../base/base-page';
+import { AllMethodsStep } from '../../../../../decorators/test-steps';
+import NotifyClaimFragment from '../../../fragments/notify-claim/notify-claim-fragments';
+
+@AllMethodsStep()
+export default class NotifyClaimPage extends BasePage {
+  private notifyClaimFragment: NotifyClaimFragment;
+
+  constructor(notifyClaimFragment: NotifyClaimFragment, page: Page) {
+    super(page);
+    this.notifyClaimFragment = notifyClaimFragment;
+  }
+
+  async verifyContent(...args: any[]): Promise<void> {
+    await super.runVerifications([
+      this.notifyClaimFragment.verifyContent(),
+      this.notifyClaimFragment.verifyDefendant1Content(),
+    ]);
+  }
+
+  async fillNotifyClaimDetails() {
+    await this.notifyClaimFragment.fillDefendant1NotifyClaim();
+    await this.notifyClaimFragment.fillDefendant1StatementOfTruth();
+  }
+
+  async continue() {
+    await this.notifyClaimFragment.continue();
+  }
+}
