9209ab5ad2eafd048b3b16e4ae6ba0fb846fd7b2
sherlynkhaw <sherlynkhaw@gmail.com>
2024-11-19 16:15:43 +0000

    CIV-15693 Uncomment api tests for discontinue claim (#5079)
    CIV-15693: Uncomment api tests for disconitnue claim

--- e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_discontinue_claim_test.js
+++ e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_discontinue_claim_test.js
@@ -6,7 +6,7 @@ const {createAccount, deleteAccount} = require('../../../api/idamHelper');
 const claimType = 'SmallClaims';
 let caseId;
 
-Feature.skip('CCD 1v1 API test @api-spec-cui @api-nonprod @api-settle-discont');
+Feature('CCD 1v1 API test @api-spec-cui @api-nonprod @api-settle-discont');
 
 Before(async () => {
   await createAccount(config.defendantCitizenUser2.email, config.defendantCitizenUser2.password);
