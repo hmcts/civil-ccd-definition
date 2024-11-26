43df2745a3b70239645f28367ede45fc45301438
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2024-11-26 15:16:46 +0000

    Update 1v2CreateClaim_DiffSol_GA_test.js

--- e2e/tests/api_tests/generalapplication/1v2CreateClaim_DiffSol_GA_test.js
+++ e2e/tests/api_tests/generalapplication/1v2CreateClaim_DiffSol_GA_test.js
@@ -1,5 +1,5 @@
 const config = require('../../../config.js');
-const {assignCaseRoleToUser, addUserCaseMapping, unAssignAllUsers} = require('../../../api/caseRoleAssignmentHelper');
+const {addUserCaseMapping, unAssignAllUsers} = require('../../../api/caseRoleAssignmentHelper');
 const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
 let caseNumber;
 
@@ -8,7 +8,6 @@ Feature('1v2 Different Solicitors General application creation @e2e-unspec @e2e-
 Scenario.only('Make a general application', async ({api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
   caseNumber = await api.getCaseId();
-  await I.setCaseId(caseNumber);
   addUserCaseMapping(caseNumber, config.applicantSolicitorUser);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
