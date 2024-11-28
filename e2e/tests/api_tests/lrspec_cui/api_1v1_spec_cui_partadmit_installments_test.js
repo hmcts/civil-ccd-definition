3a7c16d0c2f06336b3d0c5438321a40618e118a5
LiaoP <pijian.liao@hmcts.net>
2024-11-28 19:32:42 +0000

    CIV-15958 api test DONT MERGE

--- e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_partadmit_installments_test.js
+++ e2e/tests/api_tests/lrspec_cui/api_1v1_spec_cui_partadmit_installments_test.js
@@ -1,5 +1,6 @@
 const config = require('../../../config.js');
 const {createAccount, deleteAccount} = require('../../../api/idamHelper');
+const { otherSolicitorUser1 } = require('../../../config');
 
 const claimType = 'SmallClaims';
 let caseId;
@@ -10,18 +11,19 @@ Before(async () => {
     await createAccount(config.defendantCitizenUser2.email, config.defendantCitizenUser2.password);
   });
 
-  Scenario('1v1 LiP v LiP defendant response with part admit pay by installments', async ({api_spec_cui}) => {
-    await respondWithPAPayByInstallments(api_spec_cui);
+  Scenario.only('1v1 LiP v LiP defendant response with part admit pay by installments', async ({api_spec_cui, noc}) => {
+    await respondWithPAPayByInstallments(api_spec_cui, noc);
   });
 
-  async function respondWithPAPayByInstallments(api_spec_cui) {
+  async function respondWithPAPayByInstallments(api_spec_cui, noc) {
     caseId = await api_spec_cui.createClaimWithUnrepresentedClaimant(config.applicantCitizenUser,'SmallClaims',false,'INDIVIDUAL');
-    await api_spec_cui.performCitizenDefendantResponse(config.defendantCitizenUser2, caseId,claimType,carmEnabled,'PA_INSTALLMENTS_INDIVIDUAL');
-    await api_spec_cui.performCitizenClaimantResponse(config.applicantCitizenUser, caseId, 'JUDICIAL_REFERRAL', carmEnabled,'PA_REJECT_NO_MEDIATION');
+    await noc.requestNoticeOfChangeForRespondent1Solicitor(caseId, otherSolicitorUser1);
+    // await api_spec_cui.performCitizenDefendantResponse(config.defendantCitizenUser2, caseId,claimType,carmEnabled,'PA_INSTALLMENTS_INDIVIDUAL');
+    // await api_spec_cui.performCitizenClaimantResponse(config.applicantCitizenUser, caseId, 'JUDICIAL_REFERRAL', carmEnabled,'PA_REJECT_NO_MEDIATION');
   }
 
   AfterSuite(async  ({api_spec_cui}) => {
   await api_spec_cui.cleanUp();
     await deleteAccount(config.defendantCitizenUser2.email);
   });
-  
+
