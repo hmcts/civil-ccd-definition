41f828c8af2ed3d108ea8c6509dd5af0b6a6b7ef
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2024-11-28 14:58:01 +0000

    removing only tag and changing to api tags

--- e2e/tests/api_tests/generalapplication/1v2CreateClaim_DiffSol_GA_test.js
+++ e2e/tests/api_tests/generalapplication/1v2CreateClaim_DiffSol_GA_test.js
@@ -3,9 +3,9 @@ const {addUserCaseMapping, unAssignAllUsers} = require('../../../api/caseRoleAss
 const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
 let caseNumber;
 
-Feature('1v2 Different Solicitors General application creation @e2e-unspec @e2e-nightly @e2e-unspec-1v2DS @master-e2e-ft');
+Feature('1v2 Different Solicitors General application creation @api-unspec @api-multiparty @api-nightly-prod');
 
-Scenario.only('Make a general application', async ({api}) => {
+Scenario('Make a general application', async ({api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
   caseNumber = await api.getCaseId();
   addUserCaseMapping(caseNumber, config.applicantSolicitorUser);
