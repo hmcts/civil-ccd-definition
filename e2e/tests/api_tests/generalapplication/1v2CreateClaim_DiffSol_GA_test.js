d68979a17d4a645bb12b7690d506db1cdec3eb61
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2024-11-29 10:59:11 +0000

    adding business process waits

--- e2e/tests/api_tests/generalapplication/1v2CreateClaim_DiffSol_GA_test.js
+++ e2e/tests/api_tests/generalapplication/1v2CreateClaim_DiffSol_GA_test.js
@@ -1,16 +1,20 @@
 const config = require('../../../config.js');
 const {addUserCaseMapping, unAssignAllUsers} = require('../../../api/caseRoleAssignmentHelper');
+const {waitForFinishedBusinessProcess} = require("../../../api/testingSupport");
 const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
 let caseNumber;
 
 Feature('1v2 Different Solicitors General application creation @api-unspec @api-nonprod');
 
-Scenario('Make a general application', async ({api}) => {
+Scenario.only('Make a general application', async ({api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
   caseNumber = await api.getCaseId();
   addUserCaseMapping(caseNumber, config.applicantSolicitorUser);
+  await waitForFinishedBusinessProcess(caseNumber);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
+  await waitForFinishedBusinessProcess(caseNumber);
+
   await api.initiateGeneralApplication(caseNumber, config.applicantSolicitorUser, 'AWAITING_RESPONDENT_ACKNOWLEDGEMENT');
 });
 
