e7dd79084cdc1b9cb8936235501f212d4d8e8254
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-11-29 14:38:25 +0000

    CIV-15826 changes

--- e2e/fixtures/events/judgmentOnline1v1Spec.js
+++ e2e/fixtures/events/judgmentOnline1v1Spec.js
@@ -211,6 +211,7 @@ module.exports = {
           setAsideJudgment.userInput = {
             ...setAsideJudgment.userInput,
             SetAsideJudgment: {
+              joSetAsideApplicationDate: '2008-06-05',
               joSetAsideOrderDate: '2008-06-06',
               joSetAsideOrderType: setAsideOrderType,
               joSetAsideReason: setAsideReason
