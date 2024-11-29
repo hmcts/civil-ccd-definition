5fe7001f1e2b9d6fdc480f26ac89b703f08ef872
Mike Lemos <gobii@me.com>
2024-11-29 11:17:56 +0000

    DTSCCI-1105 fix test

--- e2e/api/steps.js
+++ e2e/api/steps.js
@@ -1861,7 +1861,7 @@ const clearHearingLocationData = (responseBody) => {
 };
 
 const clearDataForDefendantResponse = (responseBody, solicitor) => {
-  delete responseBody.data['businessProcess'];
+
   delete responseBody.data['caseNotes'];
   delete responseBody.data['systemGeneratedCaseDocuments'];
   delete responseBody.data['respondentSolicitor2Reference'];
