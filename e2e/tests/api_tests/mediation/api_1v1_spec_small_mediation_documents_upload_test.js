74179c8d42dff51b8006baed91fa3d86f0bc93b9
sankaviv1 <sankavi.mohanraj@version1.com>
2024-12-03 10:08:06 +0000

    CIV-15903 Fix mediation test

--- e2e/tests/api_tests/mediation/api_1v1_spec_small_mediation_documents_upload_test.js
+++ e2e/tests/api_tests/mediation/api_1v1_spec_small_mediation_documents_upload_test.js
@@ -12,7 +12,7 @@ Feature('Spec small claims mediation API test');
 
 async function prepareClaim1v1(api_spec_small, carmEnabled) {
   await api_spec_small.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_ONE', false, carmEnabled);
-  await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE');
+  await api_spec_small.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_ONE', false, carmEnabled);
   await api_spec_small.claimantResponse(config.applicantSolicitorUser, true, 'No', carmEnabled);
 }
 
