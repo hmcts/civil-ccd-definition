3e9d20d0b71cc5108d7d4567d45cd14fed094db0
Devin Rathod <devrathod4704@gmail.com>
2024-12-19 10:11:21 +0000

    adding 'document only' POM and content

---
+++ playwright-e2e/pages/exui/judge-events/add-case-notes-judge/add-case-notes-judge-selectionDocument/add-case-notes-judge-selectionDocument-content.ts
@@ -0,0 +1,37 @@
+export const heading = 'Add a document';
+
+export const subHeading = 'Upload a document';
+
+export const testText = {
+  documentName: 'Testing document',
+};
+
+export const textFields = {
+  documentName: {
+    label: 'Name of document',
+    selector: '#documentAndNameToAdd_0_documentName',
+  },
+  documentUpload: {
+    label: 'Document',
+    selector: '#documentAndNameToAdd_0_document',
+  },
+};
+
+export const buttons = {
+  addNew: {
+    label: 'Add new',
+    selector: '.button write-collection-add-item__top [type="button"]',
+  },
+  remove: {
+    label: 'Remove',
+    selector: 'button button-secondary [type="button"]',
+  },
+  previous: {
+    label: 'Previous',
+    selector: '.button-secondary [type="button"]',
+  },
+  submit: {
+    label: 'Continue',
+    selector: 'button[type="submit"]"',
+  },
+};
