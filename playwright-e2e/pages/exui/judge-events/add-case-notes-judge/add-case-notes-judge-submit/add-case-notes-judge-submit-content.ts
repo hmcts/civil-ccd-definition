fb542139833cdb36ba29643a4d219ca7201cfbef
Devin Rathod <devrathod4704@gmail.com>
2024-12-19 10:12:38 +0000

    adding 'submit' POM and content

---
+++ playwright-e2e/pages/exui/judge-events/add-case-notes-judge/add-case-notes-judge-submit/add-case-notes-judge-submit-content.ts
@@ -0,0 +1,89 @@
+export const heading = 'Add a case note';
+
+export const subheading = 'Check your answers';
+
+export const labels = {
+  noteOnly: {
+    checkInformationCarefully: {
+      label: 'Check the information below carefully',
+    },
+    thisCaseisOnlyVisibleTo: {
+      label: 'This case note is only visible to you and other judges',
+    },
+    writeACaseNote: {
+      label: 'Write a note about this case',
+    },
+    selection: {
+      label: 'Note Only',
+    },
+    selectionContent: {
+      label: 'Testing case note',
+    },
+  },
+  documentWithNote: {
+    checkInformationCarefully: {
+      label: 'Check the information below carefully',
+    },
+    thisCaseisOnlyVisibleTo: {
+      label: 'This case note is only visible to you and other judges',
+    },
+    uploadDocument: {
+      label: 'Upload a document',
+    },
+    selection: {
+      label: 'Document with a note',
+    },
+    selectionContentName: {
+      label: 'Testing document',
+    },
+    selectionContentDocument: {
+      label: 'fileupload.txt',
+    },
+    selectionContentNote: {
+      label: 'Testing document with note',
+    },
+  },
+  documentOnly: {
+    checkInformationCarefully: {
+      label: 'Check the information below carefully',
+    },
+    thisCaseisOnlyVisibleTo: {
+      label: 'This case note is only visible to you and other judges',
+    },
+    uploadDocument: {
+      label: 'Upload a document',
+    },
+    selection: {
+      label: 'Document only',
+    },
+    selectionContentName: {
+      label: 'Testing document',
+    },
+    selectionContentDocument: {
+      label: 'fileupload.txt',
+    },
+  },
+};
+
+export const changeLinks = {
+  changeToSelectionPage: {
+    label: 'Change',
+    selector:
+      'span.text-16[aria-label="Change This case note is only visible to you and other judges"]',
+  },
+  changeToSelectionNotePage: {
+    label: 'Change',
+    selector: 'span.text-16[aria-label="Change Write a note about this case"]',
+  },
+};
+
+export const buttons = {
+  previous: {
+    label: 'Previous',
+    selector: '.button-secondary [type="button"]',
+  },
+  submit: {
+    label: 'Continue',
+    selector: 'button[type="submit"]"',
+  },
+};
---
+++ playwright-e2e/pages/exui/judge-events/add-case-notes-judge/add-case-notes-judge-submit/add-case-notes-judge-submit-page.ts
@@ -0,0 +1,66 @@
+import BasePage from '../../../../../base/base-page';
+import {
+  heading,
+  subheading,
+  labels,
+  buttons,
+  changeLinks,
+} from './add-case-notes-judge-submit-content';
+import { AllMethodsStep } from '../../../../../decorators/test-steps';
+import CCDCaseData from '../../../../../models/ccd/ccd-case-data';
+import ExuiPage from '../../../exui-page/exui-page';
+
+@AllMethodsStep()
+export default class AddCaseNotesJudgeSubmitPage extends ExuiPage(BasePage) {
+  async verifyContent(ccdCaseData: CCDCaseData): Promise<void> {
+    await super.runVerifications([
+      super.expectHeading(heading),
+      super.expectSubheading(subheading),
+      super.expectText(buttons.previous.label),
+      super.expectText(buttons.submit.label),
+    ]);
+  }
+
+  async checkNoteOnlyContent(): Promise<void> {
+    super.expectText(labels.noteOnly.checkInformationCarefully.label);
+    super.expectText(labels.noteOnly.thisCaseisOnlyVisibleTo.label);
+    super.expectText(labels.noteOnly.writeACaseNote.label);
+    super.expectText(labels.noteOnly.selection.label);
+    super.expectText(labels.noteOnly.selectionContent.label);
+  }
+
+  async checkDocumentWithNoteContent(): Promise<void> {
+    super.expectText(labels.documentWithNote.checkInformationCarefully.label);
+    super.expectText(labels.documentWithNote.thisCaseisOnlyVisibleTo.label);
+    super.expectText(labels.documentWithNote.uploadDocument.label, { exact: true });
+    super.expectText(labels.documentWithNote.selection.label);
+    super.expectText(labels.documentWithNote.selectionContentName.label, { exact: true });
+    super.expectText(labels.documentWithNote.selectionContentDocument.label);
+    super.expectText(labels.documentWithNote.selectionContentNote.label, { exact: true });
+  }
+
+  async checkDocumentOnlyContent(): Promise<void> {
+    super.expectText(labels.documentOnly.checkInformationCarefully.label);
+    super.expectText(labels.documentOnly.thisCaseisOnlyVisibleTo.label);
+    super.expectText(labels.documentOnly.uploadDocument.label, { exact: true });
+    super.expectText(labels.documentOnly.selection.label);
+    super.expectText(labels.documentOnly.selectionContentName.label, { exact: true });
+    super.expectText(labels.documentOnly.selectionContentDocument.label);
+  }
+
+  async changeToSelectionPage(): Promise<void> {
+    await super.clickBySelector(changeLinks.changeToSelectionPage.selector);
+  }
+
+  async changeToSelectionNotePage(): Promise<void> {
+    await super.clickBySelector(changeLinks.changeToSelectionNotePage.selector);
+  }
+
+  async previous(): Promise<void> {
+    await super.clickButtonByName(buttons.previous.label);
+  }
+
+  async submit(...args: any[]): Promise<void> {
+    await super.retryClickSubmit();
+  }
+}
