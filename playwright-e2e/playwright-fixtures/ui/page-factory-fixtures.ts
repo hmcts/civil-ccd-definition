c0fd3cbf89218fb4a081301cb2b1050bd97f371a
Devin Rathod <devrathod4704@gmail.com>
2024-12-19 17:09:15 +0000

    adding manage document factory

--- playwright-e2e/playwright-fixtures/ui/page-factory-fixtures.ts
+++ playwright-e2e/playwright-fixtures/ui/page-factory-fixtures.ts
@@ -9,6 +9,7 @@ import DefendantResponsePageFactory from '../../pages/exui/solicitor-events/defe
 import ClaimantResponsePageFactory from '../../pages/exui/solicitor-events/claimant-response/claimant-response-page-factory';
 import InformAgreedExtensionDatePageFactory from '../../pages/exui/solicitor-events/inform-agreed-extension-date/inform-agreed-extension-date-page-factory';
 import CreateCaseFlagsPageFactory from '../../pages/exui/caseworker-events/create-case-flags/create-case-flags-page-factory';
+import JudgeEventsPageFactory from '../../pages/exui/judge-events/judge-events-page-factory';
 
 type PageFactoryFixtures = {
   _pageUtilsFactory: PageUtilsFactory;
@@ -21,6 +22,7 @@ type PageFactoryFixtures = {
   _defendantResponsePageFactory: DefendantResponsePageFactory;
   _claimantResponsePageFactory: ClaimantResponsePageFactory;
   _createCaseFlagsPageFactory: CreateCaseFlagsPageFactory;
+  _judgeEventsPageFactory: JudgeEventsPageFactory;
 };
 
 export const test = base.extend<PageFactoryFixtures>({
@@ -53,5 +55,8 @@ export const test = base.extend<PageFactoryFixtures>({
   },
   _createCaseFlagsPageFactory: async ({ page }, use) => {
     await use(new CreateCaseFlagsPageFactory(page));
+  },
+  _judgeEventsPageFactory: async ({ page }, use) => {
+    await use(new JudgeEventsPageFactory(page));
   }
 });
