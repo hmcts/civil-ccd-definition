b12ab487ad1dd0835374e31086d07261f4290e8d
Devin Rathod <devrathod4704@gmail.com>
2024-12-19 10:12:58 +0000

    adding 'confirm' POM and content

---
+++ playwright-e2e/pages/exui/judge-events/add-case-notes-judge/add-case-notes-judge-confirm/add-case-notes-judge-confirm-page.ts
@@ -0,0 +1,38 @@
+import BasePage from '../../../../../base/base-page';
+import { heading, labels, buttons } from './add-case-notes-judge-confirm-content';
+import { AllMethodsStep } from '../../../../../decorators/test-steps';
+import CCDCaseData from '../../../../../models/ccd/ccd-case-data';
+import ExuiPage from '../../../exui-page/exui-page';
+
+@AllMethodsStep()
+export default class AddCaseNotesJudgeConfirmPage extends ExuiPage(BasePage) {
+  async verifyContent(ccdCaseData: CCDCaseData): Promise<void> {
+    await super.runVerifications([
+      // super.verifyHeadings(ccdCaseData),
+      super.expectText(buttons.submit.label),
+    ]);
+  }
+
+  async checkConfirmPageNoteOnlyContent(): Promise<void> {
+    await super.expectHeading(heading);
+    await super.expectText(labels.noteOnly.label);
+  }
+
+  async checkConfirmPageDocumentAndNoteContent(): Promise<void> {
+    await super.expectHeading(heading);
+    await super.expectText(labels.documentWithNote.confirmationBanner.label);
+    await super.expectText(labels.documentWithNote.uploadedSubheading.label);
+    await super.expectText(labels.documentWithNote.uploadedFileName.label);
+  }
+
+  async checkConfirmPageDocumentOnlyContent(): Promise<void> {
+    await super.expectHeading(heading);
+    await super.expectText(labels.documentOnly.confirmationBanner.label);
+    await super.expectText(labels.documentOnly.uploadedSubheading.label);
+    await super.expectText(labels.documentOnly.uploadedFileName.label);
+  }
+
+  async submit(...args: any[]): Promise<void> {
+    await super.retryClickSubmit();
+  }
+}
