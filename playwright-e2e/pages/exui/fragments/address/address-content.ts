735d72ed071dd2f313b19fdc995846a76c761597
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-01-10 14:38:09 +0000

    update address fragment

--- playwright-e2e/pages/exui/fragments/address/address-content.ts
+++ playwright-e2e/pages/exui/fragments/address/address-content.ts
@@ -1,52 +1,56 @@
 import Party from '../../../../enums/party';
 
-export const heading = 'Address';
-
 export const getInputs = (party: Party) => ({
   postCodeInput: {
     label: 'Enter a UK postcode',
-    selector: `#${party}_primaryAddress_primaryAddress_postcodeInput`,
+    selector: `#${party}_primaryaddress_primaryaddress_postcodeInput`,
   },
-  detailAddressLine1: {
+  addressLine1: {
     label: 'Building and Street',
-    selector: `#${party}_primaryAddress__detailAddressLine1`,
+    selector: `#${party}_primaryaddress__addressLine1`,
   },
-  detailAddressLine2: {
-    label: 'Address Line 2 (Optional)',
-    selector: `#${party}_primaryAddress__detailAddressLine2`,
+  addressLine2: {
+    label: 'address Line 2 (Optional)',
+    selector: `#${party}_primaryaddress__addressLine2`,
   },
-  detailAddressLine3: {
-    label: 'Address Line 3 (Optional)',
-    selector: `#${party}_primaryAddress__detailAddressLine3`,
+  addressLine3: {
+    label: 'address Line 3 (Optional)',
+    selector: `#${party}_primaryaddress__addressLine3`,
   },
-  detailPostTown: {
+  postTown: {
     label: 'Town or City (Optional)',
-    selector: `#${party}_primaryAddress__detailPostTown`,
+    selector: `#${party}_primaryaddress__PostTown`,
   },
-  detailCounty: {
+  county: {
     label: 'County (Optional)',
-    selector: `#${party}_primaryAddress__detailCounty`,
+    selector: `#${party}_primaryaddress__County`,
   },
-  detailCountry: {
+  country: {
     label: 'Country (Optional)',
-    selector: `#${party}_primaryAddress__detailCountry`,
+    selector: `#${party}_primaryaddress__Country`,
   },
-  detailPostCode: {
+  postCode: {
     label: 'Postcode/Zipcode',
-    selector: `#${party}_primaryAddress__detailPostCode`,
+    selector: `#${party}_primaryaddress__PostCode`,
   },
 });
 
 export const getDropdowns = (party: Party) => ({
   addressList: {
     label: 'Select an address',
-    selector: `#${party}_primaryAddress_primaryAddress_addressList`,
-    options: ['George Mitchell Comprehensive School, Farmer Road, London'],
+    selector: `#${party}_primaryaddress_primaryaddress_addressList`,
+    options: [],
   },
 });
 
 export const buttons = {
-  findAddress: {
+  findaddress: {
     title: 'Find address',
+  },
+};
+
+export const links = {
+  cannotFindAddress: {
+    title: " I can't enter a UK postcode ",
   },
 };
--- playwright-e2e/pages/exui/fragments/address/address-fragment.ts
+++ playwright-e2e/pages/exui/fragments/address/address-fragment.ts
@@ -2,7 +2,7 @@ import { Page } from 'playwright-core';
 import BasePage from '../../../../base/base-page';
 import { AllMethodsStep } from '../../../../decorators/test-steps';
 import ExuiPage from '../../exui-page/exui-page';
-import { buttons, getDropdowns, getInputs, heading } from './address-content';
+import { buttons, getInputs, links } from './address-content';
 import Party from '../../../../enums/party';
 
 @AllMethodsStep()
@@ -14,39 +14,28 @@ export default class AddressFragment extends ExuiPage(BasePage) {
     this.party = party;
   }
 
-  async verifyContent(...args: any[]): Promise<void> {
-    await super.runVerifications([
-      super.expectSubheading(heading),
-      super.expectLabel(getInputs(this.party).postCodeInput.label),
-      super.expectLabel(getInputs(this.party).detailAddressLine1.label),
-      super.expectLabel(getInputs(this.party).detailAddressLine2.label),
-      super.expectLabel(getInputs(this.party).detailAddressLine3.label),
-      super.expectLabel(getInputs(this.party).detailPostTown.label),
-      super.expectLabel(getInputs(this.party).detailCounty.label),
-      super.expectLabel(getInputs(this.party).detailCountry.label),
-      super.expectLabel(getInputs(this.party).detailPostCode.label),
-    ]);
+  async verifyContent() {
+    await super.runVerifications([super.expectLabel(getInputs(this.party).postCodeInput.label)]);
   }
 
-  async fillAddress() {
-    await super.inputText('E10 5DN', getInputs(this.party).postCodeInput.selector);
-    await super.clickByText(buttons.findAddress.title);
-    await super.expectLabel(getDropdowns(this.party).addressList.label);
-    await super.selectFromDropdown(
-      getDropdowns(this.party).addressList.options[0],
-      getDropdowns(this.party).addressList.selector,
-    );
-    await super.expectInputValue(
-      'George Mitchell Comprehensive School',
-      getInputs(this.party).detailAddressLine1.selector,
-    );
-    await super.expectInputValue('Farmer Road', getInputs(this.party).detailAddressLine2.selector);
-    await super.expectInputValue('London', getInputs(this.party).detailPostTown.selector);
-    await super.expectInputValue('United Kingdom', getInputs(this.party).detailCountry.selector);
-    await super.expectInputValue('E10 5DN', getInputs(this.party).detailPostCode.selector);
+  async enterAddressManual(postCode?: string) {
+    await super.clickLink(links.cannotFindAddress.title);
+    await super.inputText(`${this.party} Building`, getInputs(this.party).addressLine1.selector);
+    await super.inputText(`${this.party} Flat`, getInputs(this.party).addressLine2.selector);
+    await super.inputText(`${this.party} Street`, getInputs(this.party).addressLine3.selector);
+    await super.inputText(`${this.party} City`, getInputs(this.party).postTown.selector);
+    await super.inputText(`${this.party} County`, getInputs(this.party).county.selector);
+    await super.inputText(`${this.party} Country`, getInputs(this.party).country.selector);
+    await super.inputText(postCode ?? `E10 5DN`, getInputs(this.party).postCode.selector);
   }
 
-  async submit(...args: any[]): Promise<void> {
+  async findAddress(postcode: string, index: number) {
+    await super.inputText(postcode, getInputs(this.party).postCodeInput.selector);
+    await super.clickButtonByName(buttons.findaddress.title);
+    //method incomplete, need to add method in basePage for waiting for dropdown to appear.
+  }
+
+  async submit() {
     throw new Error('Method not implemented.');
   }
 }
