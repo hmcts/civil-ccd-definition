2a38680db74216d850d83af2652d2a0fa4de8709
Devin Rathod <devrathod4704@gmail.com>
2025-01-20 17:01:07 +0000

    adding 'Mediation contact information' POM and content for claimant and defendant

---
+++ playwright-e2e/pages/exui/solicitor-events/response/mediation/mediation-contact-information/mediation-contact-information-content.ts
@@ -0,0 +1,49 @@
+import { Party } from '../../../../../../models/partys';
+
+export const heading = {
+  claimant: { label: 'View and respond to defence' },
+  defendant: { label: 'Respond to claim' },
+};
+
+export const subHeading = 'Mediation contact information';
+
+export const content = {
+  paragraph1: {
+    label:
+      'Please provide the contact details of the individual who will conduct the mediation appointment.',
+  },
+  paragraph2: {
+    label: 'This should be a party to the claim or their legal representative.',
+  },
+};
+
+export const inputs = {
+  firstName: {
+    label: 'First name',
+    selector: (claimantParty: Party) => `#${claimantParty.oldKey}MediationContactInfo_firstName`,
+  },
+  lastName: {
+    label: 'Last name',
+    selector: (claimantParty: Party) => `#${claimantParty.oldKey}MediationContactInfo_lastName`,
+  },
+  emailAddress: {
+    label: 'Email address',
+    selector: (claimantParty: Party) => `#${claimantParty.oldKey}MediationContactInfo_emailAddress`,
+  },
+  telephoneNumber: {
+    label: 'Telephone number',
+    selector: (claimantParty: Party) =>
+      `#${claimantParty.oldKey}MediationContactInfo_telephoneNumber`,
+  },
+};
+
+export const buttons = {
+  previous: {
+    label: 'Previous',
+    selector: '.button.button-secondary',
+  },
+  submit: {
+    label: 'Continue',
+    selector: '.button.button[type="submit"]',
+  },
+};
