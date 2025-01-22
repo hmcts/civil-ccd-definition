f70b6a11f5ab781aa9d62e1745dd43cc9f1527e1
Devin Rathod <devrathod4704@gmail.com>
2025-01-22 14:43:07 +0000

    refactoring notify claim COS fragment

--- playwright-e2e/pages/exui/solicitor-events/notify-claim/notify-claim-page-factory.ts
+++ playwright-e2e/pages/exui/solicitor-events/notify-claim/notify-claim-page-factory.ts
@@ -1,7 +1,7 @@
 import BasePageFactory from '../../../../base/base-page-factory';
 import partys from '../../../../constants/partys';
 import CertificateOfServiceSubmitFragment from '../../fragments/certificate-of-service-submit/certificate-of-service-submit-fragment';
-import CertificateOfServiceFragment from '../../fragments/certificate-of-service/certificate-of-service-fragment';
+import CertificateOfServiceNotifyClaimFragment from '../../fragments/certificate-of-service-notify-claim/certificate-of-service-notify-claim-fragment';
 import AccessGrantedWarningPage from './access-granted-warning/access-granted-warning-page';
 import CertificateOfService1NotifyClaimPage from './certificate-of-service-1-notify-claim/certificate-of-service-1-notify-claim-page';
 import CertificateOfService2NotifyClaimPage from './certificate-of-service-2-notify-claim/certificate-of-service-2-notify-claim-page';
@@ -29,19 +29,25 @@ export default class NotifyClaimPageFactory extends BasePageFactory {
   }
 
   get certificateOfService1NotifyClaimPage() {
-    const certificateOfServiceFragment = new CertificateOfServiceFragment(
+    const certificateOfServiceNotifyClaimFragment = new CertificateOfServiceNotifyClaimFragment(
       this.page,
       partys.DEFENDANT_1,
     );
-    return new CertificateOfService1NotifyClaimPage(certificateOfServiceFragment, this.page);
+    return new CertificateOfService1NotifyClaimPage(
+      certificateOfServiceNotifyClaimFragment,
+      this.page,
+    );
   }
 
   get certificateOfService2NotifyClaimPage() {
-    const certificateOfServiceFragment = new CertificateOfServiceFragment(
+    const certificateOfServiceNotifyClaimFragment = new CertificateOfServiceNotifyClaimFragment(
       this.page,
       partys.DEFENDANT_2,
     );
-    return new CertificateOfService2NotifyClaimPage(certificateOfServiceFragment, this.page);
+    return new CertificateOfService2NotifyClaimPage(
+      certificateOfServiceNotifyClaimFragment,
+      this.page,
+    );
   }
 
   get submitNotifyClaimCOSPage() {
