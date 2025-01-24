25403faf4b044ef182782ef97726bc92f1070f94
Ruban <ruban72@hotmail.com>
2025-01-24 13:14:33 +0000

    DTSCCI-1074: hearing team leader role for doc removal

--- src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
@@ -63,7 +63,8 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
         new CcdRoleConfig("court-officer-order", "PUBLIC"),
         new CcdRoleConfig("nbc-team-leader", "PUBLIC"),
         new CcdRoleConfig("ctsc", "PUBLIC"),
-        new CcdRoleConfig("ctsc-team-leader", "PUBLIC")
+        new CcdRoleConfig("ctsc-team-leader", "PUBLIC"),
+        new CcdRoleConfig("caseworker-civil-doc-removal", "PUBLIC")
     };
 
     private final CcdEnvironment environment;
