412c1f7a2144283debb64ba08274ee0bcc652727
Jefex7 <duartepireslopes@gmail.com>
2025-01-27 14:07:49 +0000

    remove statement of truth content and checks

--- playwright-e2e/pages/exui/solicitor-events/response/claimant-response/common/statement-of-truth-claimant-response/statement-of-truth-claimant-response-page.ts
+++ playwright-e2e/pages/exui/solicitor-events/response/claimant-response/common/statement-of-truth-claimant-response/statement-of-truth-claimant-response-page.ts
@@ -3,7 +3,6 @@ import BasePage from '../../../../../../../base/base-page.ts';
 import { AllMethodsStep } from '../../../../../../../decorators/test-steps.ts';
 import CCDCaseData from '../../../../../../../models/ccd/ccd-case-data.ts';
 import ExuiPage from '../../../../../exui-page/exui-page.ts';
-import { paragraphs } from './statement-of-truth-claimant-response-content.ts';
 import StatementOfTruthFragment from '../../../../../fragments/statement-of-truth/statement-of-truth-fragment.ts';
 import partys from '../../../../../../../constants/partys.ts';
 
@@ -19,10 +18,6 @@ export default class StatementOfTruthClaimantResponsePage extends ExuiPage(BaseP
     await super.runVerifications([
       super.verifyHeadings(ccdCaseData),
       this.statementOfTruthFragment.verifyContent(),
-      super.expectText(paragraphs.descriptiveText1Spec1v1SC),
-      super.expectText(paragraphs.descriptiveText2Spec1v1SC),
-      super.expectText(paragraphs.descriptiveText3Spec1v1SC),
-      super.expectText(paragraphs.descriptiveText4Spec1v1SC),
     ]);
   }
 
