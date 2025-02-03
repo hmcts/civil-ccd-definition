20132449d6b9f44f4df5e34fd06dd01b645fde29
Devin Rathod <devrathod4704@gmail.com>
2025-02-03 11:07:00 +0000

    implementing new base page helper methods in claim amount details page

--- playwright-e2e/pages/exui/solicitor-events/create-claim/lr-spec/claim-amount-details/claim-amount-details-page.ts
+++ playwright-e2e/pages/exui/solicitor-events/create-claim/lr-spec/claim-amount-details/claim-amount-details-page.ts
@@ -4,6 +4,7 @@ import ClaimTrack from '../../../../../../enums/claim-track';
 import CaseDataHelper from '../../../../../../helpers/case-data-helper';
 import ExuiPage from '../../../../exui-page/exui-page';
 import { tableHeaders } from './claim-amount-details-content';
+import partys from '../../../../../../constants/partys';
 
 @AllMethodsStep()
 export default class ClaimAmountDetailsPage extends ExuiPage(BasePage) {
@@ -15,13 +16,15 @@ export default class ClaimAmountDetailsPage extends ExuiPage(BasePage) {
   }
 
   async verifySmallTrack() {
-    const amount = CaseDataHelper.getClaimValue(ClaimTrack.SMALL_CLAIM);
-    await super.expectText(amount, { count: 2 });
+    const amount = `£ ${CaseDataHelper.getClaimValue(ClaimTrack.SMALL_CLAIM).toFixed(2)}`;
+    await super.expectTableRowValueByName(`Roof damage - ${partys.CLAIMANT_1.key}`, amount);
+    await super.expectDataCellValue(amount, { first: true });
   }
 
   async verifyFastTrack() {
-    const amount = CaseDataHelper.getClaimValue(ClaimTrack.FAST_CLAIM);
-    await super.expectText(amount, { count: 2 });
+    const amount = `£ ${CaseDataHelper.getClaimValue(ClaimTrack.FAST_CLAIM).toFixed(2)}`;
+    await super.expectTableRowValueByName(`Roof damage - ${partys.CLAIMANT_1.key}`, amount);
+    await super.expectDataCellValue(amount, { first: true });
   }
 
   async submit() {
