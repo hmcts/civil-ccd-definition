a38bba3632a32df8f3893d5c6b459a90eb726c20
jamessmith-hmcts <James.Smith2@HMCTS.NET>
2025-02-07 16:09:55 +0000

    revert api 1v1 change

--- e2e/tests/api_tests/damages/api_1v1_test.js
+++ e2e/tests/api_tests/damages/api_1v1_test.js
@@ -1,5 +1,6 @@
 
 
+
 const config = require('../../../config.js');
 const mpScenario = 'ONE_V_ONE';
 
@@ -39,47 +40,47 @@ Scenario('Inform agreed extension date', async ({api}) => {
 });
 
 Scenario('Add Litigation Friend', async ({api}) => {
- await api.addDefendantLitigationFriend(config.defendantSolicitorUser, mpScenario);
+  await api.addDefendantLitigationFriend(config.defendantSolicitorUser, mpScenario);
 });
 
 Scenario('Defendant response', async ({api}) => {
- await api.defendantResponse(config.defendantSolicitorUser, mpScenario);
+  await api.defendantResponse(config.defendantSolicitorUser, mpScenario);
 });
 
 Scenario('Claimant response', async ({api}) => {
- await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
+  await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', 'FAST_CLAIM');
 });
 
 Scenario('Add case flags', async ({api}) => {
- await api.createCaseFlags(config.hearingCenterAdminWithRegionId1);
+  await api.createCaseFlags(config.hearingCenterAdminWithRegionId1);
 });
 
 Scenario('Manage case flags', async ({api}) => {
- await api.manageCaseFlags(config.hearingCenterAdminWithRegionId1);
+  await api.manageCaseFlags(config.hearingCenterAdminWithRegionId1);
 });
 
 Scenario('Manage contact information', async ({api}) => {
- await api.manageDefendant1Details(config.adminUser);
- await api.manageDefendant1LROrgDetails(config.defendantSolicitorUser);
+  await api.manageDefendant1Details(config.adminUser);
+  await api.manageDefendant1LROrgDetails(config.defendantSolicitorUser);
 });
 
 Scenario('Create claim where respondent is litigant in person and notify/notify details @api-cos', async ({api}) => {
- await api.createClaimWithRespondentLitigantInPerson(config.applicantSolicitorUser, mpScenario);
- await api.notifyClaimLip(config.applicantSolicitorUser);
- await api.notifyClaimDetailsLip(config.applicantSolicitorUser, mpScenario);
+  await api.createClaimWithRespondentLitigantInPerson(config.applicantSolicitorUser, mpScenario);
+  await api.notifyClaimLip(config.applicantSolicitorUser);
+  await api.notifyClaimDetailsLip(config.applicantSolicitorUser, mpScenario);
 });
 
 Scenario('Create claim and move it to caseman', async ({api}) => {
- await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
- await api.moveCaseToCaseman(config.adminUser);
+  await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
+  await api.moveCaseToCaseman(config.adminUser);
 });
 
 // This will be enabled when PAY-3817 issue of two minutes is fixed
 Scenario.skip('Resubmit claim after payment failure on PBA account ', async ({api}) => {
- await api.createClaimWithFailingPBAAccount(config.applicantSolicitorUser);
- await api.resubmitClaim(config.applicantSolicitorUser);
+  await api.createClaimWithFailingPBAAccount(config.applicantSolicitorUser);
+  await api.resubmitClaim(config.applicantSolicitorUser);
 });
 
 AfterSuite(async  ({api}) => {
- await api.cleanUp();
+  await api.cleanUp();
 });
