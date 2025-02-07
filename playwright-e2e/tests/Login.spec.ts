e858a0ed36fd9a4b91c3f4d50880a9d3ae36a20c
jamessmith-hmcts <James.Smith2@HMCTS.NET>
2025-02-07 16:22:13 +0000

    revert changes login spec

--- playwright-e2e/tests/Login.spec.ts
+++ playwright-e2e/tests/Login.spec.ts
@@ -1,47 +1,10 @@
-import defendantResponseMultiClaimSpec from '../../e2e/fixtures/events/defendantResponseMultiClaimSpec';
-import { civilAdminUser, defendantSolicitor1User } from '../config/users/exui-users';
 import { test } from '../playwright-fixtures/index';
 
-test(
-  'Testing Login',
-  { tag: '@debug' },
-  async ({
-    IdamSteps,
-    ExuiDashboardSteps,
-    ApiUserSteps,
-    ApiDataSteps,
-    _requestsFactory,
-    _exuiDashboardPageFactory,
-    _notifyClaimPageFactory,
-    AddDefendantLitigationFriendSteps,
-  }) => {
-    await ApiUserSteps.SetupUserData(civilAdminUser);
-    // await ApiDataSteps.SetupBankHolidaysData();
-    await IdamSteps.DefendantSolicitor1Login();
-    // await ExuiDashboardSteps.GoToCaseDetails();
-    await AddDefendantLitigationFriendSteps.Defendant1Journey();
-
-    // const { ccdRequests } = _requestsFactory;
-    // const ccdCaseData = await ccdRequests.fetchCCDCaseData(1732120625619001, civilAdminUser);
-
-    // const { caseDetailsPage } = _exuiDashboardPageFactory;
-    // await caseDetailsPage.goToCaseDetails(1732120625619001);
-    // await caseDetailsPage.verifyContent(ccdCaseData);
-    // await caseDetailsPage.retryChooseNextStep(ccdEvents.NOTIFY_DEFENDANT_OF_CLAIM);
-
-    // const { certificateOfService1NotifyClaimPage } = _notifyClaimPageFactory;
-    // await certificateOfService1NotifyClaimPage.verifyContent(ccdCaseData);
-    // await certificateOfService1NotifyClaimPage.fillDetails();
-    // await certificateOfService1NotifyClaimPage.submit();
-
-    // const { certificateOfService2NotifyClaimPage } = _notifyClaimPageFactory;
-    // await certificateOfService2NotifyClaimPage.verifyContent(ccdCaseData);
-    // await certificateOfService2NotifyClaimPage.fillDetails();
-    // await certificateOfService2NotifyClaimPage.submit();
-
-    // const { notifyClaimCOSSubmitPage } = _notifyClaimPageFactory;
-    // await notifyClaimCOSSubmitPage.verifyContent(ccdCaseData);
-
-    // await notifyClaimDetailsCOSSubmitPage.submit();
-  },
-);
+test('Testing Login', async ({ IdamSteps, ExuiDashboardSteps }) => {
+  await IdamSteps.ClaimantSolicitorLogin();
+  await ExuiDashboardSteps.GoToCaseList();
+  await IdamSteps.DefendantSolicitor1Login();
+  await ExuiDashboardSteps.GoToCaseList();
+  await IdamSteps.DefendantSolicitor2Login();
+  await ExuiDashboardSteps.GoToCaseList();
+});
