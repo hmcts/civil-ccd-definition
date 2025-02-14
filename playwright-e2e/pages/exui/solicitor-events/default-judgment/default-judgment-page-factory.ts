d0679b1889cfe142571c0325c6a32f7c3672bec2
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-02-14 11:55:15 +0000

    remove page objects from default judgment factory

--- playwright-e2e/pages/exui/solicitor-events/default-judgment/default-judgment-page-factory.ts
+++ playwright-e2e/pages/exui/solicitor-events/default-judgment/default-judgment-page-factory.ts
@@ -1,23 +1,3 @@
 import BasePageFactory from '../../../../base/base-page-factory';
-import DefendantDetailsPage from './unspec/defendant-details/defendant-details-page';
-import ShowCertifyStatmentPage from './unspec/show-certify-statement/show-certify-statement-page';
-import HearingTypePage from './unspec/hearing-type/hearing-type-page';
-import HearingSupportRequirementsFieldPage from './unspec/hearing-support-requirements-field/hearing-support-requirements-field-page';
 
-export default class DefaultJudgmentPageFactory extends BasePageFactory {
-  get defendantDetailsPage() {
-    return new DefendantDetailsPage(this.page);
-  }
-
-  get showCertifyStatementPage() {
-    return new ShowCertifyStatmentPage(this.page);
-  }
-
-  get hearingTypePage() {
-    return new HearingTypePage(this.page);
-  }
-
-  get hearingSupportRequirementsFieldPage() {
-    return new HearingSupportRequirementsFieldPage(this.page);
-  }
-}
+export default class DefaultJudgmentPageFactory extends BasePageFactory {}
