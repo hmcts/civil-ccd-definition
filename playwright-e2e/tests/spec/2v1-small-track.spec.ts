81f96dbaf871b691ff3534f0cdc673a40cd1956c
jamessmith-hmcts <James.Smith2@HMCTS.NET>
2025-02-19 09:50:09 +0000

    add later changes from 1517

--- playwright-e2e/tests/spec/2v1-small-track.spec.ts
+++ playwright-e2e/tests/spec/2v1-small-track.spec.ts
@@ -1,17 +1,21 @@
 import { test } from '../../playwright-fixtures/index';
 
-test('Test 2v1 small track spec', async ({
-  ClaimantSolicitorSpecSteps,
-  DefendantSolicitor1SpecSteps,
-  ClaimantSolicitorApiSteps,
-  CaseRoleAssignmentApiSteps,
-}) => {
-  await ClaimantSolicitorSpecSteps.Login();
-  await ClaimantSolicitorSpecSteps.CreateClaimSmallTrack2v1();
-  await ClaimantSolicitorApiSteps.MakePaymentForClaimIssue();
-  await CaseRoleAssignmentApiSteps.AssignCaseRoleToDefendant1();
-  await DefendantSolicitor1SpecSteps.Login();
-  await DefendantSolicitor1SpecSteps.RespondSmallTrackFullDefence2v1();
-  await ClaimantSolicitorSpecSteps.Login();
-  await ClaimantSolicitorSpecSteps.SmallClaimIntentToProceed2v1();
-});
+test(
+  'Test 2v1 small track spec',
+  { tag: '@debug' },
+  async ({
+    ClaimantSolicitorSpecSteps,
+    DefendantSolicitor1SpecSteps,
+    ClaimantSolicitorApiSteps,
+    CaseRoleAssignmentApiSteps,
+  }) => {
+    await ClaimantSolicitorSpecSteps.Login();
+    await ClaimantSolicitorSpecSteps.CreateClaimSmallTrack2v1();
+    await ClaimantSolicitorApiSteps.MakePaymentForClaimIssue();
+    await CaseRoleAssignmentApiSteps.AssignCaseRoleToDefendant1();
+    await DefendantSolicitor1SpecSteps.Login();
+    await DefendantSolicitor1SpecSteps.RespondSmallTrackFullDefence2v1();
+    await ClaimantSolicitorSpecSteps.Login();
+    await ClaimantSolicitorSpecSteps.SmallClaimIntentToProceed2v1();
+  },
+);
