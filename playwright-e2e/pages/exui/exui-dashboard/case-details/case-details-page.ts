872b39caaa82002b3a48666402e93cc912f00d81
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-02-21 11:45:30 +0000

    fixed some content and fixed retryChooseNextStep Method

--- playwright-e2e/pages/exui/exui-dashboard/case-details/case-details-page.ts
+++ playwright-e2e/pages/exui/exui-dashboard/case-details/case-details-page.ts
@@ -83,14 +83,19 @@ export default class CaseDetailsPage extends ExuiPage(BasePage) {
 
   async retryChooseNextStep(ccdEvent: CCDEvent) {
     console.log(`Starting event: ${ccdEvent.name}`);
+    let firstAttempt = true;
     await super.retryAction(
       async () => {
-        await this.selectFromDropdown(0, dropdowns.nextStep.selector);
+        if (!firstAttempt) await super.reload();
         await super.selectFromDropdown(ccdEvent.name, dropdowns.nextStep.selector);
         await super.clickBySelector(buttons.go.selector);
+        firstAttempt = false;
       },
       async () => {
         await super.waitForPageToLoad();
+        await super.expectNoTab(tabs.summary.title, {
+          timeout: 15_000,
+        });
       },
       `Starting event: ${ccdEvent.name} failed, trying again`,
       { retries: 3 },
