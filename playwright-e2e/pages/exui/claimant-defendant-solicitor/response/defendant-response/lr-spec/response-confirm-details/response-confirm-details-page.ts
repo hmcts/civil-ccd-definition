388a187b8396d97f663cbbb97eacfe932a4a972a
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-06 17:40:58 +0000

    fix imports

--- playwright-e2e/pages/exui/claimant-defendant-solicitor/response/defendant-response/lr-spec/response-confirm-details/response-confirm-details-page.ts
+++ playwright-e2e/pages/exui/claimant-defendant-solicitor/response/defendant-response/lr-spec/response-confirm-details/response-confirm-details-page.ts
@@ -2,9 +2,8 @@ import { Page } from 'playwright-core';
 import BasePage from '../../../../../../../base/base-page.ts';
 import { AllMethodsStep } from '../../../../../../../decorators/test-steps.ts';
 import ExuiPage from '../../../../../exui-page/exui-page.ts';
-import { inputs, radioButtons, tableHeadings, heading } from './response-confirm-details-content.ts';
+import { radioButtons, tableHeadings, heading } from './response-confirm-details-content.ts';
 import DateOfBirthFragment from '../../../../../fragments/date/date-of-birth-fragment.ts';
-import { heading, radioButtons } from './response-confirm-details-content.ts';
 import { Party } from '../../../../../../../models/partys.ts';
 import StringHelper from '../../../../../../../helpers/string-helper.ts';
 import SolicitorReferenceFragment from '../../../../../fragments/solicitor-reference/solicitor-reference-fragment.ts';
@@ -35,13 +34,13 @@ export default class ResponseConfirmDetailsPage extends ExuiPage(BasePage) {
       [
         super.expectHeading(heading),
         this.solicitorReferenceFragment.verifyContent(),
-        super.expectText(tableHeadings.organisation, {count:1}),
+        super.expectText(tableHeadings.organisation, { count: 1 }),
         // super.expectText(tableHeadings.reference, {count:1}),  not working for 1v2DS
         // super.expectText(inputs.defendant1DateOfBirth.label,  {count:1}),
         // this.dateOfBirthFragment.verifyContent(),
-        super.expectLegend(radioButtons.address.label, {count:1}),
+        super.expectLegend(radioButtons.address.label, { count: 1 }),
         super.expectRadioYesLabel(radioButtons.address.yes.selector(this.defendantParty)),
-        super.expectRadioNoLabel(radioButtons.address.no.selector(this.defendantParty))
+        super.expectRadioNoLabel(radioButtons.address.no.selector(this.defendantParty)),
       ],
       { axePageInsertName: StringHelper.capitalise(this.solicitorParty.key) },
     );
