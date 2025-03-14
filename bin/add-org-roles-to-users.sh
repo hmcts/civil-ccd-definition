7bbc47e406de91a3eecb7048ce6bfde7efa206f8
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2025-03-14 17:44:18 +0000

    Update add-org-roles-to-users.sh

--- bin/add-org-roles-to-users.sh
+++ bin/add-org-roles-to-users.sh
@@ -29,7 +29,7 @@ echo "Setting up Users with role assignments..."
 ./bin/utils/organisational-role-assignment.sh "hcaadmin@justice.gov.uk" "${DEFAULT_PASSWORD}" "PUBLIC" "hmcts-admin" '{"jurisdiction":"CIVIL","primaryLocation":"20262"}' "ADMIN"
 ./bin/utils/organisational-role-assignment.sh "hcaadmin@justice.gov.uk" "${DEFAULT_PASSWORD}" "PUBLIC" "hmcts-viewer" '{"jurisdiction":"CIVIL","primaryLocation":"20262"}' "ADMIN"
 ./bin/utils/organisational-role-assignment.sh "hcaadmin@justice.gov.uk" "${DEFAULT_PASSWORD}" "PUBLIC" "hearing-manager" '{"jurisdiction":"CIVIL","primaryLocation":"20262"}' "ADMIN"
-./bin/utils/organisational-role-assignment.sh "hcaadmin@justice.gov.uk" "${DEFAULT_PASSWORD}" "PUBLIC" "hearing-centre-team-leader" '{"jurisdiction":"CIVIL","primaryLocation":"20262","workTypes":"hearing_work,access_requests,routine_work"}' "ADMIN"
+./bin/utils/organisational-role-assignment.sh "hcaadmin@justice.gov.uk" "${DEFAULT_PASSWORD}" "PUBLIC" "hearing-centre-admin" '{"jurisdiction":"CIVIL","primaryLocation":"20262","workTypes":"hearing_work,access_requests,routine_work"}' "ADMIN"
 
 ./bin/utils/organisational-role-assignment.sh "4924246EMP-@ejudiciary.net" "${JUDGE_DEFAULT_PASSWORD}" "PUBLIC" "hmcts-judiciary" '{"jurisdiction":"CIVIL","primaryLocation":"214320"}' "JUDICIAL"
 ./bin/utils/organisational-role-assignment.sh "4924246EMP-@ejudiciary.net" "${JUDGE_DEFAULT_PASSWORD}" "PUBLIC" "judge" '{"jurisdiction":"CIVIL","primaryLocation":"214320","workTypes":"hearing_work,decision_making_work,applications"}' "JUDICIAL"
