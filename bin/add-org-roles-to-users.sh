9e9bda6bd7cd3881c8742411763a243949bf8e9f
jors95-moj <jo.richards-steele@hmcts.net>
2025-03-14 17:29:19 +0000

    Update add-org-roles-to-users.sh

--- bin/add-org-roles-to-users.sh
+++ bin/add-org-roles-to-users.sh
@@ -27,7 +27,8 @@ echo "Setting up Users with role assignments..."
 ./bin/utils/organisational-role-assignment.sh "hearing_center_admin_reg1@justice.gov.uk" "${DEFAULT_PASSWORD}" "PUBLIC" "hearing-centre-admin" '{"jurisdiction":"CIVIL","primaryLocation":"20262","workTypes":"hearing_work,routine_work,multi_track_hearing_work,intermediate_track_hearing_work"}' "ADMIN"
 
 /bin/utils/organisational-role-assignment.sh “hcaadmin@justice.gov.uk” “${CITIZEN_PASSWORD}” “PUBLIC” “hmcts-admin” ‘{“jurisdiction”:“CIVIL”,“primaryLocation”:“20262"}’ “ADMIN”
-./bin/utils/organisational-role-assignment.sh “hcaadmin@justice.gov.uk” “${CITIZEN_PASSWORD}” “PUBLIC” “hearing-manager” ‘{“jurisdiction”:“CIVIL”,“primaryLocation”:“20262”}’ “ADMIN”./bin/utils/organisational-role-assignment.sh “hearing_center_admin_reg2@justice.gov.uk” “${CITIZEN_PASSWORD}” “PUBLIC” “hearing-centre-admin” ‘{“jurisdiction”:“CIVIL”,“primaryLocation”:“20262",“workTypes”:“hearing_work”}’ “ADMIN”
+./bin/utils/organisational-role-assignment.sh “hcaadmin@justice.gov.uk” “${CITIZEN_PASSWORD}” “PUBLIC” “hearing-manager” ‘{“jurisdiction”:“CIVIL”,“primaryLocation”:“20262”}’ “ADMIN”
+./bin/utils/organisational-role-assignment.sh “hcaadmin@justice.gov.uk” “${CITIZEN_PASSWORD}” “PUBLIC” “hearing-centre-admin” ‘{“jurisdiction”:“CIVIL”,“primaryLocation”:“20262",“workTypes”:“hearing_work”}’ “ADMIN”
 ./bin/utils/organisational-role-assignment.sh “hcaadmin@justice.gov.ukk” “${CITIZEN_PASSWORD}” “PUBLIC” “hearing-viewer” ‘{“jurisdiction”:“CIVIL”,“primaryLocation”:“20262”}’ “ADMIN”
 
 ./bin/utils/organisational-role-assignment.sh "4924246EMP-@ejudiciary.net" "${JUDGE_DEFAULT_PASSWORD}" "PUBLIC" "hmcts-judiciary" '{"jurisdiction":"CIVIL","primaryLocation":"214320"}' "JUDICIAL"
