0702f19d0100c100cd8424345b1b1a08012afe35
drummondjm <mark.drummond@version1.com>
2025-03-18 12:21:59 +0000

    temp skip create SDO tests API

--- e2e/tests/api_tests/automated_hearing_notice/hearing_notice_scheduler_test.js
+++ e2e/tests/api_tests/automated_hearing_notice/hearing_notice_scheduler_test.js
@@ -21,17 +21,17 @@ Scenario.skip('Create Unspec claim with sdo', async ({api}) => {
   await api.createSDO(judgeUser, 'CREATE_FAST');
 }).retry(3);
 
-Scenario('Generate Unspec Disposal hearing notice', async ({hearings}) => {
+Scenario.skip('Generate Unspec Disposal hearing notice', async ({hearings}) => {
   const hearingId = await hearings.createUnspecDisposalHearing(caseId);
   await hearings.triggerUnspecAutomatedHearingNoticeScheduler(hearingId);
 }).retry(3);
 
-Scenario('Generate Unspec Trial hearing notice', async ({hearings}) => {
+Scenario.skip('Generate Unspec Trial hearing notice', async ({hearings}) => {
     const hearingId = await hearings.createUnspecTrialHearing(caseId);
     await hearings.triggerUnspecAutomatedHearingNoticeScheduler(hearingId);
 }).retry(3);
 
-Scenario('Generate Unspec Dispute Resolution hearing notice', async ({hearings}) => {
+Scenario.skip('Generate Unspec Dispute Resolution hearing notice', async ({hearings}) => {
   const hearingId = await hearings.createUnspecDisputeResolutionHearing(caseId);
   await hearings.triggerUnspecAutomatedHearingNoticeScheduler(hearingId);
 }).retry(3);
@@ -43,17 +43,17 @@ Scenario.skip('Create Spec claim with SDO', async ({api_spec_small}) => {
   await api_spec_small.createSDO(config.judgeUser2WithRegionId4, 'CREATE_SMALL', true);
 }).retry(3);
 
-Scenario('Generate Spec Disposal hearing notice', async ({hearings}) => {
+Scenario.skip('Generate Spec Disposal hearing notice', async ({hearings}) => {
   const hearingId = await hearings.createSpecDisposalHearing(caseId);
   await hearings.triggerSpecAutomatedHearingNoticeScheduler(hearingId);
 }).retry(3);
 
-Scenario('Generate Spec Trial hearing notice', async ({hearings}) => {
+Scenario.skip('Generate Spec Trial hearing notice', async ({hearings}) => {
   const hearingId = await hearings.createSpecTrialHearing(caseId);
   await hearings.triggerSpecAutomatedHearingNoticeScheduler(hearingId);
 }).retry(3);
 
-Scenario('Generate Spec Dispute Resolution hearing notice', async ({hearings}) => {
+Scenario.skip('Generate Spec Dispute Resolution hearing notice', async ({hearings}) => {
   const hearingId = await hearings.createSpecDisputeResolutionHearing(caseId);
   await hearings.triggerSpecAutomatedHearingNoticeScheduler(hearingId);
 }).retry(3);
