a87d73bcab1329987472f6620fbc87c83e1762e5
Velican <duzgunce.vd@gmail.com>
2025-03-19 16:59:38 +0000

    Revert disabling temp failing FT

--- e2e/tests/api_tests/sdo_R2/api_dispute_resolution_hearing_test.js
+++ e2e/tests/api_tests/sdo_R2/api_dispute_resolution_hearing_test.js
@@ -10,7 +10,7 @@ const claimAmount = '100';
 
 let mediationAdminRegion4 = config.localMediationTests ? config.nbcUserLocal : config.nbcUserWithRegionId4;
 
-Feature('Dispute resolution hearing API test - fast claim - unspec @api-unspec @api-tests-1v1 @api-r2-sdo');
+Feature('Dispute resolution hearing API test - fast claim - unspec @api-unspec @api-tests-1v1 @api-prod @api-r2-sdo');
 
 async function prepareClaim(api) {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario1v1, claimAmount);
@@ -27,7 +27,7 @@ async function prepareClaim1v1(api_spec_small, carmEnabled) {
   await api_spec_small.claimantResponse(config.applicantSolicitorUser, true, 'No', carmEnabled);
 }
 
-Scenario.skip('1v1 unspec create SDO for DRH', async ({api}) => {
+Scenario('1v1 unspec create SDO for DRH', async ({api}) => {
     await prepareClaim(api);
     await api.createSDO(judgeUser, 'CREATE_SMALL_DRH');
     await api.evidenceUploadApplicant(config.applicantSolicitorUser, mpScenario1v1, 'DRH');
@@ -41,7 +41,7 @@ Scenario.skip('1v1 unspec create SDO for DRH', async ({api}) => {
     await api.createFinalOrderJO(judgeUser, 'FREE_FORM_ORDER');
 });
 
-Scenario.skip('1v1 spec small create SDO for DRH - CARM enabled', async ({api_spec_small}) => {
+Scenario('1v1 spec small create SDO for DRH - CARM enabled', async ({api_spec_small}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     await prepareClaim1v1(api_spec_small, true);
     await api_spec_small.mediationUnsuccessful(mediationAdminRegion4, true);
@@ -51,7 +51,7 @@ Scenario.skip('1v1 spec small create SDO for DRH - CARM enabled', async ({api_sp
   }
 });
 
-Scenario.skip('1v1 spec small create SDO for DRH - CARM disabled', async ({api_spec_small}) => {
+Scenario('1v1 spec small create SDO for DRH - CARM disabled', async ({api_spec_small}) => {
   if (['preview', 'demo'].includes(config.runningEnv)) {
     await prepareClaim1v1(api_spec_small, false);
     await api_spec_small.uploadMediationDocuments(config.applicantSolicitorUser);
