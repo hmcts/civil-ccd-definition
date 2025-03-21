9d57e2df34ac40c8f09e2a161fb18abee4a64aa3
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2025-03-21 11:01:02 +0000

    add madhans changes back

--- e2e/tests/api_tests/multiIntermediateTrack/api_multi_unspec_test.js
+++ e2e/tests/api_tests/multiIntermediateTrack/api_multi_unspec_test.js
@@ -6,18 +6,35 @@ const judgeUser = config.judgeUserWithRegionId1;
 const hearingCenterAdminToBeUsed = config.hearingCenterAdminWithRegionId1;
 let caseId, taskId, intermediateTrackOrderMadeReviewCaseExpectedTask, multiTrackDirectionsExpectedTask;
 
-
+if (config.runWAApiTest) {
+  multiTrackDirectionsExpectedTask = require('../../../../wa/tasks/multiTrackDirectionsTask.js');
+  intermediateTrackOrderMadeReviewCaseExpectedTask = require('../../../../wa/tasks/intermediateTrackOrderMadeReviewCaseTask.js');
+}
 Feature('CCD API test unspec multi track @api-unspec-multi-intermediate');
 
-async function prepareClaim(api, mpScenario, claimAmount) {
+async function prepareClaim(api, mpScenario, claimAmount, WA) {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, claimAmount, mintiEnabled);
   await api.notifyClaim(config.applicantSolicitorUser);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
   await defendantResponse(api, mpScenario);
   await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO', track);
   caseId = await api.getCaseId();
+  if (config.runWAApiTest) {
+    const multiTrackDirections = await api.retrieveTaskDetails(config.judgeUserWithRegionId1, caseId, config.waTaskIds.multiTrackDirections);
+    console.log('multiTrackDirections...', multiTrackDirections);
+    WA.validateTaskInfo(multiTrackDirections, multiTrackDirectionsExpectedTask);
+    taskId = multiTrackDirections['id'];
+    api.assignTaskToUser(config.judgeUserWithRegionId1, taskId);
+  }
   await api.createFinalOrder(judgeUser, 'DOWNLOAD_ORDER_TEMPLATE', 'MULTI');
   await api.evidenceUploadApplicant(config.applicantSolicitorUser, mpScenario);
+  if (config.runWAApiTest) {
+    const multiTrackOrderMakeReview = await api.retrieveTaskDetails(config.hearingCenterAdminWithRegionId1, caseId, config.waTaskIds.multiTrackOrderMadeReview);
+    console.log('multiTrackOrderMakeReview...', multiTrackOrderMakeReview);
+    WA.validateTaskInfo(multiTrackOrderMakeReview, intermediateTrackOrderMadeReviewCaseExpectedTask);
+    taskId = multiTrackOrderMakeReview['id'];
+    api.assignTaskToUser(config.hearingCenterAdminWithRegionId1, taskId);
+  }
   await api.scheduleHearing(hearingCenterAdminToBeUsed, 'FAST_TRACK_TRIAL', true);
 }
 
