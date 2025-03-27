bcf862c2ea4d9b457326e2cd5062736d2f003195
kalachandrasekar1 <114995593+kalachandrasekar1@users.noreply.github.com>
2025-03-27 15:18:56 +0000

    removed commented code

--- e2e/tests/api_tests/judgmentOnline/api_1v1_spec_jba_mark_paid_in_full_1v1_test.js
+++ e2e/tests/api_tests/judgmentOnline/api_1v1_spec_jba_mark_paid_in_full_1v1_test.js
@@ -32,19 +32,6 @@ Scenario('1v1 LiP v LiP defendant response with part admit pay by installments j
   await respondWithPAPayByInstallmentsJBAPaidInFullLipvLip(api_spec_cui);
 });
 
-/*async function prepareClaimLRvLiPExui(api_spec_cui, carmEnabled, claimType = 'SmallClaims') {
-  caseId = await api_spec_cui.createSpecifiedClaimWithUnrepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_ONE', claimType, carmEnabled);
-  await api_spec_cui.performCitizenDefendantResponse(config.defendantCitizenUser2, caseId, claimType, carmEnabled, 'PA_IMMEDIATELY_INDIVIDUAL');
-  await api_spec_cui.claimantResponse(config.applicantSolicitorUser, 'PART_ADMISSION_IMMEDIATELY', 'ONE_V_ONE', 'No', 'AWAITING_APPLICANT_INTENTION', carmEnabled);
-  await api_spec_cui.amendWhenWillThisAmountBePaidDeadLine(config.systemupdate);
-  await api_spec_cui.requestJudgement(config.applicantSolicitorUser, 'REQUEST_JUDGEMENT');
-  await api_spec_cui.markJudgmentPaid(config.applicantSolicitorUser);
-}
-
-Scenario('1v1 LR v LiP defendant response with part admit pay immediately judgment by admission mark paid in full', async ({api_spec_cui}) => {
-  await prepareClaimLRvLiPExui(api_spec_cui, false);
-});*/
-
 AfterSuite(async  ({api_spec_cui}) => {
   await api_spec_cui.cleanUp();
   await deleteAccount(config.defendantCitizenUser2.email);
