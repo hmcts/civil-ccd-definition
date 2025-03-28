4f0efcbcc58188b18ab238dc9a28a230a3c59e78
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-28 14:13:12 +0000

    update

--- playwright-e2e/actions/ui/exui/caseworker/caseworker-actions-factory.ts
+++ playwright-e2e/actions/ui/exui/caseworker/caseworker-actions-factory.ts
@@ -1,9 +1,8 @@
 import BasePageActionsFactory from '../../../../base/base-page-actions-factory';
 import CaseProceedsInCasemanPageFactory from '../../../../pages/exui/caseworker/case-proceeds-in-caseman/case-proceeds-in-caseman-page-factory.ts';
 import CaseProceedsInCasemanActions from './case-proceeds-in-caseman-actions.ts';
-import ManageDocumentsActions from "./manage-documents-actions.ts";
-import ManageDocumentsPageFactory
-  from "../../../../pages/exui/caseworker/manage-documents/manage-documents-page-factory.ts";
+import ManageDocumentsActions from './manage-documents-actions.ts';
+import ManageDocumentsPageFactory from '../../../../pages/exui/caseworker/manage-documents/manage-documents-page-factory.ts';
 
 export default class CaseworkerActionsFactory extends BasePageActionsFactory {
   get caseProceedsInCasemanActions() {
@@ -12,7 +11,7 @@ export default class CaseworkerActionsFactory extends BasePageActionsFactory {
       this.testData,
     );
   }
-    
+
   get manageDocumentsActions() {
     return new ManageDocumentsActions(new ManageDocumentsPageFactory(this.page), this.testData);
   }
