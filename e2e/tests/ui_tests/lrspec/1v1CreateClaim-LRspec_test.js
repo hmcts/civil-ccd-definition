1746869e3e6f50b8a48aec1b8739fe66c7d3f389
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-04-16 13:11:02 +0100

    fix LRspec 1v1 test

--- e2e/tests/ui_tests/lrspec/1v1CreateClaim-LRspec_test.js
+++ e2e/tests/ui_tests/lrspec/1v1CreateClaim-LRspec_test.js
@@ -1,41 +1,27 @@
 const config = require('../../../config.js');
-const {paymentUpdate} = require('../../../api/apiRequest');
-const {assignCaseToLRSpecDefendant, checkToggleEnabled} = require('../../../api/testingSupport');
+const {assignCaseToLRSpecDefendant} = require('../../../api/testingSupport');
 const {addUserCaseMapping, unAssignAllUsers} = require('../../../api/caseRoleAssignmentHelper');
-const {PBAv3} = require('../../../fixtures/featureKeys');
 const serviceRequest = require('../../../pages/createClaim/serviceRequest.page');
 const {PARTY_FLAGS} = require('../../../fixtures/caseFlags');
-const claimData = require('../../../fixtures/events/createClaimSpec.js');
 const apiRequest = require('../../../api/apiRequest');
 // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
 //const caseEventMessage = eventName => `Case ${caseNumber} has been updated with event: ${eventName}`;
-const caseId = () => `${caseNumber.split('-').join('').replace(/#/, '')}`;
 
 let caseNumber;
 
 Feature('Claim creation 1v1 @e2e-spec @e2e-nightly-prod');
 
 Scenario('1v1 Applicant solicitor creates specified claim for fast track @create-claim-spec', async ({LRspec}) => {
-  console.log('1v1 Applicant solicitor creates specified claim for fast track @create-claim-spec');
   await LRspec.login(config.applicantSolicitorUser);
   await LRspec.createCaseSpecified('1v1 fast claim', 'organisation', null, 'company', null, 19000);
   caseNumber = await LRspec.grabCaseNumber();
-
-  const pbaV3 = await checkToggleEnabled(PBAv3);
-  console.log('Is PBAv3 toggle on?: ' + pbaV3);
-
-  if (pbaV3) {
-    await apiRequest.setupTokens(config.applicantSolicitorUser);
-    await serviceRequest.openServiceRequestTab();
-    await serviceRequest.payFee(caseId());
-    await paymentUpdate(caseId(), '/service-request-update-claim-issued',
-      claimData.serviceUpdateDto(caseId(), 'paid'));
-    console.log('Service request update sent to callback URL');
-  }
+  await apiRequest.setupTokens(config.applicantSolicitorUser);
+  await serviceRequest.openServiceRequestTab();
+  await serviceRequest.payFee(caseNumber);
 
   // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
   //await LRspec.see(`Case ${caseNumber} has been created.`);
-  addUserCaseMapping(caseId(), config.applicantSolicitorUser);
+  addUserCaseMapping(caseNumber, config.applicantSolicitorUser);
 }).retry(3);
 
 //As part of CIV-13925 this functionality is hidden
@@ -50,8 +36,8 @@ Scenario.skip('1v1 Claimant solicitor Lift Breathing Space', async ({LRspec}) =>
 }).retry(3);
 
 Scenario('1v1 Defendant solicitor perform Inform Agreed Extension', async ({LRspec}) => {
-  console.log('1v1 Defendant solicitor Inform Agreed Extension claim-spec: ' + caseId());
-  await assignCaseToLRSpecDefendant(caseId());
+  console.log('1v1 Defendant solicitor Inform Agreed Extension claim-spec: ' + caseNumber);
+  await assignCaseToLRSpecDefendant(caseNumber);
   await LRspec.login(config.defendantSolicitorUser);
   await LRspec.informAgreedExtensionDateSpec();
   // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
@@ -67,7 +53,7 @@ Scenario('1v1 Respond To Claim - Defendants solicitor rejects claim for defendan
   });
   // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
   //await LRspec.see(caseEventMessage('Respond to claim'));
-  //await waitForFinishedBusinessProcess(caseId());
+  //await waitForFinishedBusinessProcess(caseNumber);
 }).retry(3);
 
 Scenario('1v1 Claimant solicitor responds to defence - claimant Intention to proceed', async ({LRspec}) => {
@@ -75,7 +61,7 @@ Scenario('1v1 Claimant solicitor responds to defence - claimant Intention to pro
   await LRspec.respondToDefence({mpScenario: 'ONE_V_ONE', claimType: 'fast'});
 }).retry(3);
 
-Scenario.skip('Add case flags', async ({LRspec}) => {
+Scenario('Add case flags', async ({LRspec}) => {
   const caseFlags = [{
     partyName: 'Example applicant1 company', roleOnCase: 'Claimant 1',
     details: [PARTY_FLAGS.vulnerableUser.value]
