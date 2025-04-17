09bf4d579a8db86a2299fee29714d6deaba823d5
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2025-04-17 13:48:54 +0100

    Enabled only QM related api test scenarios

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -3,9 +3,10 @@ set -ex
 
 echo "Running Functional tests on ${ENVIRONMENT} env"
 
-export CCD_UI_TESTS=true
+export CCD_UI_TESTS=false
 if [ ${ENVIRONMENT} == preview ]; then
-  yarn test:non-prod-e2e-ft
+#  yarn test:non-prod-e2e-ft
+  yarn test:test:api-QM
 else
   yarn test:master-e2e-ft
 fi
