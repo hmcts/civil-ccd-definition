9b934e115b8bb00124c9199b5458b8ac0be99d02
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2025-04-24 15:17:27 +0100

    Added api tests for LR query management

--- e2e/tests/api_tests/hearings/api_1v2DS_unspec_fast_hearings_test.js
+++ e2e/tests/api_tests/hearings/api_1v2DS_unspec_fast_hearings_test.js
@@ -1,27 +1,28 @@
 const config = require('../../../config.js');
 const {getSupportWorkerFlag, getDetainedIndividualFlag, getDisruptiveIndividualFlag
 } = require('../../../api/caseFlagsHelper');
-const {checkCaseFlagsAndHmcEnabled} = require('../../../api/testingSupport');
+const {checkCaseFlagsAndHmcEnabled, checkLRQueryManagementEnabled} = require('../../../api/testingSupport');
+const {
+  APPLICANT_SOLICITOR_QUERY,
+  RESPONDENT_SOLICITOR_1_QUERY,
+  RESPONDENT_SOLICITOR_2_QUERY
+} = require('../../../fixtures/queryTypes');
+const {createAccount} = require('../../../api/idamHelper');
 
 const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
 const fastClaimAmount = '11000';
 const serviceId = 'AAA7';
 const hmcTest = true;
 let caseId;
-let caseFlagsAndHmcEnabled = false;
+let isQueryManagementEnabled = false;
 
-let continueWithScenario = () => {
-  return caseFlagsAndHmcEnabled;
-};
+Feature('CCD 1v2 Unspec fast hearings API test @api-hearings-unspec @api-hearings @api-nonprod @QM');
 
-Feature('CCD 1v2 Unspec fast hearings API test @api-hearings-unspec @api-hearings @api-nonprod');
-
-BeforeSuite(async () => {
-  caseFlagsAndHmcEnabled = await checkCaseFlagsAndHmcEnabled();
+Before(async () => {
+  isQueryManagementEnabled = await checkLRQueryManagementEnabled();
 });
 
 Scenario('1v2DS full defence defendant and claimant response', async ({api}) => {
-  if(!continueWithScenario()) return;
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, fastClaimAmount, false, hmcTest);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
@@ -33,18 +34,42 @@ Scenario('1v2DS full defence defendant and claimant response', async ({api}) => 
 });
 
 Scenario('Listing officer adds case flags', async ({hearings}) => {
-  if(!continueWithScenario()) return;
   await hearings.createCaseFlags(config.hearingCenterAdminWithRegionId2, caseId, 'respondent1', getDetainedIndividualFlag());
   await hearings.createCaseFlags(config.hearingCenterAdminWithRegionId2, caseId, 'respondent1', getDisruptiveIndividualFlag());
   await hearings.createCaseFlags(config.hearingCenterAdminWithRegionId2, caseId, 'respondent2Witnesses', getSupportWorkerFlag());
 });
 
+Scenario('Claimant queries', async ({api, qmSteps}) => {
+  if (isQueryManagementEnabled) {
+    const caseId = await api.getCaseId();
+    const applicantSolicitorQuery = await qmSteps.raiseQuery(caseId, config.applicantSolicitorUser, APPLICANT_SOLICITOR_QUERY);
+    await qmSteps.respondToQuery(caseId, config.hearingCenterAdminWithRegionId1, applicantSolicitorQuery, APPLICANT_SOLICITOR_QUERY);
+    await qmSteps.followUpOnQuery(caseId, config.applicantSolicitorUser, applicantSolicitorQuery, APPLICANT_SOLICITOR_QUERY);
+  }
+});
+
+Scenario('Defendant 1 solicitor queries', async ({api, qmSteps}) => {
+  if (isQueryManagementEnabled) {
+    const caseId = await api.getCaseId();
+    const defendantSolicitorQuery = await qmSteps.raiseQuery(caseId, config.defendantSolicitorUser, RESPONDENT_SOLICITOR_1_QUERY);
+    await qmSteps.respondToQuery(caseId, config.hearingCenterAdminWithRegionId2, defendantSolicitorQuery, RESPONDENT_SOLICITOR_1_QUERY);
+    await qmSteps.followUpOnQuery(caseId, config.defendantSolicitorUser, defendantSolicitorQuery, RESPONDENT_SOLICITOR_1_QUERY);
+  }
+});
+
+Scenario('Defendant 2 solicitor queries', async ({api, qmSteps}) => {
+  if (isQueryManagementEnabled) {
+    const caseId = await api.getCaseId();
+    const defendantSolicitorQuery = await qmSteps.raiseQuery(caseId, config.secondDefendantSolicitorUser, RESPONDENT_SOLICITOR_2_QUERY);
+    await qmSteps.respondToQuery(caseId, config.hearingCenterAdminWithRegionId2, defendantSolicitorQuery, RESPONDENT_SOLICITOR_2_QUERY);
+    await qmSteps.followUpOnQuery(caseId, config.secondDefendantSolicitorUser, defendantSolicitorQuery, RESPONDENT_SOLICITOR_2_QUERY);
+  }
+});
+
 Scenario('Judge choose hearing in person', async ({api}) => {
-  if(!continueWithScenario()) return;
   await api.createSDO(config.judgeUser2WithRegionId2, 'CREATE_FAST_IN_PERSON');
 });
 
 Scenario('Hearing centre admin requests a hearing', async ({hearings}) => {
-  if(!continueWithScenario()) return;
   await hearings.generateHearingsPayload(config.hearingCenterAdminWithRegionId2, caseId, serviceId);
 });
