5e59bf87c3bf7157881bdfca8b78f0f1d0fa08b9
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-04-29 14:10:43 +0100

    fix last tests

--- e2e/tests/ui_tests/carm/spec_claim_upload_mediation_doc_test.js
+++ e2e/tests/ui_tests/carm/spec_claim_upload_mediation_doc_test.js
@@ -4,11 +4,8 @@ const config = require('../../../config.js');
 const legalAdvisorUser = config.tribunalCaseworkerWithRegionId4;
 const hearingCenterAdminToBeUsed = config.hearingCenterAdminWithRegionId1;
 
-let civilCaseReference, validSummaryLegalAdvisorDirectionsTask;
+let civilCaseReference;
 
-if (config.runWAApiTest) {
-  validSummaryLegalAdvisorDirectionsTask = require('../../../../wa/tasks/legalAdvisorSmallClaimsTrackDirectionsTask.js');
-}
 
 Feature('SDO Carm - Upload mediation documents @e2e-nightly-prod @e2e-carm');
 
@@ -30,7 +27,7 @@ Scenario('2v1 defendant upload mediation docs', async ({LRspec}) => {
   await LRspec.uploadMediationDocs(civilCaseReference, 'Defendant 1', 'Non-attendance');
 }).retry(2);
 
-Scenario('1v2 upload mediation documents in different SDO states', async ({api_spec, LRspec, WA}) => {
+Scenario('1v2 upload mediation documents in different SDO states', async ({api_spec, LRspec}) => {
   civilCaseReference = await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   await api_spec.defendantResponse(config.defendantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO');
   await api_spec.claimantResponse(config.applicantSolicitorUser, 'FULL_DEFENCE', 'ONE_V_TWO', 'JUDICIAL_REFERRAL');
@@ -38,35 +35,20 @@ Scenario('1v2 upload mediation documents in different SDO states', async ({api_s
   console.log('1v2 Spec claims created : ' + civilCaseReference);
 
   console.log('Create SDO');
-  let taskId;
-
-  if (config.runWAApiTest) {
-    const legalAdvisorDirectionsTask = await api_spec.retrieveTaskDetails(legalAdvisorUser, civilCaseReference, config.waTaskIds.legalAdvisorDirections);
-    console.log('legalAdvisorDirectionsTask...' , legalAdvisorDirectionsTask);
-    WA.validateTaskInfo(legalAdvisorDirectionsTask, validSummaryLegalAdvisorDirectionsTask);
-    taskId = legalAdvisorDirectionsTask['id'];
-    api_spec.assignTaskToUser(legalAdvisorUser, taskId);
-  }
-  await LRspec.login(legalAdvisorUser);
-  await LRspec.initiateSDO(null, null, 'smallClaims', null, civilCaseReference);
-  await LRspec.wait(10);
-
-  if (config.runWAApiTest) {
-    api_spec.completeTaskByUser(legalAdvisorUser, taskId);
-  }
+  await api_spec.createSDO(legalAdvisorUser, 'CREATE_SMALL');
 
   await LRspec.login(config.applicantSolicitorUser);
   await LRspec.uploadMediationDocs(civilCaseReference, 'Claimant 1', 'Both docs');
 
   console.log('Schedule Hearing');
-  await LRspec.login(hearingCenterAdminToBeUsed);
-  await LRspec.createHearingScheduled();
+  await api_spec.scheduleHearing(hearingCenterAdminToBeUsed, 'SMALL_CLAIMS');
   await LRspec.login(config.defendantSolicitorUser);
   await LRspec.uploadMediationDocs(civilCaseReference, 'Defendant 1', 'Non-attendance');
 
   console.log('Prepare for Hearing Conduct Hearing');
   await api_spec.amendHearingDueDate(config.systemupdate);
   await api_spec.hearingFeePaid(hearingCenterAdminToBeUsed);
+  await LRspec.login(config.defendantSolicitorUser);
   await LRspec.uploadMediationDocs(civilCaseReference, 'Defendant 2', 'Both docs');
 }).retry(2);
 
