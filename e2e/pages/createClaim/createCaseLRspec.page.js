069844a12652dd75aefd8f96c412cd0d8a57de00
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-05-01 11:28:31 +0100

    change start event to use faster url

--- e2e/pages/createClaim/createCaseLRspec.page.js
+++ e2e/pages/createClaim/createCaseLRspec.page.js
@@ -5,7 +5,7 @@ const config = require('../../config.js');
 module.exports = {
    async createCaseSpecified() {
         await I.waitForText('Case list');
-        await I.amOnPage(config.url.manageCase + '/cases/case-create/CIVIL/' + config.definition.caseType + '/CREATE_CLAIM_SPEC');
+        await I.amOnPage(config.url.manageCase + '/cases/case-create/CIVIL/' + config.definition.caseType + '/CREATE_CLAIM_SPEC/CREATE_CLAIM_SPEC');
         await I.waitForText('Legal representatives: specified civil money claims service', 60);
     }
 };
