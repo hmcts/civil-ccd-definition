cb7a2e65797a8b3e48722f788a84b027d80be379
Mike Lemos <gobii@me.com>
2025-06-12 10:24:56 +0100

    DTSCCI-1897 fixing nightlies

--- e2e/tests/api_tests/damages/api_1v2_different_solicitor_test.js
+++ e2e/tests/api_tests/damages/api_1v2_different_solicitor_test.js
@@ -5,7 +5,7 @@ const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
 
 // add @api-tests to run
 
-Feature('CCD 1v2 Different Solicitor API test @api-unspec @api-multiparty @api-tests-1v2DS @api-nightly-prod @api-unspec-full-defence @mike');
+Feature('CCD 1v2 Different Solicitor API test @api-unspec @api-multiparty @api-tests-1v2DS @api-nightly-prod @api-unspec-full-defence');
 
 Scenario('Create claim', async ({I, api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
