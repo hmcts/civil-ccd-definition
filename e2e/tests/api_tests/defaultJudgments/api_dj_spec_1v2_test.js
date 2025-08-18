8efbce739d5dab041756947ecf0e2235105e6ea2
Johnny Vineall <johnny.vineall@HMCTS.NET>
2025-08-18 11:30:31 +0100

    Update api_dj_spec_1v2_test.js

--- e2e/tests/api_tests/defaultJudgments/api_dj_spec_1v2_test.js
+++ e2e/tests/api_tests/defaultJudgments/api_dj_spec_1v2_test.js
@@ -3,7 +3,7 @@
 const config = require('../../../config.js');
 const mpScenario = 'ONE_V_TWO';
 
-Feature('CCD 1v2 Spec API test @api-dj-1v2, @api-dj @api-prod-dj');
+Feature('CCD 1v2 Spec API test @api-dj-1v2, @api-dj');
 
 Scenario('Default Judgment Spec claim 1v2 non divergent', async ({I, api_spec}) => {
   await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario, false );
