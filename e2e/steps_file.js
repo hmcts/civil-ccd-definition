c0e1e43e699b1d905b26d8ec6416a3f3852d193e
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-09-11 14:53:43 +0100

    remove test case id

--- e2e/steps_file.js
+++ e2e/steps_file.js
@@ -179,7 +179,7 @@ const CONFIRMATION_MESSAGE = {
   offline: 'Your claim has been received and will progress offline',
 };
 
-let caseId = '1757346700999352', screenshotNumber, eventName, currentEventName, loggedInUser;
+let caseId, screenshotNumber, eventName, currentEventName, loggedInUser;
 let eventNumber = 0;
 
 const isTestEnv = ['preview', 'demo'].includes(config.runningEnv);
