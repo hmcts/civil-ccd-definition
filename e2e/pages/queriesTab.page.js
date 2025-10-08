06eae032e988ab58451031e3c909da1c11334671
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-10-08 14:27:44 +0100

    Fix QM test

--- e2e/pages/queriesTab.page.js
+++ e2e/pages/queriesTab.page.js
@@ -75,7 +75,6 @@ module.exports = {
     if (party) {
       I.fillField('textarea[id="body"]', 'Claimant follow up');
     }
-    I.click('Continue');
     I.retryUntilExists(() => I.click('Continue'), '//h1[contains(text(), \'Review query details\')]');
   },
 
