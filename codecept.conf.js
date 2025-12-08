a76c37fbe123478018f499e49c834eab771b12d6
Sabah u Din Irfan <sabah.irfan@gmail.com>
2025-12-08 09:05:35 +0000

    DTSCCI-3413 Optimise Functional/E2E Test Performance (#6170)

--- codecept.conf.js
+++ codecept.conf.js
@@ -81,7 +81,7 @@ exports.config = {
     Playwright: {
       url: process.env.URL || 'http://localhost:3333',
       show: process.env.SHOW_BROWSER_WINDOW === 'true' || false,
-      waitForTimeout: parseInt(process.env.WAIT_FOR_TIMEOUT_MS || 90000),
+      waitForTimeout: parseInt(process.env.WAIT_FOR_TIMEOUT_MS || 30000), // 30 seconds (reduced from 90 seconds)
       windowSize: '1280x960',
       browser: 'chromium',
       timeout: 20000,
