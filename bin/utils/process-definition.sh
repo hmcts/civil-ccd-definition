076bd773d9380752bbec87e6c7db7283bee49f10
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-12-29 14:05:47 +0000

    DTSCCI-3484 changes

--- bin/utils/process-definition.sh
+++ bin/utils/process-definition.sh
@@ -20,6 +20,6 @@ docker run --rm --name json2xlsx \
   -e CCD_DEF_CASE_SERVICE_BASE_URL=${CCD_DEF_CASE_SERVICE_BASE_URL:-https://civil-ccd-pr-6199.preview.platform.hmcts.net} \
   -e CCD_DEF_GEN_APP_SERVICE_BASE_URL=${CCD_DEF_GEN_APP_SERVICE_BASE_URL:-https://ga-civil-ccd-pr-6199.preview.platform.hmcts.net} \
   -e CCD_DEF_AAC_URL=${CCD_DEF_AAC_URL:-https://manage-case-assignment-civil-ccd-pr-6199.preview.platform.hmcts.net} \
-  -e CCD_DEF_VERSION=${CCD_DEF_VERSION:-} \
+  -e CCD_DEF_VERSION=${CCD_PREVIEW_DEF_VERSION:-} \
   hmctspublic.azurecr.io/ccd/definition-processor:${definition_processor_version} \
   json2xlsx -D /tmp/ccd-definition -o /tmp/ccd-definition.xlsx ${additionalParameters}
