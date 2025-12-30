9a1292b8799a963f2c4045c86ed56998a1439ac4
Anirudha <67697720+anirudha-deshpande@users.noreply.github.com>
2025-12-30 16:21:41 +0000

    DTSCCI-3484 script changes

--- bin/import-local-definition-to-preview.sh
+++ bin/import-local-definition-to-preview.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -eu
+repoType=${1:-ccd}
+prNumber=${2:-'6199'}
+
 
 # Ensure dependencies have execution permissions
 chmod +x ./bin/variables/load-dev-user-preview-environment-variables.sh
@@ -8,8 +11,18 @@ chmod +x ./bin/utils/ccd-import-definition.sh
 chmod +x ./bin/utils/idam-lease-user-token.sh
 chmod +x ./bin/utils/idam-lease-service-token.sh
 
+if [[ "${repoType}" == 'ccd' ]]; then
+  echo "Loading environment variables repoType : (${repoType}) prNumber: ${prNumber}";
+  source .env.ccd.local "${prNumber}"
+fi
+
+if [[ "${repoType}" == 'cui' ]]; then
+  echo "Loading environment variables (${repoType})";
+  source .env.cui.local "${prNumber}"
+fi
+
 # Load environment variables for the preview environment
-source ./bin/variables/load-dev-user-preview-environment-variables.sh
+source ./bin/variables/load-dev-user-local-environment-variables.sh
 
 export CCD_DEF_VERSION=""
 
