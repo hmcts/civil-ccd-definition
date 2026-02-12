43c9dffd3f2f026d3b79ce642f1e69d1697baf26
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2026-02-12 12:04:58 +0000

    remove e2e-ga folder

--- e2e-ga/e2e-documentation/generator/support/dependent-ui-features.js
+++
@@ -1,5 +0,0 @@
-/**
- * Dependent UI scenario files (relative paths using forward slashes).
- * Add file paths here when scenarios in the file must run sequentially as one journey.
- */
-module.exports = [];
--- e2e-ga/fixtures/featureKeys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PBAv3: 'pba-version-3-ways-to-pay',
-  COSC:'isCoSCEnabled',
-  isJOLive:'isJudgmentOnlineLive'
-};
