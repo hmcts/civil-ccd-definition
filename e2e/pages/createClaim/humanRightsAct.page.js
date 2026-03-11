1114084713907b33f001e6c3c9d589eaab8b4daa
Raja <raja.mani@hmcts.net>
2026-03-11 15:46:57 +0000

    added test for OtherRemedy features

--- e2e/pages/createClaim/humanRightsAct.page.js
+++ e2e/pages/createClaim/humanRightsAct.page.js
@@ -12,12 +12,17 @@ module.exports = {
     }
   },
 
-  async humanRightsAct() {
+  async humanRightsAct(option) {
     I.waitForElement(this.fields.isHumanRightsActIssuesAdded.id);
-    await I.runAccessibilityTest();
-    await within(this.fields.isHumanRightsActIssuesAdded.id, () => {
-      I.click(this.fields.isHumanRightsActIssuesAdded.options.yes);
-    });
+    if(option === 'yes') {
+      await within(this.fields.isHumanRightsActIssuesAdded.id, () => {
+        I.click(this.fields.isHumanRightsActIssuesAdded.options.yes);
+      });
+    } else {
+      await within(this.fields.isHumanRightsActIssuesAdded.id, () => {
+        I.click(this.fields.isHumanRightsActIssuesAdded.options.no);
+      });
+    }
     await I.clickContinue();
   }
 };
