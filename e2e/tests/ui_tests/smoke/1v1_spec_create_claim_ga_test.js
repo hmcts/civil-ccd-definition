1431f804d38007228b53caa5ecbec07e6785f298
Mike Lemos <gobii@me.com>
2026-03-13 17:02:33 +0000

    DTSCCI-3653 merging GA history in

--- e2e/tests/ui_tests/smoke/1v1_spec_create_claim_ga_test.js
+++ e2e/tests/ui_tests/smoke/1v1_spec_create_claim_ga_test.js
@@ -7,12 +7,25 @@ let civilCaseReference, gaCaseReference;
 Feature('Smoke test - API 1v1 spec create claim and create general application').tag('@civil-ccd-smoke');
 
 Scenario('1v1 spec create claim and create general application', async ({I, api_ga}) => {
-  civilCaseReference = await api_ga.createSpecifiedClaim(config.applicantSolicitorUser, mpScenario);
-  gaCaseReference = await api_ga.initiateGeneralApplication(config.applicantSolicitorUser, civilCaseReference);
-  await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(civilCaseReference);
-  await I.navigateToCaseDetails(gaCaseReference);
-}).retry(1);
+  civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', claimAmountJudge);
+  await api.amendClaimDocuments(config.applicantSolicitorUser);
+  await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
+  await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
+  await api.acknowledgeClaim(config.defendantSolicitorUser, civilCaseReference, true);
+  await api.defendantResponseClaim(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
+  await api.claimantResponseUnSpec(config.applicantSolicitorUser, mpScenario, 'JUDICIAL_REFERRAL');
+  console.log('Civil Case created for general application: ' + civilCaseReference);
+  console.log('Make a General Application');
+  gaCaseReference = await api.initiateGeneralApplicationWithOutNotice(config.defendantSolicitorUser, civilCaseReference);
+  console.log('Without Notice General Application Initiated by Defendant2 : ' + gaCaseReference);
+  console.log('*** Start Judge makes decision order made: ' + gaCaseReference + ' ***');
+  await api.judgeMakesDecisionOrderMade(config.judgeUser2WithRegionId2, gaCaseReference);
+  console.log('*** End Judge makes decision order made - GA Case Reference: ' + gaCaseReference + ' ***');
+
+  await I.login(config.defendantSolicitorUser);
+  await I.navigateToTab(civilCaseReference, 'Applications');
+  await I.see('Order Made');
+});
 
 AfterSuite(async ({api_ga}) => {
   await api_ga.cleanUp();
