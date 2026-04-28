51d319615ae999e83e4c71c3a2d9cbfa119c662d
Sabah u Din Irfan <sabah.irfan@gmail.com>
2026-04-28 15:54:20 +0100

    DTSCCI-5291: Fix 504 Gateway Timeout on CCD Definition Upload for ITHC/Perftest (#6460)
    * Fix 504 Gateway Timeout on CCD Definition Upload for ITHC/Perftest
    
    * Fix 504 Gateway Timeout on CCD Definition Upload for ITHC/Perftest

--- bin/variables/load-perftest-environment-variables.sh
+++ bin/variables/load-perftest-environment-variables.sh
@@ -3,4 +3,8 @@
 set -eu
 
 echo 'export ENVIRONMENT=perftest'
-echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-perftest.service.core-compute-perftest.internal"
+# Internal cluster URLs use HTTP (no TLS certs for internal k8s services)
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-perftest.service.core-compute-perftest.internal" # NOSONAR - internal cluster URL
+echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://ccd-definition-store-api-perftest.service.core-compute-perftest.internal" # NOSONAR - internal cluster URL
+echo "export IDAM_API_BASE_URL=https://idam-api.perftest.platform.hmcts.net"
+echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-perftest.service.core-compute-perftest.internal" # NOSONAR - internal cluster URL
