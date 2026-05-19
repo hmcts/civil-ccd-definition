b374e22307437065749f45c13c90ebd5270b5767
jameskr <james.skriczka@hmcts.net>
2026-05-19 13:22:38 +0100

    Remove payment step from journey

--- playwright-e2e/tests/ui-tests/flight-delay/1v2DS_spec_fast_flight_delay.test.ts
+++ playwright-e2e/tests/ui-tests/flight-delay/1v2DS_spec_fast_flight_delay.test.ts
@@ -1,16 +1,14 @@
 import { test } from '../../../playwright-fixtures/index';
 
 test.describe(
-  '1v2DS spec fast track flight delay claim journey',
+  '1v2DS spec fast track flight delay claim journey @debug',
   { tag: '@civil-ccd-nightly @ui-flight-delay' },
   async () => {
     test('1v2DS spec fast track flight delay claim journey', async ({
       ClaimantSolicitorSpecSteps,
-      ClaimantSolicitorSpecApiSteps,
     }) => {
       await ClaimantSolicitorSpecSteps.Login();
       await ClaimantSolicitorSpecSteps.CreateClaimFastTrack1v2DSFlightDelay();
-      await ClaimantSolicitorSpecApiSteps.MakePaymentForClaimIssue();
     });
   },
 );
