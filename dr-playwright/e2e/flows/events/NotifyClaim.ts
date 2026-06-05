17af974cf5b310da96cc25ea1c734d8de88f3387
Douglas Rice <douglas.rice@hmcts.net>
2026-06-05 21:57:00 +0100

    updates

---
+++ dr-playwright/e2e/flows/events/NotifyClaim.ts
@@ -0,0 +1,47 @@
+import { expect, Page } from '@playwright/test';
+import { PageHelper } from '../../../helpers/PageHelper';
+import { ButtonHelper } from '../../../helpers/ButtonHelper.ts';
+import claimTypes from '../../../enums/claim-types.ts';
+import notifyClaimOptions from '../../../enums/notifyClaimOptions.ts';
+
+export class NotifyClaim {
+
+  private buttonHelper: ButtonHelper;
+  private pageHelper: PageHelper;
+
+  constructor(public page: Page) {
+    this.buttonHelper = new ButtonHelper(this.page);
+    this.pageHelper = new PageHelper(this.page);
+  }
+
+  async notify(claimType: claimTypes, whomToNotify: notifyClaimOptions = notifyClaimOptions.BOTH) {
+
+    await this.pageHelper.selectNextStep('Notify claim');
+    if (claimType === claimTypes.ONE_VS_TWO_DIFF_SOL) {
+      await expect(this.page.locator('#defendantSolicitorNotifyClaimOptions')).toContainText('Both');
+      const notifyOptions: string[] = await this.page.locator('#defendantSolicitorNotifyClaimOptions > option').allTextContents();
+
+      for (const [i, value] of notifyOptions.entries()) {
+        if (value.indexOf(whomToNotify) > -1) {
+          await this.page.locator('#defendantSolicitorNotifyClaimOptions').selectOption({ index: i });
+          await this.buttonHelper.continueButton.click();
+
+          if (whomToNotify === notifyClaimOptions.BOTH) {
+            await this.buttonHelper.continueButton.click();
+            await this.buttonHelper.submitButton.click();
+          } else {
+            await this.buttonHelper.IgnoreWarningAndContinueButton.click();
+            await this.buttonHelper.continueButton.click();
+            await this.buttonHelper.submitButton.click();
+          }
+          break;
+        }
+      }
+    } else {
+      await this.buttonHelper.continueButton.click();
+      await this.buttonHelper.submitButton.click();
+    }
+    await this.buttonHelper.closeAndReturnToCaseDetailsButton.click();
+  };
+
+}
---
+++ dr-playwright/enums/notifyClaimOptions.ts
@@ -0,0 +1,8 @@
+enum NotifyClaimOptions {
+  DEFENDANT1 = 'Defendant One',
+  DEFENDANT2 = 'Defendant Two',
+  BOTH = 'Both',
+
+}
+
+export default NotifyClaimOptions;
