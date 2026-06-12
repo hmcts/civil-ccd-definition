f4503199c808c0395645e88ad437410cb43e8e94
zakk scorer <zakk.scorer@hmcts.net>
2026-06-12 14:48:17 +0100

    add pdf flow to ui full defence test

--- e2e/tests/api_tests/spec-full-defence/api_1v1_spec_full_defence_test.js
+++ e2e/tests/api_tests/spec-full-defence/api_1v1_spec_full_defence_test.js
@@ -1,47 +1,18 @@
 const config = require('../../../config.js');
-const claimDocumentHelper = require('../../../helpers/claimDocumentHelper.js');
-
-const TEST_DIR = 'e2e/tests/api_tests/spec-full-defence';
-const BASELINE_DIR = 'e2e/pdf-baselines/api-spec-full-defence';
-
-const SEALED_CLAIM_PDF = 'sealed_claim_form.pdf';
-const DEFENDANT_DIRECTIONS_QUESTIONNAIRE_PDF = 'defendant_directions_questionnaire.pdf';
-const DEFENDANT_DEFENCE_PDF = 'response_sealed_form.pdf';
-const CLAIMANT_DIRECTIONS_QUESTIONNAIRE_PDF = 'claimant_directions_questionnaire.pdf';
 
 Feature('1v1 spec full defence api journey').tag('@civil-service-nightly @api-spec-full-defence');
 
-Scenario('1v1 spec full defence', async ({ api_spec, I }) => {
+Scenario('1v1 spec full defence', async ({ api_spec }) => {
   const caseId = await api_spec.createClaimWithRepresentedRespondent(config.applicantSolicitorUser);
-
-  await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(caseId);
-  await I.waitForText('Summary');
-
-  await claimDocumentHelper.viewAndAssertPdf(I, 'sealed_claim_form', TEST_DIR, BASELINE_DIR, SEALED_CLAIM_PDF);
 
   await api_spec.informAgreedExtensionDate(config.applicantSolicitorUser);
   await api_spec.defendantResponse(config.defendantSolicitorUser);
-
-  await I.login(config.defendantSolicitorUser);
-  await I.navigateToCaseDetails(caseId);
-  await I.waitForText('Summary');
-
-  await claimDocumentHelper.viewAndAssertPdf(I, 'defendant_directions_questionnaire_form', TEST_DIR, BASELINE_DIR, DEFENDANT_DIRECTIONS_QUESTIONNAIRE_PDF);
-  await claimDocumentHelper.viewAndAssertPdf(I, 'response_sealed_form', TEST_DIR, BASELINE_DIR, DEFENDANT_DEFENCE_PDF);
-
   await api_spec.claimantResponse(
     config.applicantSolicitorUser,
     'FULL_DEFENCE',
     'ONE_V_ONE',
     'AWAITING_APPLICANT_INTENTION'
   );
-
-  await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(caseId);
-  await I.waitForText('Summary');
-
-  await claimDocumentHelper.viewAndAssertPdf(I, 'claimant_directions_questionnaire_form', TEST_DIR, BASELINE_DIR, CLAIMANT_DIRECTIONS_QUESTIONNAIRE_PDF);
 });
 
 AfterSuite(async ({ api_spec }) => {
--- e2e/tests/ui_tests/spec-full-defence/1v1_spec_fast_full_defence_test.js
+++ e2e/tests/ui_tests/spec-full-defence/1v1_spec_fast_full_defence_test.js
@@ -1,16 +1,24 @@
 const config = require('../../../config.js');
-const { assignCaseToLRSpecDefendant } = require('../../../api/testingSupport');
+const { assignCaseToLRSpecDefendant, waitForFinishedBusinessProcess } = require('../../../api/testingSupport');
 const { addUserCaseMapping, unAssignAllUsers } = require('../../../api/caseRoleAssignmentHelper');
 const serviceRequest = require('../../../pages/createClaim/serviceRequest.page');
+const claimDocumentHelper = require('../../../helpers/claimDocumentHelper.js');
 
 // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
 //const caseEventMessage = eventName => `Case ${caseNumber} has been updated with event: ${eventName}`;
+
+const TEST_DIRECTORY = 'e2e/tests/ui_tests/spec-full-defence';
+const BASELINE_DIRECTORY = 'e2e/pdf-baselines/ui-spec-full-defence';
+const SEALED_CLAIM_PDF = 'sealed_claim_form.pdf';
+const DEFENDANT_DIRECTIONS_QUESTIONNAIRE_PDF = 'defendant_directions_questionnaire.pdf';
+const DEFENDANT_DEFENCE_PDF = 'response_sealed_form.pdf';
+const CLAIMANT_DIRECTIONS_QUESTIONNAIRE_PDF = 'claimant_directions_questionnaire.pdf';
 
 let caseNumber;
 
 Feature('1v1 spec claim journey').tag('@civil-ccd-nightly @ui-spec-full-defence');
 
-Scenario('01 1v1 Applicant solicitor creates specified claim for fast track-spec', async ({ LRspec }) => {
+Scenario('01 1v1 Applicant solicitor creates specified claim for fast track-spec', async ({ LRspec, I }) => {
   await LRspec.login(config.applicantSolicitorUser);
   await LRspec.createCaseSpecified('1v1 fast claim', 'organisation', null, 'company', null, 19000);
   caseNumber = await LRspec.grabCaseNumber();
@@ -20,6 +28,11 @@ Scenario('01 1v1 Applicant solicitor creates specified claim for fast track-spec
   // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
   //await LRspec.see(`Case ${caseNumber} has been created.`);
   addUserCaseMapping(caseNumber, config.applicantSolicitorUser);
+
+  await I.login(config.applicantSolicitorUser);
+  await I.navigateToCaseDetails(caseNumber);
+  await I.waitForText('Summary');
+  await claimDocumentHelper.viewAndAssertPdf(I, 'sealed_claim_form', TEST_DIRECTORY, BASELINE_DIRECTORY, SEALED_CLAIM_PDF);
 }).retry(2);
 
 Scenario('02 1v1 Defendant solicitor perform Inform Agreed Extension', async ({ LRspec, I }) => {
@@ -41,11 +54,22 @@ Scenario('03 1v1 Respond To Claim - Defendants solicitor rejects claim for defen
 
   // Reinstate the line below when https://tools.hmcts.net/jira/browse/EUI-6286 is fixed
   //await LRspec.see(caseEventMessage('Respond to claim'));
+
+  await I.navigateToCaseDetails(caseNumber);
+  await I.waitForText('Summary');
+
+  await claimDocumentHelper.viewAndAssertPdf(I, 'defendant_directions_questionnaire_form', TEST_DIRECTORY, BASELINE_DIRECTORY, DEFENDANT_DIRECTIONS_QUESTIONNAIRE_PDF);
+  await claimDocumentHelper.viewAndAssertPdf(I, 'response_sealed_form', TEST_DIRECTORY, BASELINE_DIRECTORY, DEFENDANT_DEFENCE_PDF);
 }).retry(2);
 
 Scenario('04 1v1 Claimant solicitor responds to defence - claimant Intention to proceed', async ({ LRspec, I }) => {
   await LRspec.login(config.applicantSolicitorUser);
   await LRspec.respondToDefence({ mpScenario: 'ONE_V_ONE', claimType: 'fast' });
+
+  await I.navigateToCaseDetails(caseNumber);
+  await I.waitForText('Summary');
+
+  await claimDocumentHelper.viewAndAssertPdf(I, 'claimant_directions_questionnaire_form', TEST_DIRECTORY, BASELINE_DIRECTORY, CLAIMANT_DIRECTIONS_QUESTIONNAIRE_PDF);
 }).retry(2);
 
 
