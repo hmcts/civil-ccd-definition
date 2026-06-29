125c99b7c614e80cd12e1a04ab10edfae9adc1fe
rishikrsharma <rishikrsharma@users.noreply.github.com>
2026-06-29 09:10:29 +0100

    Revert "fix wa repo smoke test (#6611)"
    This reverts commit 53f0af862f381a72d2abe401b2fffad6c030047d.

--- e2e/api/apiRequest.js
+++ e2e/api/apiRequest.js
@@ -250,7 +250,7 @@ module.exports = {
     return response;
   },
   validatePage: async (eventName, pageId, caseData, caseId, expectedStatus = 200) => {
-    return restHelper.retriedJsonRequest(`${getCcdDataStoreBaseUrl()}/validate?pageId=${eventName}${pageId}`, getRequestHeaders(tokens.userAuth),
+    return restHelper.retriedRequest(`${getCcdDataStoreBaseUrl()}/validate?pageId=${eventName}${pageId}`, getRequestHeaders(tokens.userAuth),
       {
         case_reference: caseId,
         data: caseData,
