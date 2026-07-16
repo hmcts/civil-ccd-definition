e78a5777311d75b7234dc2da5096d76820e8e8c1
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2026-07-16 16:59:10 +0100

    Migrated Not Suitable Sdo Tests

--- playwright-e2e/schema-builders/exui/judge-la/judge-la-schema-builder-factory.ts
+++ playwright-e2e/schema-builders/exui/judge-la/judge-la-schema-builder-factory.ts
@@ -1,6 +1,7 @@
 import BaseSchemaBuilderFactory from '../../../base/base-schema-builder-factory';
 import CreateSdoSchemaBuilder from './create-sdo/create-sdo-schema-builder';
 import GenerateDirectionsOrderSchemaBuilder from './generate-directions-order/generate-directions-order-schema-builder';
+import NotSuitableSdoSchemaBuilder from './not-suitable-sdo/not-suitable-sdo-schema-builder';
 
 export default class JudgeLASchemaBuilderFactory extends BaseSchemaBuilderFactory {
   get createSdoSchemaBuilder() {
@@ -9,5 +10,9 @@ export default class JudgeLASchemaBuilderFactory extends BaseSchemaBuilderFactor
 
   get generateDirectionsOrderSchemaBuilder() {
     return new GenerateDirectionsOrderSchemaBuilder(this.testData);
+  }
+
+  get notSuitableSdoSchemaBuilder() {
+    return new NotSuitableSdoSchemaBuilder(this.testData);
   }
 }
