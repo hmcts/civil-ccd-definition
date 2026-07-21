34a58db0e4f894de1d9abf30ef54e834db031388
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2026-07-21 13:49:04 +0100

    update

--- playwright-e2e/tests/api-tests/case-flags/api_1v1_case_flags.test.ts
+++ playwright-e2e/tests/api-tests/case-flags/api_1v1_case_flags.test.ts
@@ -1,22 +1,22 @@
 import { test } from '../../../playwright-fixtures/index';
 
-test.describe('1v1 case flags api journey @debug', { tag: '@civil-service-nightly' }, async () => {
+test.describe('1v1 case flags api journey', { tag: '@civil-service-nightly' }, async () => {
   test('1v1 case flags', async ({
     ClaimantSolicitorApiSteps,
     CaseRoleAssignmentApiSteps,
     DefendantSolicitor1ApiSteps,
     HearingCenterAdminApiSteps,
   }) => {
-    // await ClaimantSolicitorApiSteps.CreateClaimFast1v1();
-    // await ClaimantSolicitorApiSteps.MakePaymentForClaimIssue();
-    // await ClaimantSolicitorApiSteps.NotifyClaim();
-    // await CaseRoleAssignmentApiSteps.AssignCaseRoleToDS1();
-    // await ClaimantSolicitorApiSteps.NotifyClaimDetails();
-    // await DefendantSolicitor1ApiSteps.RespondFastFullDefence();
-    // await ClaimantSolicitorApiSteps.RespondFastProceed();
-    // await HearingCenterAdminApiSteps.CreateCaseFlagCaseLevel();
-    // await HearingCenterAdminApiSteps.CreateCaseFlagClaimant1();
-    // await HearingCenterAdminApiSteps.CreateCaseFlagDefendant1();
+    await ClaimantSolicitorApiSteps.CreateClaimFast1v1();
+    await ClaimantSolicitorApiSteps.MakePaymentForClaimIssue();
+    await ClaimantSolicitorApiSteps.NotifyClaim();
+    await CaseRoleAssignmentApiSteps.AssignCaseRoleToDS1();
+    await ClaimantSolicitorApiSteps.NotifyClaimDetails();
+    await DefendantSolicitor1ApiSteps.RespondFastFullDefence();
+    await ClaimantSolicitorApiSteps.RespondFastProceed();
+    await HearingCenterAdminApiSteps.CreateCaseFlagCaseLevel();
+    await HearingCenterAdminApiSteps.CreateCaseFlagClaimant1();
+    await HearingCenterAdminApiSteps.CreateCaseFlagDefendant1();
     await HearingCenterAdminApiSteps.ManageCaseFlagCaseLevel();
     await HearingCenterAdminApiSteps.ManageCaseFlagClaimant1();
     await HearingCenterAdminApiSteps.ManageCaseFlagDefendant1();
