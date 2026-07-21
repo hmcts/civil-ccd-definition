fd8031386c1d70771a1700aba91bd771ab6af438
jameskr <james.skriczka@hmcts.net>
2026-07-21 15:18:44 +0100

    Remove casestate line

--- playwright-e2e/steps/api/exui/defendant-solicitor-1-spec-api-steps.ts
+++ playwright-e2e/steps/api/exui/defendant-solicitor-1-spec-api-steps.ts
@@ -169,7 +169,6 @@ export default class DefendantSolicitor1SpecApiSteps extends BaseApi {
       defendantSolicitor1User,
       ccdEvents.DEFENDANT_RESPONSE_SPEC,
       defendantResponseEventData,
-      CaseState.AWAITING_RESPONDENT_ACKNOWLEDGEMENT,
     );
 
     const { defendantResponseSpecSchemaBuilder } =
