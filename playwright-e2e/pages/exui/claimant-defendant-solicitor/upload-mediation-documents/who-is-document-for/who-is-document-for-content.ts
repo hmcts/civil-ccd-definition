1998678ec62a9860eff46b37ecbcc2edcab5800a
James Skriczka <james.skriczka@hmcts.net>
2026-08-06 13:32:56 +0100

    DTSCCI-6023: Migrate 'Mediation' UI tests to Playwright (#6714)
    * DTSCCI-6023: Migrate Mediation UI tests to Playwright
    
    * DTSCCI-6023: Migrate Mediation UI tests to Playwright
    
    * Refactor upload mediation tests
    
    ---------
    
    Co-authored-by: kdaHMCTS <128375235+kdaHMCTS@users.noreply.github.com>
    Co-authored-by: kdaHMCTS <kiyrean.dyer-allen@hmcts.net>

---
+++ playwright-e2e/pages/exui/claimant-defendant-solicitor/upload-mediation-documents/who-is-document-for/who-is-document-for-content.ts
@@ -0,0 +1,40 @@
+import partys from "../../../../../constants/users/partys";
+import CaseDataHelper from "../../../../../helpers/case-data-helper";
+import { ClaimantDefendantPartyType } from "../../../../../models/users/claimant-defendant-party-types";
+
+export const headings = {
+  selectParty: 'Select what party the document is for',
+};
+
+export const radioButtons = {
+  uploadMediationDocumentsPartyChosen: {
+    label: 'Select one of the options',
+    hintText: 'You can use the options below to let the court know who this document is from',
+    parties: {
+      claimant1: {
+        label: (claimant1PartyType: ClaimantDefendantPartyType) => `Claimant 1:${CaseDataHelper.buildClaimantAndDefendantData(partys.CLAIMANT_1,claimant1PartyType).partyName}`,
+        selector:'#uploadMediationDocumentsPartyChosen_CLAIMANT_1',
+      },
+      claimant2: {
+        label: (claimant2PartyType: ClaimantDefendantPartyType) => `Claimant 2: ${CaseDataHelper.buildClaimantAndDefendantData(partys.CLAIMANT_2,claimant2PartyType).partyName}`,
+        selector:'#uploadMediationDocumentsPartyChosen_CLAIMANT_2',
+      },
+      claimants: {
+        label: 'Claimants 1 and 2',
+        selector:'#uploadMediationDocumentsPartyChosen_CLAIMANTS',
+      },
+      defendant1: {
+        label: (defendant1PartyType: ClaimantDefendantPartyType) => `Defendant 1: ${CaseDataHelper.buildClaimantAndDefendantData(partys.DEFENDANT_1,defendant1PartyType).partyName}`,
+        selector:'#uploadMediationDocumentsPartyChosen_DEFENDANT_1',
+      },
+      defendant2: {
+        label: (defendant2PartyType: ClaimantDefendantPartyType) => `Defendant 2: ${CaseDataHelper.buildClaimantAndDefendantData(partys.DEFENDANT_2,defendant2PartyType).partyName}`,
+        selector:'#uploadMediationDocumentsPartyChosen_DEFENDANT_2',
+      },
+      defendants: {
+        label: 'Defendants 1 and 2',
+        selector:'#uploadMediationDocumentsPartyChosen_DEFENDANTS',
+      },
+    },
+  },
+};
