aeb59a23580f0a595a26f161e86f142bfa85f1ff
Matt Goodwin <matt.goodwin@solirius.com>
2026-08-17 11:17:54 +0100

    DTSCCI-5194 Migrate Defendant And Claimant Response Workflow Coverage (#6727)

--- e2e/tests/api_tests/unspec-full-defence/api_2v1_test.js
+++ e2e/tests/api_tests/unspec-full-defence/api_2v1_test.js
@@ -5,7 +5,7 @@ const mpScenario = 'TWO_V_ONE';
 
 Feature('2v1 unspec full defence api journey').tag('@civil-service-nightly @api-unspec-full-defence');
 
-Scenario('2v1 unspec full defence', async ({api}) => {
+Scenario.skip('2v1 unspec full defence', async ({api}) => {
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
   await api.addCaseNote(config.adminUser);
   await api.amendClaimDocuments(config.applicantSolicitorUser);
