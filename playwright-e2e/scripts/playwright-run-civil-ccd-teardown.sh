b55c8f07d35ee22e7ce386d399e765b0d96606a1
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2026-08-26 14:29:48 +0100

    finished implementation of run failed tests for playwright tests

---
+++ playwright-e2e/scripts/playwright-run-civil-ccd-teardown.sh
@@ -0,0 +1,19 @@
+if [ "$RUN_FAILED_TESTS" != "true" ]; then
+  PLAYWRIGHT_LAST_RUN_REPORT='playwright-functional-test-results/civil-ccd-nightly/.last-run.json'
+
+  if [ ! -f "$PLAYWRIGHT_LAST_RUN_REPORT" ] || [ ! -s "$PLAYWRIGHT_LAST_RUN_REPORT" ]; then
+    echo ".last-run.json not found or is empty."
+    exit 1
+  elif [ "$(jq -r '.status // empty' "$PLAYWRIGHT_LAST_RUN_REPORT")" = "passed" ]; then
+    echo ".last-run.json status is passed"
+    exit 0
+  elif [ "$(jq -r '.status // empty' "$PLAYWRIGHT_LAST_RUN_REPORT")" = "failed" ]; then
+    yarn test:playwright:teardown:civil-ccd-nightly:ci
+  else
+    LAST_RUN_STATUS=$(jq -r '.status // empty' "$PLAYWRIGHT_LAST_RUN_REPORT")
+    echo ".last-run.json status is '$LAST_RUN_STATUS', expected 'passed' or 'failed'"
+    exit 1
+  fi
+else
+  yarn test:playwright:teardown:civil-ccd-nightly:ci
+fi
