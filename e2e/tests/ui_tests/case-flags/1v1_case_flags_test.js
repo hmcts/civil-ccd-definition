aac176cb446067ad947bd32fcd9b0c7b7112bdb1
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2026-08-27 15:47:03 +0100

    failing codeceptjs test

--- e2e/tests/ui_tests/case-flags/1v1_case_flags_test.js
+++ e2e/tests/ui_tests/case-flags/1v1_case_flags_test.js
@@ -14,42 +14,43 @@ Scenario('01 Prepare 1v1 unspec claim up to case progression', async ({api, I}) 
   await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
   await api.notifyClaim(config.applicantSolicitorUser);
   await api.notifyClaimDetails(config.applicantSolicitorUser);
-  await api.defendantResponse(config.defendantSolicitorUser, mpScenario);
-  await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO');
-  caseNumber = await api.getCaseId();
-  await I.setCaseId(caseNumber);
-  addUserCaseMapping(caseNumber, config.applicantSolicitorUser);
-}).retry(1);
+  throw Error('This test does not work');
+  // await api.defendantResponse(config.defendantSolicitorUser, mpScenario);
+  // await api.claimantResponse(config.applicantSolicitorUser, mpScenario, 'AWAITING_APPLICANT_INTENTION', 'FOR_SDO');
+  // caseNumber = await api.getCaseId();
+  // await I.setCaseId(caseNumber);
+  // addUserCaseMapping(caseNumber, config.applicantSolicitorUser);
+}).retry(0);
 
-Scenario('02 Add case flags', async ({I}) => {
-  const caseFlags = [{
-    partyName: 'Test Inc', roleOnCase: 'Claimant 1',
-    details: [PARTY_FLAGS.vulnerableUser.value]
-  }, {
-    partyName: 'John Doe', roleOnCase: 'Defendant solicitor 1 expert',
-    details: [PARTY_FLAGS.unacceptableBehaviour.value]
-  }];
+// Scenario('02 Add case flags', async ({I}) => {
+//   const caseFlags = [{
+//     partyName: 'Test Inc', roleOnCase: 'Claimant 1',
+//     details: [PARTY_FLAGS.vulnerableUser.value]
+//   }, {
+//     partyName: 'John Doe', roleOnCase: 'Defendant solicitor 1 expert',
+//     details: [PARTY_FLAGS.unacceptableBehaviour.value]
+//   }];
 
-  await I.login(config.hearingCenterAdminWithRegionId1);
-  await I.createCaseFlags(caseFlags);
-  // await I.validateCaseFlags(caseFlags);
-}).retry(1);
+//   await I.login(config.hearingCenterAdminWithRegionId1);
+//   await I.createCaseFlags(caseFlags);
+//   // await I.validateCaseFlags(caseFlags);
+// }).retry(1);
 
-Scenario('03 Manage case flags', async ({I}) => {
-  const caseFlags = [{
-    partyName: 'Test Inc', roleOnCase: 'Claimant 1',
-    flagType: 'Vulnerable user',
-    flagComment: 'test comment'
-  }, {
-    partyName: 'John Doe', roleOnCase: 'Defendant solicitor 1 expert',
-    flagType: 'Unacceptable/disruptive customer behaviour',
-    flagComment: 'test comment'
-  }];
+// Scenario('03 Manage case flags', async ({I}) => {
+//   const caseFlags = [{
+//     partyName: 'Test Inc', roleOnCase: 'Claimant 1',
+//     flagType: 'Vulnerable user',
+//     flagComment: 'test comment'
+//   }, {
+//     partyName: 'John Doe', roleOnCase: 'Defendant solicitor 1 expert',
+//     flagType: 'Unacceptable/disruptive customer behaviour',
+//     flagComment: 'test comment'
+//   }];
 
-  await I.login(config.hearingCenterAdminWithRegionId1);
-  await I.manageCaseFlags(caseFlags);
-  // await I.validateUpdatedCaseFlags(caseFlags);
-}).retry(1);
+//   await I.login(config.hearingCenterAdminWithRegionId1);
+//   await I.manageCaseFlags(caseFlags);
+//   // await I.validateUpdatedCaseFlags(caseFlags);
+// }).retry(1);
 
 AfterSuite(async () => {
   await unAssignAllUsers();
