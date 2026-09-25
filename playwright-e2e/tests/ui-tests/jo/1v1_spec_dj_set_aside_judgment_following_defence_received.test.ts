0389021695a6bcf64875acd2747e5698c433caeb
jameskr <james.skriczka@hmcts.net>
2026-09-25 16:59:55 +0100

    scenario > revert func fail + keep smoke test fail + keep teardown fail

--- playwright-e2e/tests/ui-tests/jo/1v1_spec_dj_set_aside_judgment_following_defence_received.test.ts
+++ playwright-e2e/tests/ui-tests/jo/1v1_spec_dj_set_aside_judgment_following_defence_received.test.ts
@@ -10,7 +10,7 @@ test.describe(
       ClaimantSolicitorSpecSteps,
       HearingCenterAdminSpecSteps,
     }) => {
-      await ClaimantSolicitorSpecApiSteps.CreateClaimSmall1v1a();
+      await ClaimantSolicitorSpecApiSteps.CreateClaimSmall1v1();
       // await ClaimantSolicitorSpecApiSteps.MakePaymentForClaimIssue();
       // await CaseRoleAssignmentApiSteps.AssignCaseRoleToDS1();
       // await ClaimantSolicitorSpecApiSteps.AmendRespondent1ResponseDeadline();
